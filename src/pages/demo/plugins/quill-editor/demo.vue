fda763cc43c0341de59f87da3f5c9af48381e7a7
李杨 <1711467488@qq.com>
2018-01-16 15:26:07 +0800

    no message
    Former-commit-id: 8ef30c0089a7268a441a328fc74f113b0af017a1
    Former-commit-id: 2d06ade06f52ff92a2fefcf42b8b114b3506ca27
    Former-commit-id: 5fda8f1a5e9ec9bc5e30e17f44f21bf1a43666cf

---
+++ src/pages/demo/plugins/quill-editor/demo.vue
@@ -0,0 +1,40 @@
+<template>
+  <Container type="ghost">
+    <el-card>
+      <QuillEditor
+        v-model="value"
+        @text-change="textChangeHandler"
+        @selection-change="selectionChangeHandler"
+        @editor-change="editorChangeHandler">
+      </QuillEditor>
+      {{value}}
+    </el-card>
+  </Container>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      value: '<p><strong style="background-color: rgb(240, 102, 102); color: rgb(255, 255, 255);">Hello</strong> <strong style="color: rgb(255, 255, 255); background-color: rgb(102, 163, 224);">World</strong></p>'
+    }
+  },
+  methods: {
+    textChangeHandler (delta, oldDelta, source) {
+      // console.group('QuillEditor textChangeHandler')
+      // console.log(delta, oldDelta, source)
+      // console.groupEnd()
+    },
+    selectionChangeHandler (range, oldRange, source) {
+      // console.group('QuillEditor selectionChangeHandler')
+      // console.log(range, oldRange, source)
+      // console.groupEnd()
+    },
+    editorChangeHandler (eventName, ...args) {
+      // console.group('QuillEditor editorChangeHandler')
+      // console.log(eventName, args)
+      // console.groupEnd()
+    }
+  }
+}
+</script>
