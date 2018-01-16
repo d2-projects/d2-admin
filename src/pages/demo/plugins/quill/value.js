752e87f5124bd1ebf86629b357886b12f9468db7
李杨 <1711467488@qq.com>
2018-01-16 21:08:02 +0800

    no message

--- src/pages/demo/plugins/quill/value.js
+++ src/pages/demo/plugins/quill/value.js
@@ -1,7 +1,6 @@
-export default `<p class="ql-align-center">
+export default `<p>
 <a href="https://github.com/FairyEver/d2admin-vue-element" target="_blank" class="ql-size-large" style="color: rgb(65, 184, 131);"><strong>D2 Admin</strong></a>
-<strong style="color: rgb(65, 184, 131);">
-</strong>
+<strong style="color: rgb(65, 184, 131);"></strong>
 by
 <a href="https://cn.vuejs.org/" target="_blank" class="ql-size-large" style="color: rgb(65, 184, 131);"><strong>vue.js</strong></a>
 </p>`
