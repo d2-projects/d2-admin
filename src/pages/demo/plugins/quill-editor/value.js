903d62c01231a453904a8374cb95c340edb198d3
李杨 <1711467488@qq.com>
2018-01-16 16:02:37 +0800

    富文本编辑器结果格式化
    Former-commit-id: 8396ff85368638200e44ccb21d8ec32081563908
    Former-commit-id: fae52c391ca954e5772f44793fde1805a6561bb4
    Former-commit-id: 2f60a6d5371e98bb4f1152d41937f9fceedcc6be

---
+++ src/pages/demo/plugins/quill-editor/value.js
@@ -0,0 +1,9 @@
+export default `<p>
+<strong class="ql-size-large" style="color: rgb(0, 138, 0);">
+  D2 Admin
+</strong>
+by
+<strong class="ql-size-large" style="color: rgb(0, 138, 0);">
+  vue.js
+</strong>
+</p>`
