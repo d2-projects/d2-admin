8d783c5bc8dc9246440ee47cf22f2afeb24f97e9
李杨 <1711467488@qq.com>
2018-01-16 09:13:15 +0800

    no message
    Former-commit-id: 347253b910c9c8fd2c5a7d563e5c59dff6759ea3
    Former-commit-id: 11a8486ada0efc2244927c648ee96511b06b27dd
    Former-commit-id: 2549cc4501bc5f40eccdba413c8ca458dae7a3b9

--- src/pages/demo/plugins/mock/ajax.vue
+++ src/pages/demo/plugins/mock/ajax.vue
@@ -4,7 +4,7 @@
       <div slot="header">
         <el-button @click="ajax">发送请求</el-button>
       </div>
-      <el-table v-bind="table" style="width: 100%">
+      <el-table v-bind="table" style="width: 100%" class="mb">
         <el-table-column
           v-for="(item, index) in table.columns"
           :key="index"
@@ -12,6 +12,7 @@
           :label="item.label">
         </el-table-column>
       </el-table>
+      <Markdown url="/static/markdownFiles/article/mock拦截ajax.md"></Markdown>
     </el-card>
   </Container>
 </template>
@@ -31,7 +32,7 @@ export default {
   },
   methods: {
     ajax () {
-      this.$axios.get('/abc')
+      this.$axios.get('/api/demo/001')
       .then(res => {
         this.table.columns = Object.keys(res.data.list[0]).map(e => ({
           label: e,
