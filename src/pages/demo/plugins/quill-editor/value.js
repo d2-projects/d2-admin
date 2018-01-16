f2309e7e18393555776e694d9f4ef278dff0a902
李杨 <1711467488@qq.com>
2018-01-16 17:20:59 +0800

    no message
    Former-commit-id: 1bf0f091631880655781ca89613890d1b122de81
    Former-commit-id: a22d62cd2d99700ab930931e4b27a3490407c912
    Former-commit-id: 6122da3f1ef9b446943482d6719534ba2edaec97

--- src/pages/demo/plugins/quill-editor/value.js
+++ src/pages/demo/plugins/quill-editor/value.js
@@ -1,9 +1,7 @@
 export default `<p class="ql-align-center">
-<strong class="ql-size-large" style="color: rgb(65, 184, 131);">
-  D2 Admin
+<a href="https://github.com/FairyEver/d2admin-vue-element" target="_blank" class="ql-size-large" style="color: rgb(65, 184, 131);"><strong>D2 Admin</strong></a>
+<strong style="color: rgb(65, 184, 131);">
 </strong>
 by
-<strong class="ql-size-large" style="color: rgb(65, 184, 131);">
-  vue.js
-</strong>
+<a href="https://cn.vuejs.org/" target="_blank" class="ql-size-large" style="color: rgb(65, 184, 131);"><strong>vue.js</strong></a>
 </p>`
