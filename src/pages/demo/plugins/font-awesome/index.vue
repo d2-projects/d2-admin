6a9de9142ea4795fca1d97dfa06e386aedc024f7
李杨 <1711467488@qq.com>
2018-01-17 10:28:04 +0800

    no message
    Former-commit-id: 2793ce71c127ee431da465a4ba84805626fcf964
    Former-commit-id: f7ecb10fa6d87ffe72f965ee62b64cdd8230c8b6
    Former-commit-id: f3679faa52dcc824dd3e687f7319dce7ea39162b

--- src/pages/demo/plugins/font-awesome/index.vue
+++ src/pages/demo/plugins/font-awesome/index.vue
@@ -1,11 +1,20 @@
 <template>
-  <Container>
-    <div v-for="(item, index) in icon" :key="index">
-      <h1>{{item.title}}</h1>
-      <div>
-        <span v-for="(iconItem, iconIndex) in item.icon" :key="iconIndex" :class="'fa fa-' + iconItem"></span>
-      </div>
-    </div>
+  <Container type="ghost">
+    <el-card v-for="(item, index) in icon" :key="index" :class="index === icon.length - 1 ? '' : 'dd-mb'">
+      <template slot="header">{{item.title}}</template>
+      <el-row style="margin: -10px;">
+        <el-col
+          v-for="(iconItem, iconIndex) in item.icon"
+          :key="iconIndex"
+          :span="4"
+          class="dd-m-10">
+          <el-tag type="info">
+            <span :class="'fa fa-' + iconItem"></span>
+          </el-tag>
+          <span style="font-size: 10px;">{{iconItem}}</span>
+        </el-col>
+      </el-row>
+    </el-card>
   </Container>
 </template>
 
