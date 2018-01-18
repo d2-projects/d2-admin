680c1d0942e61e3ed770114853f15ddcaa83aeb7
李杨 <1711467488@qq.com>
2018-01-18 14:34:38 +0800

    no message

--- src/router/menu/index.js
+++ src/router/menu/index.js
@@ -1,17 +1,14 @@
 import * as plugin from './_plugin'
-import * as charts from './_charts'
 import * as business from './_business'
 
 // 菜单
 export const menu = [
   plugin.menu,
-  charts.menu,
   business.menu
 ]
 
 // 路由
 export const router = [
   plugin.router,
-  charts.router,
   business.router
 ]
