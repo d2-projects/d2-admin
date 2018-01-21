9b7a5d3b5b5e3eb99dfc135d66138b64fc96e9e7
李杨 <1711467488@qq.com>
2018-01-21 22:47:50 +0800

    no message
    Former-commit-id: 6f04869536fc54ea3209d68ad1c1b3a99ebf1f90
    Former-commit-id: c6cbfaaed3ba2905d1f3420bf8ea89b8c9325741
    Former-commit-id: ebc4c064129b7c58abbb2802d275a4f59a210e46

---
+++ src/components/core/index.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+
+Vue.component('Container', resolve => { require(['@/components/core/Container'], resolve) })
+Vue.component('CountUp', resolve => { require(['@/components/core/CountUp'], resolve) })
+Vue.component('Highlight', resolve => { require(['@/components/core/Highlight'], resolve) })
+Vue.component('IconSelect', resolve => { require(['@/components/core/IconSelect/index.vue'], resolve) })
+Vue.component('Markdown', resolve => { require(['@/components/core/Markdown'], resolve) })
+Vue.component('QuillEditor', resolve => { require(['@/components/core/QuillEditor'], resolve) })
+Vue.component('SimpleMDE', resolve => { require(['@/components/core/SimpleMDE'], resolve) })
