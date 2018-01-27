89ea9e7fb7a0701077bbf121669f90a61b0e2de2
李杨 <1711467488@qq.com>
2018-01-27 22:38:39 +0800

    no message
    Former-commit-id: c1d5307180c35274a47138df473f6e7c93d82184
    Former-commit-id: 7f699ffc0438187d2efa64e6980226afead071cc
    Former-commit-id: 3a5fac080c16b69737de6403eb3c2f49e7e79a5b

---
+++ src/pages/demo/chart/index/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <Container>
+    INDEX
+  </Container>
+</template>
--- src/router/menu/index.js
+++ src/router/menu/index.js
@@ -1,14 +1,17 @@
 import * as plugin from './_plugin'
 import * as business from './_business'
+import * as chart from './_chart'
 
 // 菜单
 export const menu = [
   plugin.menu,
-  business.menu
+  business.menu,
+  chart.menu
 ]
 
 // 路由
 export const router = [
   plugin.router,
-  business.router
+  business.router,
+  chart.router
 ]
