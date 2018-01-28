3d2201a4311e855ef8f57fb861bbb0e591da8398
李杨 <1711467488@qq.com>
2018-01-28 23:52:37 +0800

    no message
    Former-commit-id: 313b095dffa946b4a4dfffde241353bce31d8cef
    Former-commit-id: 049f9d83d669be0ec8267004227e773eb38b96ac
    Former-commit-id: 508d4dbe3860eb5e339c116253bb2ff14b01ae3b

--- src/mock/index.js
+++ src/mock/index.js
@@ -1,5 +1,12 @@
-import '@/mock/demo/001'
+import Mock from 'mockjs'
+
+import '@/mock/demo'
 
 import '@/mock/login'
 
-import '@/mock/chart/G2Line1'
+import '@/mock/chart'
+
+// 设置全局延时 没有延时的话有时候会检测不到数据变化 建议保留
+Mock.setup({
+  timeout: '600-1000'
+})
