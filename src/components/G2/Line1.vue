430c760b138e5ad5c10a5fb42f0317eb1e201a54
李杨 <1711467488@qq.com>
2018-01-28 14:58:34 +0800

    no message
    Former-commit-id: fa1070f5fcb35fc52abb413635e934c37355e6c6
    Former-commit-id: 776e9b6d9de589b0b8cfddd6fb9c344d2e18e1e0
    Former-commit-id: 5a91fe2b8da25531e4f0d322d5cb5ab12b9e22be

--- src/components/G2/Line1.vue
+++ src/components/G2/Line1.vue
@@ -1,3 +1,47 @@
 <template>
-  <div>G2</div>
+  <div ref="chart" style="height: 100%;"></div>
 </template>
+
+<script>
+import G2Mixin from './G2'
+export default {
+  mixins: [
+    G2Mixin
+  ],
+  methods: {
+    initHandler () {
+      const data = [
+        { year: '1991', value: 3 },
+        { year: '1992', value: 4 },
+        { year: '1993', value: 3.5 },
+        { year: '1994', value: 5 },
+        { year: '1995', value: 4.9 },
+        { year: '1996', value: 6 },
+        { year: '1997', value: 7 },
+        { year: '1998', value: 9 },
+        { year: '1999', value: 13 }
+      ]
+      this.creatChart()
+      this.chart.source(data)
+      this.chart.scale('value', {
+        min: 0
+      })
+      this.chart.scale('year', {
+        range: [0, 1]
+      })
+      this.chart.tooltip({
+        crosshairs: {
+          type: 'line'
+        }
+      })
+      this.setChartTitle('历史趋势')
+      this.chart.line().position('year*value')
+      this.chart.point().position('year*value').size(4).shape('circle').style({
+        stroke: '#fff',
+        lineWidth: 1
+      })
+      this.chart.render()
+    }
+  }
+}
+</script>
--- src/components/core/Container/index.vue
+++ src/components/core/Container/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container-component">
+  <div class="container-component" :class="{responsive}">
     <!-- [card] 卡片容器 -->
     <el-card v-if="type === 'card'">
       <div v-if="$slots.header" slot="header">
@@ -17,11 +17,42 @@
 <script>
 export default {
   props: {
+    // 容器样式
     type: {
       type: String,
       required: false,
       default: 'card'
+    },
+    // 是否开启响应式尺寸变化
+    responsive: {
+      type: Boolean,
+      required: false,
+      default: false
     }
   }
 }
 </script>
+
+<style lang="scss">
+.container-component {
+  margin-right: 20px;
+  margin-bottom: 20px;
+}
+@media (min-width: 576px) {
+  // 根据你的需要在这里添加样式
+}
+@media (min-width: 768px) {
+  // 根据你的需要在这里添加样式
+}
+@media (min-width: 992px) {
+  // 根据你的需要在这里添加样式
+}
+// 在大于1920分辨率的时候
+@media (min-width: 1921px) {
+  .container-component.responsive {
+    margin: 0px auto;
+    margin-bottom: 20px;
+    max-width: 1920px - 200px;
+  }
+}
+</style>
