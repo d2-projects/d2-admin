ae6555489398d2f1093b3ebb4f3cc64fffd9f00c
李杨 <1711467488@qq.com>
2018-01-29 14:20:21 +0800

    no message

--- src/components/G2/index.js
+++ src/components/G2/index.js
@@ -1,3 +1,4 @@
 import Vue from 'vue'
 
 Vue.component('G2Line1', resolve => { require(['@/components/G2/Line/1.vue'], resolve) })
+Vue.component('G2Pie1', resolve => { require(['@/components/G2/Pie/1.vue'], resolve) })
