db6beafd47566c80479597ee425ca34d192a80d1
李杨 <1711467488@qq.com>
2018-02-09 15:13:27 +0800

    no message
    Former-commit-id: a654629cd662c0cb63d33df0bc14c6cef6602c72
    Former-commit-id: d2d6e1d3201698beb0496d06f86aa9f1d6132bd2
    Former-commit-id: 19b5b3f1c97dbffc16daf64936d7633f435f42d2

---
+++ src/pages/demo/plugins/tableExport/data/index.js
@@ -0,0 +1,20 @@
+import Mock from 'mockjs'
+
+const mockData = Mock.mock({
+  'data|4-10': [{
+    'id|+1': 1,
+    'name': '@CNAME',
+    'delFlag|1': [0, 1],
+    'creatDate': '@DATE',
+    'address': '@CITY',
+    'zip': '@ZIP'
+  }]
+})
+
+export default {
+  data: mockData.data,
+  columns: Object.keys(mockData.data[0]).map(e => ({
+    label: e,
+    prop: e
+  }))
+}
