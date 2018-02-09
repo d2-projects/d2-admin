db0805c1179cbd47d930d6ba5ca7622f1e4e41ff
李杨 <1711467488@qq.com>
2018-02-09 15:13:27 +0800

    no message

--- src/pages/demo/plugins/tableExport/demo.vue
+++ src/pages/demo/plugins/tableExport/demo.vue
@@ -16,50 +16,17 @@
 </template>
 
 <script>
-import Mock from 'mockjs'
+import table from './data'
 export default {
   data () {
     return {
       table: {
-        columns: [],
-        data: [],
+        columns: table.columns,
+        data: table.data,
         size: 'mini',
         stripe: true,
         border: true
       }
-    }
-  },
-  created () {
-    this.loadTableData()
-  },
-  methods: {
-    // ajax () {
-    //   this.$axios.get('/api/demo/001')
-    //   .then(res => {
-    //     this.table.columns = Object.keys(res.list[0]).map(e => ({
-    //       label: e,
-    //       prop: e
-    //     }))
-    //     this.table.data = res.list
-    //   })
-    // }
-    // 载入表格数据
-    loadTableData () {
-      const mockTableData = Mock.mock({
-        'list|4-10': [{
-          'id|+1': 1,
-          'name': '@CNAME',
-          'delFlag|1': [0, 1],
-          'creatDate': '@DATE',
-          'address': '@CITY',
-          'zip': '@ZIP'
-        }]
-      })
-      this.table.columns = Object.keys(mockTableData.list[0]).map(e => ({
-        label: e,
-        prop: e
-      }))
-      this.table.data = mockTableData.list
     }
   }
 }
