c997f662e871ae587ca3da1fb9b576b41b743e51
李杨 <1711467488@qq.com>
2018-02-09 15:42:28 +0800

    no message
    Former-commit-id: 408058fab164d62f8a60dcb3abbac19a0464dec1
    Former-commit-id: ec41a7db6766d29a89679ed3769fad5ead12058a
    Former-commit-id: 0f4d0fbaa32e40973b79ad12a1a90c3035106a68

---
+++ src/components/core/Icon/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <i class="fa" :class="`fa-${name}`" aria-hidden="true"></i>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: false,
+      default: 'font-awesome'
+    }
+  }
+}
+</script>
