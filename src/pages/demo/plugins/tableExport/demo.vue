85cc0ac66a34b8a694dc54ace70b4928e13e5abb
李杨 <1711467488@qq.com>
2018-02-09 14:56:34 +0800

    no message
    Former-commit-id: 6c34c1431f3ac414bf6bfb612984e118a62d87e4
    Former-commit-id: 1dc32a553d2f5bfb561fbf78d8afae972fe784d9
    Former-commit-id: e4a420650274dd882256f6d5530c5ed4e0ae081b

--- src/pages/demo/plugins/tableExport/demo.vue
+++ src/pages/demo/plugins/tableExport/demo.vue
@@ -4,6 +4,63 @@
       slot="header"
       title="基本示例">
     </PageHeader>
-    
+    <el-table v-bind="table" style="width: 100%" class="dd-mb">
+      <el-table-column
+        v-for="(item, index) in table.columns"
+        :key="index"
+        :prop="item.prop"
+        :label="item.label">
+      </el-table-column>
+    </el-table>
   </Container>
 </template>
+
+<script>
+import Mock from 'mockjs'
+export default {
+  data () {
+    return {
+      table: {
+        columns: [],
+        data: [],
+        size: 'mini',
+        stripe: true,
+        border: true
+      }
+    }
+  },
+  created () {
+    this.loadTableData()
+  },
+  methods: {
+    // ajax () {
+    //   this.$axios.get('/api/demo/001')
+    //   .then(res => {
+    //     this.table.columns = Object.keys(res.list[0]).map(e => ({
+    //       label: e,
+    //       prop: e
+    //     }))
+    //     this.table.data = res.list
+    //   })
+    // }
+    // 载入表格数据
+    loadTableData () {
+      const mockTableData = Mock.mock({
+        'list|4-10': [{
+          'id|+1': 1,
+          'name': '@CNAME',
+          'delFlag|1': [0, 1],
+          'creatDate': '@DATE',
+          'address': '@CITY',
+          'zip': '@ZIP'
+        }]
+      })
+      this.table.columns = Object.keys(mockTableData.list[0]).map(e => ({
+        label: e,
+        prop: e
+      }))
+      this.table.data = mockTableData.list
+    }
+  }
+}
+</script>
