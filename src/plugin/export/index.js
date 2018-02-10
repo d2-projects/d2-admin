bc21855a25c71c88ea2fcce8250812fbc3c9b0aa
李杨 <1711467488@qq.com>
2018-02-10 23:02:32 +0800

    no message
    Former-commit-id: 1291a183755febf46865b02cd09d65541611b6a0
    Former-commit-id: 313bcc3eee11b8b4a54e0e4cd921a0e585ea0db4
    Former-commit-id: 35cce0d3d0336a6883f249fb98db25f83741fa23

--- src/plugin/export/index.js
+++ src/plugin/export/index.js
@@ -6,19 +6,23 @@ export default {
     // 导出
     Vue.prototype.$export = {
       csv (params) {
-        // 默认值
-        const paramsDefault = {
-          columns: [],
-          data: [],
-          noHeader: false
-        }
-        // 合并参数
-        const _params = Object.assign({}, paramsDefault, params)
-        console.log(_params)
-        // 生成数据
-        const data = Csv(_params.columns, _params.data, params, _params.noHeader)
-        // 下载数据
-        ExportCsv.download('table.csv', data)
+        return new Promise((resolve, reject) => {
+          // 默认值
+          const paramsDefault = {
+            columns: [],
+            data: [],
+            noHeader: false
+          }
+          // 合并参数
+          const _params = Object.assign({}, paramsDefault, params)
+          console.log(_params)
+          // 生成数据
+          const data = Csv(_params.columns, _params.data, params, _params.noHeader)
+          // 下载数据
+          ExportCsv.download('table.csv', data)
+          // 完成
+          resolve()
+        })
       }
     }
   }
