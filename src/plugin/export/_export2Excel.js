0000366b700149ab7a2e6cbd3c0c49abe599dffa
李杨 <1711467488@qq.com>
2018-02-11 11:32:36 +0800

    no message

--- src/plugin/export/_export2Excel.js
+++ src/plugin/export/_export2Excel.js
@@ -17,25 +17,20 @@ function generateArray(table) {
             var rowspan = cell.getAttribute('rowspan');
             var cellValue = cell.innerText;
             if (cellValue !== "" && cellValue == +cellValue) cellValue = +cellValue;
-
             //Skip ranges
             ranges.forEach(function (range) {
                 if (R >= range.s.r && R <= range.e.r && outRow.length >= range.s.c && outRow.length <= range.e.c) {
                     for (var i = 0; i <= range.e.c - range.s.c; ++i) outRow.push(null);
                 }
             });
-
             //Handle Row Span
             if (rowspan || colspan) {
                 rowspan = rowspan || 1;
                 colspan = colspan || 1;
                 ranges.push({s: {r: R, c: outRow.length}, e: {r: R + rowspan - 1, c: outRow.length + colspan - 1}});
-            }
-            ;
-
+            };
             //Handle Value
             outRow.push(cellValue !== "" ? cellValue : null);
-
             //Handle Colspan
             if (colspan) for (var k = 0; k < colspan - 1; ++k) outRow.push(null);
         }
