6ed0c2968cebb21c20b8c3f22f3a11ad22e41887
李杨 <1711467488@qq.com>
2018-02-12 18:25:17 +0800

    no message

--- src/pages/demo/plugins/export/txt.vue
+++ src/pages/demo/plugins/export/txt.vue
@@ -4,6 +4,30 @@
       slot="header"
       title="导出文本">
     </PageHeader>
+    <el-input
+      type="textarea"
+      :autosize="{ minRows: 2, maxRows: 4}"
+      placeholder="请输入内容"
+      v-model="text">
+    </el-input>
+    <div class="dd-mt dd-mb">
+      <el-button @click="exportTxt">保存为 txt</el-button>
+    </div>
     <Markdown url="/static/markdownFiles/article/插件 - 导出.md"></Markdown>
   </Container>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      text: ''
+    }
+  },
+  methods: {
+    exportTxt () {
+      console.log('1')
+    }
+  }
+}
+</script>
