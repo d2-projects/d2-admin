ed5f060c966b061544289514d4ccc33e93fd6acc
李杨 <1711467488@qq.com>
2018-02-13 20:52:54 +0800

    no message

--- src/pages/demo/plugins/vue-i18n/demo.vue
+++ src/pages/demo/plugins/vue-i18n/demo.vue
@@ -1,5 +1,22 @@
 <template>
   <Container>
-    i18n
+    <div>
+      <el-radio-group v-model="lang">
+        <el-radio-button label="CN">中文</el-radio-button>
+        <el-radio-button label="EN">英语</el-radio-button>
+      </el-radio-group>
+    </div>
+    {{$t('message.hello')}}
   </Container>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      lang: ''
+    }
+  }
+}
+</script>
+
