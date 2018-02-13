6d92ccbee520ca82c014b363c0945809e2fc3aad
李杨 <1711467488@qq.com>
2018-02-14 00:18:46 +0800

    no message
    Former-commit-id: 32d6da8e107ce91abddc659200a9a1d59a939daa
    Former-commit-id: 6e57bf54a46310b26e57b19e6f207d89a369f824
    Former-commit-id: 9a0dc07dc46f8b1e2f58a3ec335515361627b726

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,13 +1,17 @@
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
 
+import cn from './lang/cn'
+import ja from './lang/ja'
+import en from './lang/en'
+
 Vue.use(VueI18n)
 
 export default new VueI18n({
   locale: 'cn',
   messages: {
-    'cn': require('./lang/cn/').default,
-    'ja': require('./lang/ja/').default,
-    'en': require('./lang/en/').default
+    cn,
+    ja,
+    en
   }
 })
