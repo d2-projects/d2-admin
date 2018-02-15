81568e48fa51c044ac95c4c9a33f72e62ba39bbc
李杨 <1711467488@qq.com>
2018-02-15 15:41:18 +0800

    no message

--- src/plugin/import/index.js
+++ src/plugin/import/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 // 库
 import papa from 'papaparse'
-import XLSX from 'xlsx'
+import xlsx from 'xlsx'
 export default {
   install (Vue, options) {
     Vue.prototype.$import = {
@@ -31,13 +31,13 @@ export default {
           }
           const getHeaderRow = sheet => {
             const headers = []
-            const range = XLSX.utils.decode_range(sheet['!ref'])
+            const range = xlsx.utils.decode_range(sheet['!ref'])
             let C
             const R = range.s.r
             for (C = range.s.c; C <= range.e.c; ++C) {
-              var cell = sheet[XLSX.utils.encode_cell({ c: C, r: R })]
+              var cell = sheet[xlsx.utils.encode_cell({ c: C, r: R })]
               var hdr = 'UNKNOWN ' + C
-              if (cell && cell.t) hdr = XLSX.utils.format_cell(cell)
+              if (cell && cell.t) hdr = xlsx.utils.format_cell(cell)
               headers.push(hdr)
             }
             return headers
@@ -45,11 +45,11 @@ export default {
           reader.onload = e => {
             const data = e.target.result
             const fixedData = fixdata(data)
-            const workbook = XLSX.read(btoa(fixedData), { type: 'base64' })
+            const workbook = xlsx.read(btoa(fixedData), { type: 'base64' })
             const firstSheetName = workbook.SheetNames[0]
             const worksheet = workbook.Sheets[firstSheetName]
             const header = getHeaderRow(worksheet)
-            const results = XLSX.utils.sheet_to_json(worksheet)
+            const results = xlsx.utils.sheet_to_json(worksheet)
             resolve({header, results})
           }
           reader.readAsArrayBuffer(file)
