a17d2ac3bad0e91b0fe3aad1e0754e746474ec3d
李杨 <1711467488@qq.com>
2018-02-15 15:38:07 +0800

    no message

--- src/plugin/import/index.js
+++ src/plugin/import/index.js
@@ -1,5 +1,7 @@
+/* eslint-disable */
 // 库
 import papa from 'papaparse'
+import XLSX from 'xlsx'
 export default {
   install (Vue, options) {
     Vue.prototype.$import = {
@@ -13,6 +15,44 @@ export default {
               resolve(results)
             }
           })
+        })
+      },
+      // 导入 excel
+      excel (file) {
+        return new Promise((resolve, reject) => {
+          const reader = new FileReader()
+          const fixdata = data => {
+            let o = ''
+            let l = 0
+            const w = 10240
+            for (; l < data.byteLength / w; ++l) o += String.fromCharCode.apply(null, new Uint8Array(data.slice(l * w, l * w + w)))
+            o += String.fromCharCode.apply(null, new Uint8Array(data.slice(l * w)))
+            return o
+          }
+          const getHeaderRow = sheet => {
+            const headers = []
+            const range = XLSX.utils.decode_range(sheet['!ref'])
+            let C
+            const R = range.s.r
+            for (C = range.s.c; C <= range.e.c; ++C) {
+              var cell = sheet[XLSX.utils.encode_cell({ c: C, r: R })]
+              var hdr = 'UNKNOWN ' + C
+              if (cell && cell.t) hdr = XLSX.utils.format_cell(cell)
+              headers.push(hdr)
+            }
+            return headers
+          }
+          reader.onload = e => {
+            const data = e.target.result
+            const fixedData = fixdata(data)
+            const workbook = XLSX.read(btoa(fixedData), { type: 'base64' })
+            const firstSheetName = workbook.SheetNames[0]
+            const worksheet = workbook.Sheets[firstSheetName]
+            const header = getHeaderRow(worksheet)
+            const results = XLSX.utils.sheet_to_json(worksheet)
+            resolve({header, results})
+          }
+          reader.readAsArrayBuffer(file)
         })
       }
     }
