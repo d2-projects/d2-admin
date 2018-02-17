cb218ddfccd66e4f618f35cea75ec7f9f04d2f88
李杨 <1711467488@qq.com>
2018-02-17 20:25:11 +0800

    no message

---
+++ src/pages/demo/chart/index/components/ChartCardHeader.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    {{title}}
+    <span class="dd-fr">
+      <el-tooltip effect="dark" content="刷新数据" placement="top">
+        <Icon class="chart-card-header-icon" name="refresh"></Icon>
+      </el-tooltip>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: false,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import '~@/assets/style/public.scss';
+.chart-card-header-icon {
+  color: $color-text-sub;
+}
+</style>
+
