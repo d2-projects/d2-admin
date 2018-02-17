08e281df1008b5eaf5d22692f9f346c2ba51ccdf
李杨 <1711467488@qq.com>
2018-02-17 09:54:59 +0800

    no message

--- src/router/menu/index.js
+++ src/router/menu/index.js
@@ -3,18 +3,18 @@ import * as chart from './_chart'
 import * as plugin from './_plugin'
 import * as components from './_components'
 
-// 菜单
+// 菜单 这里的顺序决定了在顶栏显示的顺序
 export const menu = [
-  business.menu,
-  chart.menu,
   plugin.menu,
-  components.menu
+  components.menu,
+  chart.menu,
+  business.menu
 ]
 
-// 路由 这里的顺序决定了在顶栏显示的顺序
+// 路由
 export const router = [
+  business.router,
   chart.router,
   plugin.router,
-  components.router,
-  business.router
+  components.router
 ]
