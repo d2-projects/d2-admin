98157ee04cd71a5076d5a723ef5f3a5270affa06
李杨 <1711467488@qq.com>
2018-02-17 12:25:02 +0800

    no message
    Former-commit-id: 421ac77815742836c1b3a9f187ae15ff1f7f2582
    Former-commit-id: a811f5632c01cab93f430cc1631e9d6ea458621a
    Former-commit-id: a02431021c233a8ce569fb267bcb2188b007651f

---
+++ src/components/charts/G2/mixins/_ChartProps.js
@@ -0,0 +1,49 @@
+export default {
+  // 宽度 http://antv.alipay.com/zh-cn/g2/3.x/api/chart.html#_width
+  width: {
+    type: Number,
+    required: false,
+    default: 400
+  },
+  // 高度 http://antv.alipay.com/zh-cn/g2/3.x/api/chart.html#_height
+  height: {
+    type: Number,
+    required: false,
+    default: 300
+  },
+  // 设置图表的内边距 http://antv.alipay.com/zh-cn/g2/3.x/api/chart.html#_padding
+  padding: {
+    required: false,
+    default: () => [40, 40, 40, 40]
+  },
+  // 设置图表整体的边框和背景样式 http://antv.alipay.com/zh-cn/g2/3.x/api/chart.html#_background
+  background: {
+    type: Object,
+    required: false,
+    default: () => ({})
+  },
+  // 图表绘图区域的边框和背景样式 http://antv.alipay.com/zh-cn/g2/3.x/api/chart.html#_plotBackground
+  plotBackground: {
+    type: Object,
+    required: false,
+    default: () => ({})
+  },
+  // 图表的宽度自适应开关 http://antv.alipay.com/zh-cn/g2/3.x/api/chart.html#_forceFit
+  forceFit: {
+    type: Boolean,
+    required: false,
+    default: true
+  },
+  // 图表动画开关 http://antv.alipay.com/zh-cn/g2/3.x/api/chart.html#_animate
+  animate: {
+    type: Boolean,
+    required: false,
+    default: true
+  },
+  // 设置设备像素比  http://antv.alipay.com/zh-cn/g2/3.x/api/chart.html#_pixelRatio
+  pixelRatio: {
+    type: Number,
+    required: false,
+    default: window.devicePixelRatio
+  }
+}
