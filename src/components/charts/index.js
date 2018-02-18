e034df59b9bc5cbe0b8630dd8e1aa42b0043b637
李杨 <1711467488@qq.com>
2018-02-18 17:56:08 +0800

    no message

--- src/components/charts/index.js
+++ src/components/charts/index.js
@@ -3,3 +3,4 @@ import Vue from 'vue'
 Vue.component('G2LineBase', resolve => { require(['@/components/charts/G2/components/Line/base.vue'], resolve) })
 Vue.component('G2LineStep', resolve => { require(['@/components/charts/G2/components/Line/step.vue'], resolve) })
 Vue.component('G2ColumnBase', resolve => { require(['@/components/charts/G2/components/Column/base.vue'], resolve) })
+Vue.component('G2BarBase', resolve => { require(['@/components/charts/G2/components/Bar/base.vue'], resolve) })
