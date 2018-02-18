60a14b1dee600dd13832624a053cfa54c4aef552
李杨 <1711467488@qq.com>
2018-02-18 11:25:24 +0800

    no message

--- src/components/charts/index.js
+++ src/components/charts/index.js
@@ -1,3 +1,4 @@
 import Vue from 'vue'
 
 Vue.component('G2LineBase', resolve => { require(['@/components/charts/G2/components/Line/base.vue'], resolve) })
+Vue.component('G2LineStep', resolve => { require(['@/components/charts/G2/components/Line/step.vue'], resolve) })
--- src/mock/chart/G2Line.js
+++ src/mock/chart/G2Line.js
@@ -7,20 +7,13 @@ const r = (add = 0) => {
 Mock.mock('/api/chart/G2Line', 'post', ({body, type, url}) => {
   const _body = JSON.parse(body)
   switch (_body.code) {
-    case 1:
+    case 1: {
       let lastValue = 0
-      const temp = [
-        { year: '1991', value: 0 },
-        { year: '1992', value: 0 },
-        { year: '1993', value: 0 },
-        { year: '1994', value: 0 },
-        { year: '1995', value: 0 },
-        { year: '1996', value: 0 },
-        { year: '1997', value: 0 },
-        { year: '1998', value: 0 },
-        { year: '1999', value: 0 }
-      ]
-      const data = temp.map(e => {
+      const year = ['1991', '1992', '1993', '1994', '1995', '1996', '1997', '1998', '1999']
+      const data = year.map(y => ({
+        year: y,
+        value: 0
+      })).map(e => {
         e.value = lastValue + r()
         lastValue = e.value
         return e
@@ -31,6 +24,22 @@ Mock.mock('/api/chart/G2Line', 'post', ({body, type, url}) => {
         msg: '请求成功',
         data
       }
+    }
+    case 2: {
+      const month = ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月']
+      const data = month.map(y => ({
+        month: y,
+        value: 0
+      })).map(e => {
+        e.value = r()
+        return e
+      })
+      return {
+        code: 0,
+        msg: '请求成功',
+        data
+      }
+    }
     default:
       return {}
   }
