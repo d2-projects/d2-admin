639c99cfec6095416e5298ab0ea37be36b5ade36
李杨 <1711467488@qq.com>
2018-02-19 17:52:04 +0800

    no message

---
+++ src/pages/demo/chart/demo/PieBase.vue
@@ -0,0 +1,33 @@
+<template>
+  <Container type="ghost" :responsive="true">
+    <ChartDemoCard v-bind="card" @resize="$refs.chart.resize()">
+      <G2PieBase slot-scope="{data}" ref="chart" :data="data" v-bind="chart"></G2PieBase>
+    </ChartDemoCard>
+  </Container>
+</template>
+
+<script>
+import publicMixin from './mixins/public'
+export default {
+  mixins: [
+    publicMixin
+  ],
+  data () {
+    return {
+      // 给卡片容器的参数
+      card: {
+        api: {
+          url: '/api/chart/G2Pie',
+          data: {
+            type: 'base'
+          }
+        }
+      },
+      // 给图表的参数
+      chart: {
+        // 图表设置
+      }
+    }
+  }
+}
+</script>
---
+++ src/pages/demo/chart/demo/radarBase.vue
@@ -0,0 +1,33 @@
+<template>
+  <Container type="ghost" :responsive="true">
+    <ChartDemoCard v-bind="card" @resize="$refs.chart.resize()">
+      <G2RadarBase slot-scope="{data}" ref="chart" :data="data" v-bind="chart"></G2RadarBase>
+    </ChartDemoCard>
+  </Container>
+</template>
+
+<script>
+import publicMixin from './mixins/public'
+export default {
+  mixins: [
+    publicMixin
+  ],
+  data () {
+    return {
+      // 给卡片容器的参数
+      card: {
+        api: {
+          url: '/api/chart/G2Radar',
+          data: {
+            type: 'base'
+          }
+        }
+      },
+      // 给图表的参数
+      chart: {
+        // 图表设置
+      }
+    }
+  }
+}
+</script>
