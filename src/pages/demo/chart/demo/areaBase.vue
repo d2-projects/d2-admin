da38f442890dbbb66060ea9281505a93d9d601fa
李杨 <1711467488@qq.com>
2018-02-19 17:29:30 +0800

    no message

--- src/pages/demo/chart/demo/areaBase.vue
+++ src/pages/demo/chart/demo/areaBase.vue
@@ -1,9 +1,7 @@
 <template>
   <Container type="ghost" :responsive="true">
-    <ChartDemoCard v-bind="card">
-      <template slot-scope="{data}">
-        <G2AreaBase :data="data" v-bind="chart"></G2AreaBase>
-      </template>
+    <ChartDemoCard v-bind="card" @resize="$refs.chart.resize()">
+      <G2AreaBase slot-scope="{data}" ref="chart" :data="data" v-bind="chart"></G2AreaBase>
     </ChartDemoCard>
   </Container>
 </template>
