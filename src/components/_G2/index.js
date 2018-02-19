a4cbc12cd0ad06b1e9c8b3ef33e36f378b3a7889
李杨 <1711467488@qq.com>
2018-02-19 12:42:50 +0800

    no message

--- src/components/_G2/index.js
+++
@@ -1,5 +0,0 @@
-import Vue from 'vue'
-
-Vue.component('G2Line1', resolve => { require(['@/components/G2/charts/Line/1.vue'], resolve) })
-Vue.component('G2Pie1', resolve => { require(['@/components/G2/charts/Pie/1.vue'], resolve) })
-Vue.component('G2Column1', resolve => { require(['@/components/G2/charts/Column/1.vue'], resolve) })
