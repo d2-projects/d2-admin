cd2ba2e826aaef10778ed7e856c05a7d9e60a32f
李杨 <1711467488@qq.com>
2018-02-19 12:15:42 +0800

    no message
    Former-commit-id: de8aba5aa8b239e2abba42684b43d3d93bc0f761
    Former-commit-id: d2b379a92960a7a183040463a47e5fe551084f50
    Former-commit-id: 0b6c1177a037a967d0619678d68620673787e770

--- src/pages/demo/chart/index/index.vue
+++ src/pages/demo/chart/index/index.vue
@@ -50,6 +50,13 @@
           <G2RadarBase :ref="chart[6].refName" v-bind="chart[6]"></G2RadarBase>
         </el-card>
       </GridItem>
+      <!-- 卡片 -->
+      <GridItem v-bind="layout.layout[7]" @resized="handleResized(chart[7].refName)">
+        <el-card class="header-in">
+          <ChartCardHeader slot="header" @refresh="handleRefreshData(7)" title="近年行情"></ChartCardHeader>
+          <G2AreaBase :ref="chart[7].refName" v-bind="chart[7]"></G2AreaBase>
+        </el-card>
+      </GridItem>
     </GridLayout>
   </Container>
 </template>
@@ -100,6 +107,12 @@ export default {
           refName: 'G2RadarBase',
           data: [],
           padding: [30, 30, 30, 30]
+        },
+        {
+          api: {url: '/api/chart/G2Area', data: {type: 'base'}},
+          refName: 'G2AreaBase',
+          data: [],
+          padding: [30, 30, 30, 30]
         }
       ],
       layout: {
@@ -111,7 +124,8 @@ export default {
           {'x': 0, 'y': 7, 'w': 4, 'h': 7, 'i': '3'},
           {'x': 4, 'y': 7, 'w': 4, 'h': 7, 'i': '4'},
           {'x': 8, 'y': 7, 'w': 4, 'h': 7, 'i': '5'},
-          {'x': 0, 'y': 14, 'w': 4, 'h': 7, 'i': '6'}
+          {'x': 0, 'y': 14, 'w': 4, 'h': 7, 'i': '6'},
+          {'x': 4, 'y': 14, 'w': 4, 'h': 7, 'i': '7'}
         ],
         colNum: 12,
         rowHeight: 30,
