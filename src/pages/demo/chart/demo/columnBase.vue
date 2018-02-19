4facfeae39bea75029a04e455e737c75c3b89614
李杨 <1711467488@qq.com>
2018-02-20 07:33:12 +0800

    no message
    Former-commit-id: 27dd273629c60bbaaf2de1d4d8bdf66ac795fc19
    Former-commit-id: dffe1caa1455c69896b1cf0719f05b6d8c851739
    Former-commit-id: 0b599d698602c1707b9f60fde2db7068c8475f6d

--- src/pages/demo/chart/demo/columnBase.vue
+++ src/pages/demo/chart/demo/columnBase.vue
@@ -1,6 +1,6 @@
 <template>
   <Container type="ghost" :responsive="true">
-    <ChartDemoCard v-bind="card" @resize="$refs.chart.resize()">
+    <ChartDemoCard class="dd-mb" v-bind="card" @resize="$refs.chart.resize()">
       <G2ColumnBase slot-scope="{data}" ref="chart" :data="data" v-bind="chart"></G2ColumnBase>
     </ChartDemoCard>
     <el-card>
