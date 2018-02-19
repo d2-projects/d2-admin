f16c03b3b6590f3341c12043407b5db4372608e8
李杨 <1711467488@qq.com>
2018-02-19 13:32:10 +0800

    no message

--- src/pages/demo/chart/demo/areaBase.vue
+++ src/pages/demo/chart/demo/areaBase.vue
@@ -1,42 +1,5 @@
 <template>
   <Container type="ghost" :responsive="true">
-    <el-row :gutter="20">
-      <el-col :span="6">
-        <ChartDemoControl
-          @sync="syncData"
-          :height.sync="height">
-        </ChartDemoControl>
-      </el-col>
-      <el-col :span="18">
-        <el-card>
-          <div :style="style">
-            <G2AreaBase ref="chart" v-bind="chart"></G2AreaBase>
-          </div>
-        </el-card>
-      </el-col>
-    </el-row>
+    Hello
   </Container>
 </template>
-
-<script>
-import publicMixin from './mixins/public'
-export default {
-  mixins: [
-    publicMixin
-  ],
-  data () {
-    return {
-      api: {
-        url: '/api/chart/G2Area',
-        data: {
-          type: 'base'
-        }
-      },
-      chart: {
-        data: []
-      }
-    }
-  }
-}
-</script>
-
