0ea0723f94843d1bbbec1feb2ae4b5257841cd5c
李杨 <1711467488@qq.com>
2018-02-19 12:51:16 +0800

    no message

--- src/router/menu/_chart.js
+++ src/router/menu/_chart.js
@@ -11,9 +11,9 @@ export const menu = {
     name: 'demo-chart-index'
   },
   children: [
-    // 数据处理
+    // 所有
     {
-      title: '图表演示1',
+      title: '所有',
       icon: 'file-o',
       path: 'index',
       name: 'demo-chart-index',
@@ -21,6 +21,23 @@ export const menu = {
         requiresAuth: true
       },
       component: resolve => { require(['@/pages/demo/chart/index/index.vue'], resolve) }
+    },
+    // 单独演示
+    {
+      title: '单独演示',
+      icon: 'file-o',
+      children: [
+        {
+          title: '面积图',
+          icon: 'file-o',
+          path: 'demo/areaBase',
+          name: 'demo-chart-demo-areaBase',
+          meta: {
+            requiresAuth: true
+          },
+          component: resolve => { require(['@/pages/demo/chart/demo/areaBase.vue'], resolve) }
+        }
+      ]
     }
   ]
 }
