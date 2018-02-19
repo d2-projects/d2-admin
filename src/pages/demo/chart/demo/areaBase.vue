a8874d358954fa9c8cc5779b07ea2a0412d8fa79
李杨 <1711467488@qq.com>
2018-02-20 07:28:10 +0800

    no message
    Former-commit-id: b1ad0d407cb36a4757c947755a98fa22d913b049
    Former-commit-id: 8e9c3956fd122a3ae13b9126f3065afd580c55a7
    Former-commit-id: 0a33735542ff021c3adf81bcffb89a3b7e21d87c

--- src/pages/demo/chart/demo/areaBase.vue
+++ src/pages/demo/chart/demo/areaBase.vue
@@ -3,6 +3,7 @@
     <ChartDemoCard v-bind="card" @resize="$refs.chart.resize()">
       <G2AreaBase slot-scope="{data}" ref="chart" :data="data" v-bind="chart"></G2AreaBase>
     </ChartDemoCard>
+    <Markdown url="/static/markdownFiles/article/组件 - 图表.md"></Markdown>
   </Container>
 </template>
 
