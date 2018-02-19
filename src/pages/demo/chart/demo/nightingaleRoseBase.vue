ee736880993418292df75c8254f348fee7daa732
李杨 <1711467488@qq.com>
2018-02-20 07:29:14 +0800

    no message
    Former-commit-id: dd86291f40d3b2b457c8e192563d7d60de27261a
    Former-commit-id: 9bed2360fd9e5447e9d33dc0e24365b01b2868e7
    Former-commit-id: d1a945fbfc266a05e630b83e62fb6efedecf9fca

--- src/pages/demo/chart/demo/nightingaleRoseBase.vue
+++ src/pages/demo/chart/demo/nightingaleRoseBase.vue
@@ -3,6 +3,7 @@
     <ChartDemoCard v-bind="card" @resize="$refs.chart.resize()">
       <G2NightingaleRoseBase slot-scope="{data}" ref="chart" :data="data" v-bind="chart"></G2NightingaleRoseBase>
     </ChartDemoCard>
+    <Markdown url="/static/markdownFiles/article/组件 - 图表.md"></Markdown>
   </Container>
 </template>
 
