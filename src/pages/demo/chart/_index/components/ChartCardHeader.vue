ba1db5ca34d3312e0d895a6e4919b449b9aef71f
李杨 <1711467488@qq.com>
2018-02-19 21:38:28 +0800

    no message

---
+++ src/pages/demo/chart/_index/components/ChartCardHeader.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    {{title}}
+    <span class="dd-fr">
+      <Icon
+        class="chart-card-header-icon"
+        :class="{active}"
+        name="refresh"
+        @click.native="handleRefresh">
+      </Icon>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: false,
+      default: ''
+    }
+  },
+  data () {
+    return {
+      active: false
+    }
+  },
+  methods: {
+    handleRefresh () {
+      this.active = true
+      setTimeout(() => {
+        this.active = false
+      }, 1000)
+      this.$emit('refresh')
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import '~@/assets/style/public.scss';
+.chart-card-header-icon {
+  color: $color-text-sub;
+  transition: all .3s;
+  &.active {
+    transform: rotate(360deg);
+  }
+}
+</style>
+
