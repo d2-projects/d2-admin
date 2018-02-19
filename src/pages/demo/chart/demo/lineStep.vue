47c3cff89cee8659dae9d1317cbbc9ab0427dad5
李杨 <1711467488@qq.com>
2018-02-20 07:29:02 +0800

    no message

--- src/pages/demo/chart/demo/lineStep.vue
+++ src/pages/demo/chart/demo/lineStep.vue
@@ -3,6 +3,7 @@
     <ChartDemoCard v-bind="card" @resize="$refs.chart.resize()">
       <G2LineStep slot-scope="{data}" ref="chart" :data="data" v-bind="chart"></G2LineStep>
     </ChartDemoCard>
+    <Markdown url="/static/markdownFiles/article/组件 - 图表.md"></Markdown>
   </Container>
 </template>
 
