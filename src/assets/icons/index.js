3f536e5a897102386dba94519cbdb8345a983e8f
李杨 <1711467488@qq.com>
2018-02-22 14:01:02 +0800

    no message
    Former-commit-id: 0655c38bd415006a9be551a7590de425a7587975
    Former-commit-id: d09fe413ff61f3d76fb7ab767bc1a2348ae6ba85
    Former-commit-id: 1392d2827f7549c22fa6d97ff5ddc5af23853a41

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -6,4 +6,4 @@ const requireAll = requireContext => requireContext.keys().map(requireContext)
 const req = require.context('./svg', false, /\.svg$/)
 const iconMap = requireAll(req)
 
-console.log(iconMap)
+console.log(iconMap.map(e => e.default.id))
--- src/pages/demo/components/icon/svg.vue
+++ src/pages/demo/components/icon/svg.vue
@@ -1,5 +1,9 @@
 <template>
   <Container>
     <IconSvg name="alarm"></IconSvg>
+    <IconSvg name="chart-col"></IconSvg>
+    <IconSvg name="iPhone-x"></IconSvg>
+    <IconSvg name="macbook-pro"></IconSvg>
+    <IconSvg name="pic"></IconSvg>
   </Container>
 </template>
