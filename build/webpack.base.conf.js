921f7e1cf6c7722e76fcb4f325f7df6c16e55867
李杨 <1711467488@qq.com>
2018-02-22 12:27:31 +0800

    no message
    Former-commit-id: ab58492ec6c0470cacbf53a126e8d72f9e0851de
    Former-commit-id: 24b73991d7101c6fa10226862f4877fe21d0d195
    Former-commit-id: 3d976787a217f0da541aada42ae9386ef2876c61

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -53,6 +53,14 @@ module.exports = {
         include: [resolve('src'), resolve('test')]
       },
       {
+        test: /\.svg$/,
+        loader: 'svg-sprite-loader',
+        include: [resolve('src/assets/icons')],
+        options: {
+          symbolId: 'icon-[name]'
+        }
+      },
+      {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
         loader: 'url-loader',
         options: {
---
+++ src/components/core/IconSvg/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <svg aria-hidden="true">
+    <use :xlink:href="iconName"></use>
+  </svg>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    iconName() {
+      return `#icon-${this.name}`
+    }
+  }
+}
+</script>
