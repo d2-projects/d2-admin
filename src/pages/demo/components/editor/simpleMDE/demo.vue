61a03540a9c17d0aaeb26f9ffe9a97b656384fa5
李杨 <1711467488@qq.com>
2018-02-23 13:22:52 +0800

    no message
    Former-commit-id: c7068d628c54259c552acad26b57ffa98070f0ea
    Former-commit-id: b568327d2b0e2e17bbc03e2572834d27618600cc
    Former-commit-id: 67f27c5a966bf109d15a933195a5e2fd78c57524

--- src/pages/demo/components/editor/simpleMDE/demo.vue
+++ src/pages/demo/components/editor/simpleMDE/demo.vue
@@ -1,12 +1,15 @@
 <template>
-  <Container>
-    <PageHeader
-      slot="header"
-      title="基本示例"
-      url="https://github.com/sparksuite/simplemde-markdown-editor">
-    </PageHeader>
-    <div style="margin-bottom: -16px;">
-      <SimpleMDE></SimpleMDE>
-    </div>
+  <Container type="ghost">
+    <el-card class="dd-mb">
+      <PageHeader
+        slot="header"
+        title="基本示例"
+        url="https://github.com/sparksuite/simplemde-markdown-editor">
+      </PageHeader>
+      <SimpleMDE style="margin-bottom: -16px;"></SimpleMDE>
+    </el-card>
+    <el-card>
+      <template slot="header">输出</template>
+    </el-card>
   </Container>
 </template>
