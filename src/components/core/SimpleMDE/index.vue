9f60c62beaf97e4cf82b83783ac0f8c09b3c434f
李杨 <1711467488@qq.com>
2018-02-23 12:15:28 +0800

    no message

--- src/components/core/SimpleMDE/index.vue
+++ src/components/core/SimpleMDE/index.vue
@@ -9,16 +9,20 @@ import SimpleMDE from 'simplemde'
 export default {
   data () {
     return {
+      // 编辑器实例
       mde: null
     }
   },
   mounted () {
+    // 初始化
     this.init()
   },
   destroyed () {
+    // 在组件销毁后销毁实例
     this.mde = null
   },
   methods: {
+    // 初始化
     init () {
       this.mde = new SimpleMDE({
         element: this.$refs.mde
