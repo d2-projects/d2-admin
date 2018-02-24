bb169b455b3a3afa5281ec59dba8c6f030860f51
李杨 <1711467488@qq.com>
2018-02-24 17:22:05 +0800

    no message

--- src/components/core/Markdown/plugin/baidupan.js
+++ src/components/core/Markdown/plugin/baidupan.js
@@ -1,5 +1,14 @@
 export default (quote) => {
-  return new Promise((resolve, reject) => {
-    resolve()
-  })
+  const _quote = quote.replace(/<[^<>]+>/g, '').trim()
+  const bdShareUrl = /^https:\/\/pan\.baidu\.com\/s\/[a-z0-9]+$/i
+  const bdShareUrlPwd = /^链接: https:\/\/pan\.baidu\.com\/s\/[a-z0-9]+ 密码: [a-z0-9]{4}$/i
+  if (bdShareUrl.test(_quote)) {
+    return `<div style="color: red;">${_quote}</div>`
+  } else if (bdShareUrlPwd.test(_quote)) {
+    const url = _quote.match(/https:\/\/pan\.baidu\.com\/s\/[a-z0-9]+/i)
+    const pwd = _quote.match(/[a-z0-9]{4}$/i)
+    return `<div style="color: blue;">${url[0]} - ${pwd[0]}</div>`
+  } else {
+    return false
+  }
 }
