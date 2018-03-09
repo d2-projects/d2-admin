865cb0fe60782395517b0ae8f45ca28b6c1cc416
李杨 <1711467488@qq.com>
2018-03-09 09:35:07 +0800

    no message

--- src/components/core/MainLayout/_headerRight.vue
+++ src/components/core/MainLayout/_headerRight.vue
@@ -1,18 +1,13 @@
 <template>
   <div class="btn-group">
-    <el-tooltip
-      class="item"
-      effect="dark"
-      :content="isFullScreen ? '退出全屏' : '全屏'"
-      placement="bottom">
-      <el-button
-        class="dd-mr btn-text"
-        type="text"
-        @click="toggleFullScreen">
+    <!-- 全屏按钮 -->
+    <el-tooltip class="item" effect="dark" :content="isFullScreen ? '退出全屏' : '全屏'" placement="bottom">
+      <el-button class="dd-mr btn-text" type="text" @click="toggleFullScreen">
         <Icon v-if="isFullScreen" name="compress"></Icon>
         <Icon v-else name="arrows-alt"></Icon>
       </el-button>
     </el-tooltip>
+    <!-- 用户菜单 -->
     <el-dropdown class="dd-mr">
       <span class="el-dropdown-link">你好</span>
       <el-dropdown-menu slot="dropdown">
