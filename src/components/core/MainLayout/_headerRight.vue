0b39efe7f9563205d8bc3e83013f8e0ee8e3ed3b
李杨 <1711467488@qq.com>
2018-03-09 09:48:35 +0800

    no message
    Former-commit-id: 6c18c2d95f3775a5d19cd809b65ed674dd02f37f
    Former-commit-id: b670c342883001844720f9e29079f1ce97dccd54
    Former-commit-id: 72d3f18bfe8255dde992f3ac091a6ef966486788

--- src/components/core/MainLayout/_headerRight.vue
+++ src/components/core/MainLayout/_headerRight.vue
@@ -11,8 +11,8 @@
     <el-dropdown class="dd-mr">
       <span class="el-dropdown-link">你好</span>
       <el-dropdown-menu slot="dropdown">
-        <el-dropdown-item>注销</el-dropdown-item>
-        <el-dropdown-item>个人中心</el-dropdown-item>
+        <el-dropdown-item><Icon name="power-off"></Icon> 注销</el-dropdown-item>
+        <el-dropdown-item><Icon name="user-circle-o"></Icon> 个人中心</el-dropdown-item>
       </el-dropdown-menu>
     </el-dropdown>
   </div>
