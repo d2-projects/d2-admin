44681c3aaea86988f95a36a32c5cf7015ab9aab5
李杨 <1711467488@qq.com>
2018-03-10 23:19:08 +0800

    no message

---
+++ src/components/core/MainLayout/components/HeaderRight/components/UserDropdown.vue
@@ -0,0 +1,9 @@
+<template>
+  <el-dropdown class="dd-mr">
+    <span class="el-dropdown-link">你好</span>
+    <el-dropdown-menu slot="dropdown">
+      <el-dropdown-item><Icon name="power-off"></Icon> 注销</el-dropdown-item>
+      <el-dropdown-item><Icon name="user-circle-o"></Icon> 个人中心</el-dropdown-item>
+    </el-dropdown-menu>
+  </el-dropdown>
+</template>
