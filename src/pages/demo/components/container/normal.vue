d20d6ce01da3f492628011fef366e4cd06a884f3
李杨 <1711467488@qq.com>
2018-03-10 23:23:13 +0800

    no message

---
+++ src/pages/demo/components/container/normal.vue
@@ -0,0 +1,8 @@
+<template>
+  <Container>
+    <template slot="header">我是插入到 header 中的内容</template>
+    <p style="margin-top: 0px;">这是一个最基础的页面容器</p>
+    <p>它看起来像一个卡片</p>
+    <p style="margin-bottom: 0px;">它会将页面区域和导航区域区分开</p>
+  </Container>
+</template>
