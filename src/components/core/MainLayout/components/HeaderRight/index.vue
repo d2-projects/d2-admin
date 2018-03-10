826d0d9009a576c10e7bc34f792826a8bdb2460b
李杨 <1711467488@qq.com>
2018-03-10 23:19:08 +0800

    no message
    Former-commit-id: aadd2360c153062301cb92f6269eaf59e1c3eb52
    Former-commit-id: ea5afa42e2ad3c9f4d586edb82086d09a063caa0
    Former-commit-id: 2b624d94c8ddcd459585199a293593302f5efc45

--- src/components/core/MainLayout/components/HeaderRight/index.vue
+++ src/components/core/MainLayout/components/HeaderRight/index.vue
@@ -3,20 +3,15 @@
     <!-- 全屏按钮 -->
     <FullScreen></FullScreen>
     <!-- 用户菜单 -->
-    <el-dropdown class="dd-mr">
-      <span class="el-dropdown-link">你好</span>
-      <el-dropdown-menu slot="dropdown">
-        <el-dropdown-item><Icon name="power-off"></Icon> 注销</el-dropdown-item>
-        <el-dropdown-item><Icon name="user-circle-o"></Icon> 个人中心</el-dropdown-item>
-      </el-dropdown-menu>
-    </el-dropdown>
+    <UserDropdown></UserDropdown>
   </div>
 </template>
 
 <script>
 export default {
   components: {
-    FullScreen: () => import('./components/FullScreen.vue')
+    FullScreen: () => import('./components/FullScreen.vue'),
+    UserDropdown: () => import('./components/UserDropdown.vue')
   }
 }
 </script>
