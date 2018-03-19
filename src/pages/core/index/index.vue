378c2efe2e23cd019f5ac541a5dfb143c5b78be8
李杨 <1711467488@qq.com>
2018-03-19 22:13:56 +0800

    no message
    Former-commit-id: 04f8eca9d1bc1ca135ed186160aeab89af011366
    Former-commit-id: f4ce1b4dba1cb7a2d4e25d97e51c5461c73a5ec5
    Former-commit-id: 283e2ff96723e7830022461f34a7c8d85fdfce0e

--- src/pages/core/index/index.vue
+++ src/pages/core/index/index.vue
@@ -1,8 +1,55 @@
 <template>
   <Container type="card-full">
     <template slot="header">
-      我是插入到 header 中的内容
+      <div class="page-index-header">
+        <div class="dd-fl">
+          <span class="page-index-header__title">
+            Welcome
+          </span>
+        </div>
+        <div class="dd-fr">
+          <el-button>
+            <Icon name="github"></Icon>
+            Github
+          </el-button>
+        </div>
+      </div>
     </template>
-    <Markdown url="/static/markdownFiles/article/演示文章 - 长页面.md"></Markdown>
+    <div class="page-index-body">
+      <img class="page-index-body__logo" src="@/assets/image/logo/w500.png">
+      <p class="page-index-body__title">欢迎你，朋友。这是D2Admin</p>
+      <p class="page-index-body__sub-title">一个追求简洁和上手即用的前端模板</p>
+    </div>
   </Container>
 </template>
+
+<style lang="scss" scoped>
+@import '~@/assets/style/public.scss';
+.page-index-header {
+  overflow: hidden;
+  .page-index-header__title {
+    display: inline-block;
+    padding: 12px 20px;
+  }
+}
+.page-index-body {
+  position: absolute;
+  top: 0px;
+  bottom: 0px;
+  left: 0px;
+  right: 0px;
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  align-items: center;
+  .page-index-body__logo {
+    width: 200px;
+  }
+  .page-index-body__title {
+    color: $color-text-main;
+  }
+  .page-index-body__sub-title {
+    color: $color-text-sub;
+  }
+}
+</style>
