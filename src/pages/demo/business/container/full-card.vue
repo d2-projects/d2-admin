ce21a0c3d2756b992e85c5cfadddd10952b4da17
李杨 <1711467488@qq.com>
2018-03-21 21:48:34 +0800

    no message

--- src/pages/demo/business/container/full-card.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <Container type="card-full">
-    <template slot="header">
-      我是插入到 header 中的内容
-    </template>
-    <Markdown url="/static/md/演示文章 - 长页面.md"></Markdown>
-  </Container>
-</template>
--- src/pages/demo/business/container/ghost.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <Container type="ghost">
-    <p style="margin-top: 0px;">这是一个隐形的页面容器</p>
-    <p>没有边界和背景颜色</p>
-    <p style="margin-bottom: 0px;">适合一些比较特殊的页面</p>
-  </Container>
-</template>
