ec8bebfa46db2164801dc6152c7f0ccb87afc623
李杨 <1711467488@qq.com>
2018-03-21 22:44:07 +0800

    业务代码首页图标
    Former-commit-id: 7713ef1bda0e155542d921abd2a01388b8df9139
    Former-commit-id: 5bb5fa21857ec28f146da28aa70cbf5d2ee3b156
    Former-commit-id: 81f9ed9a68d05aa8f921528105f53665a7069c11

--- src/pages/demo/business/index/index.vue
+++ src/pages/demo/business/index/index.vue
@@ -3,7 +3,7 @@
     <PageIndexArticle
       title="示例业务页面"
       sub-title="如果用不到 建议删除相关代码">
-      <img src="@/assets/image/logo/w500-chart.png">
+      <img src="@/assets/image/logo/w500-business.png">
     </PageIndexArticle>
   </Container>
 </template>
