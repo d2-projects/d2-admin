8a824c489eac1c5a51c728a95949d310678edf4b
李杨 <1711467488@qq.com>
2018-03-22 22:55:44 +0800

    no message
    Former-commit-id: b08f2f022502445edd24b391be4b7b0ae1d900aa
    Former-commit-id: 89d810265094e2bb82b674926fe2cfac04d4d4d1
    Former-commit-id: d4435c98953e34e44c05802de1f77e9a4acbfe5e

--- src/pages/demo/components/container/card-full.vue
+++ src/pages/demo/components/container/card-full.vue
@@ -4,5 +4,8 @@
       我是插入到 header 中的内容
     </template>
     <Markdown url="/static/md/演示文章 - 长页面.md"></Markdown>
+    <template slot="footer">
+      我是插入到 footer 中的内容
+    </template>
   </Container>
 </template>
