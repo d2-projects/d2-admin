bb5b65b94dd69c7e08c8ee1d540c2b5274d15d7f
李杨 <1711467488@qq.com>
2018-03-22 22:36:03 +0800

    no message

--- src/pages/demo/business/user-center/home/index.vue
+++ src/pages/demo/business/user-center/home/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <Container>
-    Container
+  <Container type="card-full">
+    <template slot="header">
+      我是插入到 header 中的内容
+    </template>
+    <Markdown url="/static/md/演示文章 - 长页面.md"></Markdown>
   </Container>
 </template>
