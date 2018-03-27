42b0fd3b6ef30954ca679ea0d6f9aca5c7ab1902
李杨 <1711467488@qq.com>
2018-03-27 20:38:35 +0800

    no message

--- src/pages/demo/business/table/keyboard/keyboard-extend.js
+++ src/pages/demo/business/table/keyboard/keyboard-extend.js
@@ -1,22 +1,22 @@
+import sleep from '@/utils/sleep.js'
+
 export default {
   mounted () {
-    // 写法 1 测试有效
-    window.onload = () => {
-      console.log(this.$refs)
-      console.log('----')
-      for (let prop in this.$refs) {
-        console.log(this.$refs[prop])
-      }
-    }
+    this.keyboardExtentInit()
   },
   methods: {
     // 初始化
-    keyboardExtentInit () {
-      //
+    async keyboardExtentInit () {
+      await sleep(1000)
+      for (const propName in this.$refs) {
+        if (this.$refs.hasOwnProperty(propName)) {
+          console.log(propName, this.$refs[propName])
+        }
+      }
     },
     // 返回ref名称
     keyboardExtentRefNameMaker (scope) {
-      return `${scope.$index}${scope.column.property}`
+      return `kb-${scope.$index}-${scope.column.property}-kb`
     }
   }
 }
