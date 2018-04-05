2f39feeff2cababab1896a1d1b57e2e765fbeae8
李杨 <1711467488@qq.com>
2018-04-05 09:25:30 +0800

    no message

---
+++ src/pages/demo/plugins/build/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <Container type="card-full">
+    <template slot="header">
+      多环境发布 测试
+    </template>
+    <el-row>
+      <el-col :span="8">8</el-col>
+      <el-col :span="8">8</el-col>
+      <el-col :span="8">8</el-col>
+    </el-row>
+  </Container>
+</template>
