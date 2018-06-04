15f6c836489f40973cfb0ab6a8119a10ae4e0d51
liyang <1711467488@qq.com>
2018-06-04 13:21:31 +0800

    no message

--- src/components/core/MainLayout/components/SideMenu/components/ddMenu.vue
+++ src/components/core/MainLayout/components/SideMenu/components/ddMenu.vue
@@ -1,24 +1,22 @@
 <template>
   <div>
-    <!-- 没有子元素 -->
     <el-menu-item v-if="menu.children === undefined" :index="menu.path">
       <i :class="`fa fa-${menu.icon}`"></i>
       <span slot="title">{{menu.title}}</span>
     </el-menu-item>
-    <!-- 有子元素 -->
     <el-submenu v-else :index="menu.path">
       <template slot="title">
         <i :class="`fa fa-${menu.icon}`"></i>
         <span slot="title">{{menu.title}}</span>
       </template>
-      <dd-menus v-for="(child, childIndex) in menu.children" :menu="child" :key="childIndex"/>
+      <dd-menu v-for="(child, childIndex) in menu.children" :menu="child" :key="childIndex"/>
     </el-submenu>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'dd-menus',
+  name: 'dd-menu',
   props: {
     menu: {
       require: false,
