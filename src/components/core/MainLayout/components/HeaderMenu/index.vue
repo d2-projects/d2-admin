ee9128bb79253fcced28240b326c61f79d8f4b26
liyang <1711467488@qq.com>
2018-06-04 15:30:02 +0800

    no message

--- src/components/core/MainLayout/components/HeaderMenu/index.vue
+++ src/components/core/MainLayout/components/HeaderMenu/index.vue
@@ -2,6 +2,18 @@
   <el-menu
     mode="horizontal"
     :router="true">
+    <el-submenu index="2">
+      <template slot="title">我的工作台</template>
+      <el-menu-item index="2-1">选项1</el-menu-item>
+      <el-menu-item index="2-2">选项2</el-menu-item>
+      <el-menu-item index="2-3">选项3</el-menu-item>
+      <el-submenu index="2-4">
+        <template slot="title">选项4</template>
+        <el-menu-item index="2-4-1">选项1</el-menu-item>
+        <el-menu-item index="2-4-2">选项2</el-menu-item>
+        <el-menu-item index="2-4-3">选项3</el-menu-item>
+      </el-submenu>
+    </el-submenu>
     <el-menu-item
       v-for="menu in menus"
       :key="menu.path"
