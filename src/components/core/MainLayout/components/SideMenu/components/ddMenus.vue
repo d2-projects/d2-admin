d6391eb98b501da2855c0e73d0a865f4b927d804
liyang <1711467488@qq.com>
2018-06-04 13:01:38 +0800

    no message
    Former-commit-id: 3fb3864bd7dde71733cd6eb5306f7a0583fe924e
    Former-commit-id: 40dee5ed0a8182e9e6d29d8f2ef4003b61f29a80
    Former-commit-id: d3db5ce8a901006ee233a45afc4dfae5fca3cb5d

---
+++ src/components/core/MainLayout/components/SideMenu/components/ddMenus.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <el-menu-item v-if="menu.children === undefined" :index="menu.path">
+      <i :class="`fa fa-${menu.icon}`"></i>
+      <span slot="title">{{menu.title}}</span>
+    </el-menu-item>
+    <el-submenu v-else index="1">
+      <template slot="title">
+        <i :class="`fa fa-${menu.icon}`"></i>
+        <span slot="title">{{menu.title}}</span>
+      </template>
+      <el-menu-item :index="menu.path">
+        <i :class="`fa fa-${menu.icon}`"></i>
+        <span slot="title">{{menu.title}}</span>
+      </el-menu-item>
+    </el-submenu>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'menus',
+  props: {
+    menu: {
+      require: false,
+      default: () => []
+    }
+  }
+}
+</script>
