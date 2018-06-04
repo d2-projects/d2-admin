2242bc0cec414c37da3c3dcdb50b41d2eb9b121f
liyang <1711467488@qq.com>
2018-06-04 12:32:00 +0800

    no message

--- src/components/core/MainLayout/components/SideMenu/index.vue
+++ src/components/core/MainLayout/components/SideMenu/index.vue
@@ -3,6 +3,16 @@
     <el-menu
       :collapse="collapse"
       :unique-opened="true">
+      <template v-for="(menu, menuIndex) in menus">
+        <el-menu-item :key="menuIndex" index="2">
+          <i :class="`fa fa-${menu.icon}`"></i>
+          <span slot="title">导航二</span>
+        </el-menu-item>
+      </template>
+      <!-- <el-menu-item index="2">
+        <i class="el-icon-menu"></i>
+        <span slot="title">导航二</span>
+      </el-menu-item>
       <el-submenu index="1">
         <template slot="title">
           <i class="el-icon-location"></i>
@@ -21,14 +31,10 @@
           <el-menu-item index="1-4-1">选项1</el-menu-item>
         </el-submenu>
       </el-submenu>
-      <el-menu-item index="2">
-        <i class="el-icon-menu"></i>
-        <span slot="title">导航二</span>
-      </el-menu-item>
       <el-menu-item index="4">
         <i class="el-icon-setting"></i>
         <span slot="title">导航四</span>
-      </el-menu-item>
+      </el-menu-item> -->
     </el-menu>
     <!-- <div v-if="sideMenu.filter(e => e.title).length === 0 && !collapse" class="dd-side-menu-empty">
       没有菜单
@@ -43,6 +49,13 @@ export default {
       type: Boolean,
       required: false,
       default: false
+    }
+  },
+  data () {
+    return {
+      menus: [
+        {path: '/a', title: 'menu-a', icon: 'plus'}
+      ]
     }
   }
 }
