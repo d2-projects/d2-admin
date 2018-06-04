ff2d262d66d7125b62ce85245ea6a8ee33b0f45d
liyang <1711467488@qq.com>
2018-06-04 22:38:01 +0800

    no message
    Former-commit-id: bb428cfed5e1e6bc244675ee163157c83047c1db
    Former-commit-id: 1996f09d9327b429a2a88aa72cf08895b4e447df
    Former-commit-id: b90b1b7d11ba9895ab75a7e74e11f8fea72c7194

---
+++ src/components/core/MainLayout/components/SideMenu/components/ddMenuHaveChildren.vue
@@ -0,0 +1,28 @@
+<template>
+  <el-submenu :index="menu.path">
+    <template slot="title">
+      <i :class="`fa fa-${menu.icon}`"></i>
+      <span slot="title">{{menu.title}}</span>
+    </template>
+    <template v-for="(child, childIndex) in menu.children">
+      <ddMenuItem v-if="child.children === undefined" :menu="child" :key="childIndex"/>
+      <dd-menu-have-children v-else :menu="child" :key="childIndex"/>
+    </template>
+  </el-submenu>
+</template>
+
+<script>
+export default {
+  name: 'dd-menu-have-children',
+  props: {
+    menu: {
+      type: Object,
+      required: false,
+      default: () => {}
+    }
+  },
+  components: {
+    ddMenuItem: () => import('./ddMenuItem.vue')
+  }
+}
+</script>
