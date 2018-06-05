8e5661cb6c6c5d59eac42652629aec1d829d98ba
liyang <1711467488@qq.com>
2018-06-05 14:44:28 +0800

    no message
    Former-commit-id: 89e246965b501ba16c9b5e6b2703747ac976634f
    Former-commit-id: 3190b74deb83c355f8937f2b5b7d8b304a86c520
    Former-commit-id: b8a22c2943f6790e3aff5bbb7b7a97d28a5935ec

--- src/store/index.js
+++ src/store/index.js
@@ -3,12 +3,14 @@ import Vuex from 'vuex'
 
 import menu from './modules/menu'
 import fullScreen from './modules/fullScreen'
+import theme from './modules/theme'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
     menu,
-    fullScreen
+    fullScreen,
+    theme
   }
 })
