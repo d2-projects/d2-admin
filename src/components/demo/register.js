49c512e371ca467e46b88d7fa0bc4db10d80d1e0
liyang <1711467488@qq.com>
2018-06-10 16:38:19 +0800

    no message

--- src/components/demo/register.js
+++ src/components/demo/register.js
@@ -1,4 +1,4 @@
 import Vue from 'vue'
 
 Vue.component('d2-demo-page-header', () => import('@/components/demo/d2-demo-page-header'))
-Vue.component('PageIndexArticle', () => import('@/components/demo/PageIndexArticle'))
+Vue.component('d2-demo-page-cover', () => import('@/components/demo/d2-demo-page-cover'))
--- src/pages/demo/components/index/index.vue
+++ src/pages/demo/components/index/index.vue
@@ -1,9 +1,9 @@
 <template>
   <d2-container type="full">
-    <PageIndexArticle
+    <d2-demo-page-cover
       title="内置组件"
       sub-title="D2Admin 为你提供了一些上手即用的组件">
       <img src="@/assets/image/logo/w500-component.png">
-    </PageIndexArticle>
+    </d2-demo-page-cover>
   </d2-container>
 </template>
