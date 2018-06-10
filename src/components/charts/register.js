de8702afee3c02f06eb310c3c72242ae502dc92a
liyang <1711467488@qq.com>
2018-06-10 20:35:07 +0800

    no message
    Former-commit-id: 42030c47818c599f72dd59726268c942b278030b
    Former-commit-id: d63e272f36b254cc104de562f32dac625b55bbf5
    Former-commit-id: 796177ffeb4dd32973fa4769337e2d8de1efd0a8

--- src/components/charts/register.js
+++ src/components/charts/register.js
@@ -1,16 +1,16 @@
 import Vue from 'vue'
 
-Vue.component('G2AreaBase', resolve => { require(['@/components/charts/G2/components/Area/base.vue'], resolve) })
+Vue.component('G2AreaBase', () => import('@/components/charts/G2/components/Area/base.vue'))
 
-Vue.component('G2BarBase', resolve => { require(['@/components/charts/G2/components/Bar/base.vue'], resolve) })
+Vue.component('G2BarBase', () => import('@/components/charts/G2/components/Bar/base.vue'))
 
-Vue.component('G2ColumnBase', resolve => { require(['@/components/charts/G2/components/Column/base.vue'], resolve) })
+Vue.component('G2ColumnBase', () => import('@/components/charts/G2/components/Column/base.vue'))
 
-Vue.component('G2LineBase', resolve => { require(['@/components/charts/G2/components/Line/base.vue'], resolve) })
-Vue.component('G2LineStep', resolve => { require(['@/components/charts/G2/components/Line/step.vue'], resolve) })
+Vue.component('G2LineBase', () => import('@/components/charts/G2/components/Line/base.vue'))
+Vue.component('G2LineStep', () => import('@/components/charts/G2/components/Line/step.vue'))
 
-Vue.component('G2NightingaleRoseBase', resolve => { require(['@/components/charts/G2/components/NightingaleRose/base.vue'], resolve) })
+Vue.component('G2NightingaleRoseBase', () => import('@/components/charts/G2/components/NightingaleRose/base.vue'))
 
-Vue.component('G2PieBase', resolve => { require(['@/components/charts/G2/components/Pie/base.vue'], resolve) })
+Vue.component('G2PieBase', () => import('@/components/charts/G2/components/Pie/base.vue'))
 
-Vue.component('G2RadarBase', resolve => { require(['@/components/charts/G2/components/Radar/base.vue'], resolve) })
+Vue.component('G2RadarBase', () => import('@/components/charts/G2/components/Radar/base.vue'))
