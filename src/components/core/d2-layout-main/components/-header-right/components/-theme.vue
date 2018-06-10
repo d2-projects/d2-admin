3c863cca549c00b5b5c1056a8360a6a8cbe5e5bd
liyang <1711467488@qq.com>
2018-06-10 21:02:39 +0800

    no message

---
+++ src/components/core/d2-layout-main/components/-header-right/components/-theme.vue
@@ -0,0 +1,10 @@
+<template>
+  <el-dropdown class="d2-mr">
+    <span class="el-dropdown-link">主题</span>
+    <el-dropdown-menu slot="dropdown">
+      <el-dropdown-item>
+        <div style="height: 200px; width: 200px;"></div>
+      </el-dropdown-item>
+    </el-dropdown-menu>
+  </el-dropdown>
+</template>
--- src/components/core/d2-layout-main/components/-header-right/index.vue
+++ src/components/core/d2-layout-main/components/-header-right/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="d2-header-right">
     <d2-layout-main-header-right-full-screen/>
+    <d2-layout-main-header-right-theme/>
     <d2-layout-main-header-right-user/>
   </div>
 </template>
@@ -10,6 +11,7 @@ export default {
   name: 'd2-layout-main-header-right',
   components: {
     'd2-layout-main-header-right-full-screen': () => import('./components/-full-screen.vue'),
+    'd2-layout-main-header-right-theme': () => import('./components/-theme.vue'),
     'd2-layout-main-header-right-user': () => import('./components/-user.vue')
   }
 }
