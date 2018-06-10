94fa6b87d9bb1f475adcb6a5705c6068e611cdd7
liyang <1711467488@qq.com>
2018-06-10 16:38:19 +0800

    no message
    Former-commit-id: 50550f307865b163ffe2ec9c861809bb949fe1a4
    Former-commit-id: 2a71da590cbd2ff8e249111a46b854258fcce07d
    Former-commit-id: 134dceef9c31d71958a079bbddc15ae6feea9657

--- src/pages/core/index/index.vue
+++ src/pages/core/index/index.vue
@@ -1,9 +1,9 @@
 <template>
   <d2-container type="full">
-    <PageIndexArticle
+    <d2-demo-page-cover
       title="I AM D2ADMIN"
       sub-title="追求简约美感 & 上手即用的后台管理系统模板">
       <img src="@/assets/image/logo/w500.png">
-    </PageIndexArticle>
+    </d2-demo-page-cover>
   </d2-container>
 </template>
--- src/pages/demo/plugins/index/index.vue
+++ src/pages/demo/plugins/index/index.vue
@@ -1,9 +1,9 @@
 <template>
   <d2-container type="full">
-    <PageIndexArticle
+    <d2-demo-page-cover
       title="插件演示"
       sub-title="D2Admin 集成了许多实用插件">
       <img src="@/assets/image/logo/w500-plugin.png">
-    </PageIndexArticle>
+    </d2-demo-page-cover>
   </d2-container>
 </template>
