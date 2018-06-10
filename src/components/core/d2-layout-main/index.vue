55d04b7b813743f105efbc200e39a3656d14048e
liyang <1711467488@qq.com>
2018-06-10 09:04:25 +0800

    no message

--- src/components/core/d2-layout-main/index.vue
+++ src/components/core/d2-layout-main/index.vue
@@ -33,8 +33,8 @@ export default {
   name: 'd2-layout-main',
   components: {
     'd2-layout-main-side-menu': () => import('./components/side-menu'),
-    'd2-layout-main-header-menu': () => import('./components/header-menu'),
-    'd2-layout-main-header-right': () => import('./components/header-right')
+    'd2-layout-main-header-menu': () => import('./components/-header-menu'),
+    'd2-layout-main-header-right': () => import('./components/-header-right')
   },
   data () {
     return {
