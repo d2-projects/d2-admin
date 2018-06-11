e627fde02a6543217a7de3b68b57505729364242
liyang <1711467488@qq.com>
2018-06-11 19:14:53 +0800

    no message
    Former-commit-id: a838b6e7fee739930fd2b78ee1ad57515419b0c7
    Former-commit-id: dbd0bc699015ebe687c3418871e0828e2559a07a
    Former-commit-id: 6b3f23c332367bccf2eb9ffad136715105845085

--- src/pages/demo/components/container/ghost.vue
+++ src/pages/demo/components/container/ghost.vue
@@ -1,6 +1,6 @@
 <template>
   <d2-container type="ghost">
-    <el-card style="width: 50%;">
+    <el-card shadow="never" style="width: 50%;">
       <d2-markdown url="/static/md/组件 - 页面容器 - 隐形.md"/>
     </el-card>
   </d2-container>
