fc1b729294192716d2b52f00479cb06c9311b4b9
liyang <1711467488@qq.com>
2018-06-11 10:56:34 +0800

    no message
    Former-commit-id: f9b75cdafb5fad0b179a6c0f08cb879a7fd12dd3
    Former-commit-id: 3b126bcb4f5547a63cb75fe9f0a25c3b41fc56e6
    Former-commit-id: 5c161fbfdac589557a05f84585ea3def43480bce

--- src/components/core/d2-layout-main/components/-header-right/components/-theme.vue
+++ src/components/core/d2-layout-main/components/-header-right/components/-theme.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <el-tooltip effect="dark" content="theme" placement="bottom">
-      <el-button class="d2-ml-0 d2-mr btn-text" type="text" @click="dialogVisible = true">
+      <el-button class="d2-ml-0 d2-mr btn-text can-hover" type="text" @click="dialogVisible = true">
         <d2-icon name="diamond"/>
       </el-button>
     </el-tooltip>
