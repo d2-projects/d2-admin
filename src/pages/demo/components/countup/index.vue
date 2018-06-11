bb851712e0b63debfded040ee6d9944a98e8896f
liyang <1711467488@qq.com>
2018-06-11 14:23:56 +0800

    no message

--- src/pages/demo/components/countup/index.vue
+++ src/pages/demo/components/countup/index.vue
@@ -3,7 +3,7 @@
     <template slot="header">数字动画组件</template>
     <el-row :gutter="20">
       <el-col :span="6">
-        <el-card shadow="never">
+        <el-card shadow="never" class="d2-mb">
           <p slot="title">只设置目标数字</p>
           <div class="group">
             <d2-count-up :end="100"/>  
@@ -11,7 +11,7 @@
         </el-card>
       </el-col>
       <el-col :span="6">
-        <el-card shadow="never">
+        <el-card shadow="never" class="d2-mb">
           <p slot="title">设置起止数值</p>
           <div class="group">
             <d2-count-up :start="14" :end="100"/>
@@ -19,7 +19,7 @@
         </el-card>
       </el-col>
       <el-col :span="6">
-        <el-card shadow="never">
+        <el-card shadow="never" class="d2-mb">
           <p slot="title">小数位数</p>
           <div class="group">
             <d2-count-up :end="100" :decimals="2"/>
@@ -27,7 +27,7 @@
         </el-card>
       </el-col>
       <el-col :span="6">
-        <el-card shadow="never">
+        <el-card shadow="never" class="d2-mb">
           <p slot="title">动画时长</p>
           <div class="group">
             <d2-count-up :end="100" :duration="6"/>
@@ -43,7 +43,7 @@
         </el-card>
       </el-col>
       <el-col :span="6">
-        <el-card shadow="never">
+        <el-card shadow="never" class="d2-mb-0">
           <p slot="title">结束一秒后更新数值</p>
           <div class="group">
             <d2-count-up :end="end" :callback="update"/>
@@ -80,7 +80,6 @@ export default {
 .page {
   .el-card {
     @extend %unable-select;
-    margin-bottom: $margin;
     .group {
       display: flex;
       justify-content: center;
