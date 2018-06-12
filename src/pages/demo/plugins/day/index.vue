1386d61cee5567322128bbb31132516941f953c2
liyang <1711467488@qq.com>
2018-06-12 09:42:09 +0800

    no message
    Former-commit-id: 67884c24f35ac96b06aabef37e9fd375bccd47a8
    Former-commit-id: dd0f0a39432d722c87c7bda08b94868687cfa157
    Former-commit-id: b2db59c70e6f9b324cf2af00b0b5a7f895247459
    Former-commit-id: d569ecec1b2c264ba11b981941fbefea45ebd4fc

---
+++ src/pages/demo/plugins/day/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <d2-container type="full">
+    <template slot="header">日期计算</template>
+    <template slot="footer">
+      <a href="https://github.com/iamkun/dayjs">https://github.com/iamkun/dayjs</a>
+    </template>
+  </d2-container>
+</template>
+
+<script>
+import dayjs from 'dayjs'
+export default {
+}
+</script>
