e317202e85fb1ffbe7a6544a8ec601d438e5814e
liyang <1711467488@qq.com>
2018-06-12 09:26:32 +0800

    no message
    Former-commit-id: 43cda8a56782ad34aa47f18af671392015c96a97
    Former-commit-id: a4f093cad7533863e8162398e231eb98c9f8c697
    Former-commit-id: f574f1a366d1557c37f8b3a8cb1ba714dee6cff1
    Former-commit-id: 5f5b5f3c4617aa115bcb2d738bfc76aeff787853

--- src/pages/demo/business/user-center/home/index.vue
+++ src/pages/demo/business/user-center/home/index.vue
@@ -1,8 +1,6 @@
 <template>
   <d2-container type="full">
-    <template slot="header">
-      用户中心
-    </template>
+    <template slot="header">用户中心</template>
     doing...
   </d2-container>
 </template>
