f0747a53321f6b9d50d1db0eea2d3efb4a937fee
liyang <1711467488@qq.com>
2018-06-12 11:10:21 +0800

    no message
    Former-commit-id: 8a731ca8028435e0ee780e4fa4d322a33f7f8c43
    Former-commit-id: 200c1aaeb74abe28b6192ae9ee705bb471c39c23
    Former-commit-id: fd4adc33b65c76c8504e66c4ac6c29394b5ad558
    Former-commit-id: 535ae899ab441c76fc0011ec52f4342b05e3e4bd

---
+++ src/components/demo/d2-demo-link-btn/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <el-button-group>
+    <el-button size="mini">依赖</el-button>
+    <el-button size="mini" @click="$open(link)">
+      <d2-icon name="link"/>
+      {{link}}
+    </el-button>
+  </el-button-group>
+</template>
+
+<script>
+export default {
+  name: 'd2-demo-link-btn',
+  props: {
+    link: {
+      type: String,
+      required: false,
+      default: 'https://github.com/FairyEver'
+    }
+  }
+}
+</script>
--- src/pages/demo/plugins/day/index.vue
+++ src/pages/demo/plugins/day/index.vue
@@ -2,13 +2,7 @@
   <d2-container type="full">
     <template slot="header">日期计算</template>
     <template slot="footer">
-      <el-button-group>
-        <el-button size="mini">依赖</el-button>
-        <el-button size="mini" @click="$open(link)">
-          <d2-icon name="link"/>
-          {{link}}
-        </el-button>
-      </el-button-group>
+      <d2-demo-link-btn :link="link"/>
     </template>
   </d2-container>
 </template>
