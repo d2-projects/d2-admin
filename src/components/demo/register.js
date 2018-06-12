539013041b94df53ac464500363746cbcc997488
liyang <1711467488@qq.com>
2018-06-12 11:10:21 +0800

    no message

--- src/components/demo/register.js
+++ src/components/demo/register.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
 
+Vue.component('d2-demo-link-btn', () => import('@/components/demo/d2-demo-link-btn'))
 Vue.component('d2-demo-page-header', () => import('@/components/demo/d2-demo-page-header'))
 Vue.component('d2-demo-page-cover', () => import('@/components/demo/d2-demo-page-cover'))
