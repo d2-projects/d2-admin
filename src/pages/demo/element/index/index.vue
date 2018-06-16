5ea334f961b74cfba5e0b0b96b23221569c4edd2
liyang <1711467488@qq.com>
2018-06-17 07:56:49 +0800

    no message
    Former-commit-id: 332061d0682849a971ab24b7769f64dad1277746 [formerly 332061d0682849a971ab24b7769f64dad1277746 [formerly 332061d0682849a971ab24b7769f64dad1277746 [formerly 332061d0682849a971ab24b7769f64dad1277746 [formerly fb22d13e499e20b4eacb47914e2190bade1641b5 [formerly ebfd4386a3b42dbdaa33f852bc6671b7c72faf0f]]]]]
    Former-commit-id: 97999aaef119421f988fe1b7c615540702d65f74
    Former-commit-id: 883a38e78e5dfcde10fd6b036bdace74c9cd14eb
    Former-commit-id: 22acd06bcd8c6271f71188cf175c28a46165fd57 [formerly 88808f5eb3e8b5ae59319d3e07532a10ed3fa2df]
    Former-commit-id: 68c9166cde9e071583b8d2927690b8fa792adcff
    Former-commit-id: d1a249e2b65a1898ecddc6f4f140377a253cd18f
    Former-commit-id: d59b3f7d9bb74da74ff6f2f963df3929f9f93efe
    Former-commit-id: f6e729daecb4f49b5249a57d1b6a1e24e86488b8
    Former-commit-id: f3a754767f65935428a7c90b8e55fc6959548743

--- src/pages/demo/element/index/index.vue
+++ src/pages/demo/element/index/index.vue
@@ -1,9 +1,9 @@
 <template>
   <d2-container type="full">
     <d2-demo-page-cover
-      title="Element"
-      sub-title="D2Admin 集成了许多实用插件">
-      <img src="@/assets/image/logo/w500-plugin.png">
+      title="基础组件库"
+      sub-title="D2Admin 集成由饿了么出品的 ElementUI">
+      <img src="@/assets/image/logo/w500-element.png">
     </d2-demo-page-cover>
   </d2-container>
 </template>
