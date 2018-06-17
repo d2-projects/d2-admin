7fa19b599f61081d9668f7cb1eb2389817a50ad6
liyang <1711467488@qq.com>
2018-06-17 19:12:36 +0800

    no message
    Former-commit-id: 5c288bc63a74b39b847dfbb82f6e1e199dd2c462 [formerly 7612efeb07112998a4b7423d7fc8159ae559165b] [formerly 5c288bc63a74b39b847dfbb82f6e1e199dd2c462 [formerly 7612efeb07112998a4b7423d7fc8159ae559165b] [formerly 5c288bc63a74b39b847dfbb82f6e1e199dd2c462 [formerly 7612efeb07112998a4b7423d7fc8159ae559165b] [formerly 7612efeb07112998a4b7423d7fc8159ae559165b [formerly 5961bc32a761d873c0163f3891802393647d7145 [formerly b1932ef852390a7c523500fcfcb7db30d7450dda]]]]]
    Former-commit-id: 7b8f0ae5e9ffba301668fcf050ed597d1a724453
    Former-commit-id: 29ede6dc0a8515bb3749a85065561500da60a0ff
    Former-commit-id: 0f14969cb2ed4ec2db02e12d29168c982ab6a431 [formerly f1a6c6f7d45f6f5f41fbccc5bf10d5372f4e9286]
    Former-commit-id: bdcc6d127e4c9e967612648a7be771db4c327cc4
    Former-commit-id: fa28621a5111b0a83fcf8447649084a4e7c164ed
    Former-commit-id: d1d8318a1f1ac8a751902ab2ec0ca6ecca887bef
    Former-commit-id: d079dc6c22106b846e65cb401e84f61ee0bdcd2e
    Former-commit-id: 0d738c6ff6e52e9f35f8305bc446e2191d20b99d

--- src/pages/demo/element/basic-color/components/info.vue
+++ src/pages/demo/element/basic-color/components/info.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="info" :class="theme">
-    <p>{{name}}</p>
-    <p>{{sass}}</p>
-    <p>{{color}}</p>
+    <p class="name">{{name}}</p>
+    <p class="sass">{{sass}}</p>
+    <p class="color">{{color}}</p>
   </div>
 </template>
 
@@ -35,6 +35,20 @@ export default {
 
 <style lang="scss" scoped>
 .info {
+  padding: 10px;
+  p {
+    margin: 0px;
+  }
+  .name {
+    font-size: 14px;
+    font-weight: bold;
+  }
+  .sass {
+    font-size: 12px;
+  }
+  .color {
+    font-size: 12px;
+  }
   &.dark {
     color: #333;
   }
