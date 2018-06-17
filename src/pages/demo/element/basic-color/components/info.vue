184bceba51f1496cc6840e3e1cddcad61b427ac9
liyang <1711467488@qq.com>
2018-06-17 16:33:40 +0800

    no message

--- src/pages/demo/element/basic-color/components/info.vue
+++ src/pages/demo/element/basic-color/components/info.vue
@@ -1,5 +1,27 @@
 <template>
-  <div>
-    <p>颜色</p>
+  <div class="info" :class="theme">
+    <p>颜色名称</p>
+    <p>scss名称</p>
+    <p>颜色值</p>
   </div>
 </template>
+
+<script>
+export default {
+  props: {
+    theme: {
+      type: String,
+      required: false,
+      default: 'light' // dark
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.info {
+  &.light {
+    color: #FFF;
+  }
+}
+</style>
