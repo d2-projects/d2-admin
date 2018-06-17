032da0eba61962d0c68a316a2291e88c372baadf
liyang <1711467488@qq.com>
2018-06-17 23:49:19 +0800

    no message

--- src/pages/demo/element/basic-typography/index.vue
+++ src/pages/demo/element/basic-typography/index.vue
@@ -1,7 +1,10 @@
 <template>
   <d2-container type="ghost">
-    <el-card shadow="never">
+    <el-card shadow="never" class="d2-mb">
       <template slot="header">中文字体</template>
+    </el-card>
+    <el-card shadow="never">
+      <template slot="header">英文／数字字体</template>
     </el-card>
   </d2-container>
 </template>
