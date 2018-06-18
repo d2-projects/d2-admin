2784d44bcff636f7ab435a55e626ad431694276b
liyang <1711467488@qq.com>
2018-06-18 21:25:40 +0800

    no message
    Former-commit-id: 8c644e33fbf3101fa1889159301c2dfbddc4ec69 [formerly 8c644e33fbf3101fa1889159301c2dfbddc4ec69 [formerly 8c644e33fbf3101fa1889159301c2dfbddc4ec69 [formerly 8c644e33fbf3101fa1889159301c2dfbddc4ec69 [formerly f97c2c6b6395362292adb7489f63e0b57f0ef22e [formerly aaf6a907ec48d7764beb1a60039ad95550ad36e1]]]]]
    Former-commit-id: 537d650c2b362d4aa4efcfaae7967c1d5b1d9755
    Former-commit-id: a423d1b49a8075760be5742df9601828a172a4db
    Former-commit-id: 06cb658c3dea9d95a3702d08a3138f0b4c3a1b2e [formerly 701cb43e476b6a851c1564c86d83e83889885477]
    Former-commit-id: b609e3e12d16355e27730a4713417aa24d191f26
    Former-commit-id: e94f8ebc9e163af9cd83bbd5efc4555aae9f985b
    Former-commit-id: 8d6638ae5425df9ae074db9ef79f5c84d308bfe6
    Former-commit-id: 25216c343095387880f1c6a3ce172507f410385c
    Former-commit-id: e56a79cb85bc2a4e8a24fd9e66bed31359be49f5

--- src/pages/demo/element/form-radio/index.vue
+++ src/pages/demo/element/form-radio/index.vue
@@ -20,7 +20,6 @@
           <el-radio-group v-model="radio">
             <el-radio label="1">备选项1</el-radio>
             <el-radio label="2">备选项2</el-radio>
-            <el-radio label="3">备选项3</el-radio>
           </el-radio-group>
         </el-row>
 
