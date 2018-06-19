64c97eab5666edc56c1b78fdb51409d9dd87ca25
liyang <1711467488@qq.com>
2018-06-19 10:43:41 +0800

    no message
    Former-commit-id: 942ed839bc09ec4d11a55be2f6fbe4f8d6df439d [formerly be692dd44006185b433dc3767d40aec7ff5f4f39] [formerly 942ed839bc09ec4d11a55be2f6fbe4f8d6df439d [formerly be692dd44006185b433dc3767d40aec7ff5f4f39] [formerly 942ed839bc09ec4d11a55be2f6fbe4f8d6df439d [formerly be692dd44006185b433dc3767d40aec7ff5f4f39] [formerly be692dd44006185b433dc3767d40aec7ff5f4f39 [formerly 651b779ff9927c242d4dd43e90ca2ce60089f9c1 [formerly 852767021079fceb79eea194a1777337d2b34974]]]]]
    Former-commit-id: 132da3693ac7721961ccc318e97622c0d9baf5f3
    Former-commit-id: 6680b4199592acd3095f61ee09a267c82f1c8e5b
    Former-commit-id: c3d172aa8a2dc5b5a0b35c4c237c0deb987e4ae6 [formerly 5cfbbc8cf0b28a96975b67fc3bc534d396551ea9]
    Former-commit-id: f98e25baab2c5ab7ffde0b69381d0530cc328a05
    Former-commit-id: 4158685fb6b6d7c2dbdfc3e0223bbd01dbde7a0b
    Former-commit-id: b39e352d626608c5be2f60ecdbe4594fc05b4ee6
    Former-commit-id: 290d0326107c3b9928d10113c96be9d425d4ed21
    Former-commit-id: 8fdbcf65d89798bab57cfbf3c303148fc198fc78

--- src/pages/demo/plugins/better-scroll/base.vue
+++ src/pages/demo/plugins/better-scroll/base.vue
@@ -1,5 +1,40 @@
 <template>
-  <d2-container>
-    base
+  <d2-container @mounted="handleContainerMounted">
+    <div ref="wrapper" class="demo-bs-wrapper">
+      <ul class="content">
+        <li v-for="n in 30" :key="n">n</li>
+      </ul>
+    </div>
   </d2-container>
 </template>
+
+<script>
+import BScroll from 'better-scroll'
+export default {
+  data () {
+    return {
+      BS: null
+    }
+  },
+  methods: {
+    handleContainerMounted () {
+      console.log(this.$refs.wrapper)
+      let wrapper = this.$refs.wrapper
+      this.BS = new BScroll(wrapper, {
+        mouseWheel: true,
+        scrollbar: {
+          fade: true
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.demo-bs-wrapper {
+  height: 200px;
+  position: relative;
+}
+</style>
+
