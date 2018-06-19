3df4ab39327e58522a17c066d2544e47aa150de5
liyang <1711467488@qq.com>
2018-06-19 21:11:25 +0800

    no message
    Former-commit-id: a9389664de0a7b1da1e9559ca334f732b6aa404e [formerly a9389664de0a7b1da1e9559ca334f732b6aa404e [formerly a9389664de0a7b1da1e9559ca334f732b6aa404e [formerly a9389664de0a7b1da1e9559ca334f732b6aa404e [formerly 517e9c8ced41d9feb5d9b44fae36bc7ce6eb13ca [formerly 782335f7696edf3e42bdbc5a84b6b744785d35af]]]]]
    Former-commit-id: 0a1072828d8e74ee106df1ac52b0c7c5210dddae
    Former-commit-id: def12db9bffc3fd8a77ac70dd651cac44d944c73
    Former-commit-id: 6a77d28659f6e30a104e37d487821348df31e900 [formerly 75f8c2f0085414fc1d6c1ff69238765673b6b45e]
    Former-commit-id: 4fd1160ad02dc2fca7b64545385c5a57374bfb95
    Former-commit-id: 3ea40d2dc3448f7ea46ce7886b28dbd7cf0106ce
    Former-commit-id: 7f056cef965ad55f91a3bb12e47b269914932ac1
    Former-commit-id: 833e70b80b6c39a5d6abeeb7dd7720d92e135c98
    Former-commit-id: e97b9a9d7c76aa63fff03e2043b730ddbf10856b

--- src/components/core/d2-layout-main/components/-theme/index.vue
+++ src/components/core/d2-layout-main/components/-theme/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <el-tooltip effect="dark" content="theme" placement="bottom">
+    <el-tooltip effect="dark" content="主题" placement="bottom">
       <el-button class="d2-ml-0 d2-mr btn-text can-hover" type="text" @click="dialogVisible = true">
         <d2-icon name="diamond"/>
       </el-button>
