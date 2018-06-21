4be919ac77b111ecbefbe6b625d5f7e89c1ea3f4
liyang <1711467488@qq.com>
2018-06-21 12:05:15 +0800

    no message

--- src/components/core/d2-layout-main/components/-menu-header/index.vue
+++ src/components/core/d2-layout-main/components/-menu-header/index.vue
@@ -1,7 +1,5 @@
 <template>
-  <el-menu
-    mode="horizontal"
-    :router="true">
+  <el-menu mode="horizontal" @select="handleMenuSelect">
     <template v-for="(menu, menuIndex) in menus">
       <d2-layout-main-menu-item v-if="menu.children === undefined" :menu="menu" :key="menuIndex"/>
       <d2-layout-main-menu-sub v-else :menu="menu" :key="menuIndex"/>
@@ -11,8 +9,12 @@
 
 <script>
 import menus from '@/menu/index.js'
+import menuMixin from '../mixin/menu'
 export default {
   name: 'd2-layout-main-menu-header',
+  mixins: [
+    menuMixin
+  ],
   components: {
     'd2-layout-main-menu-item': () => import('../-menu-item/index.vue'),
     'd2-layout-main-menu-sub': () => import('../-menu-sub/index.vue')
--- src/components/core/d2-layout-main/components/-menu-side/index.vue
+++ src/components/core/d2-layout-main/components/-menu-side/index.vue
@@ -3,7 +3,7 @@
     <el-menu
       :collapse="collapse"
       :unique-opened="true"
-      :router="true">
+      @select="handleMenuSelect">
       <template v-for="(menu, menuIndex) in menus">
         <d2-layout-main-menu-item v-if="menu.children === undefined" :menu="menu" :key="menuIndex"/>
         <d2-layout-main-menu-sub v-else :menu="menu" :key="menuIndex"/>
@@ -18,8 +18,12 @@
 
 <script>
 import { side } from '@/menu/index.js'
+import menuMixin from '../mixin/menu'
 export default {
   name: 'd2-layout-main-menu-side',
+  mixins: [
+    menuMixin
+  ],
   props: {
     collapse: {
       type: Boolean,
