737a948ac8aaa9e94436cb66d8ec1877a6abea81
liyang <1711467488@qq.com>
2018-06-22 01:25:13 +0800

    no message

--- src/components/demo/d2-demo-page-cover/index.vue
+++ src/components/demo/d2-demo-page-cover/index.vue
@@ -8,7 +8,7 @@
     <a target="blank" href="https://github.com/FairyEver/d2-admin">
       <img
         style="position: absolute; top: 0; right: 0; border: 0;"
-        src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png"
+        src="http://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png"
         alt="Fork me on GitHub">
     </a>
   </div>
