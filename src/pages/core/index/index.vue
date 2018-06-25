8b92cb0b179b9f7c199a9e2cdda8123155ed01dd
liyang <1711467488@qq.com>
2018-06-25 23:20:44 +0800

    no message
    Former-commit-id: 2967a077555aba413b9eead536def7613353bac7 [formerly 2967a077555aba413b9eead536def7613353bac7 [formerly 2967a077555aba413b9eead536def7613353bac7 [formerly 2967a077555aba413b9eead536def7613353bac7 [formerly 641c8863a105922b01328d2f3afaa9b758ce67e4 [formerly 2c358ef16ded2432950f90d49a8a4602d0610f8e]]]]]
    Former-commit-id: 5aca62f8aabfd07c951520a14e9e933ad68c8b13
    Former-commit-id: a6a22fa8c0baedf78e3164657dff64b5e6b18683
    Former-commit-id: bff1ad19c2f1de4e68b4b22d980dbb900f267e3d [formerly 46e4f0df80fd2f702df83f9b5efd3f54db715a2e]
    Former-commit-id: 584aef86f7a919ba19c2a77c3cb714a26ca3a4d2
    Former-commit-id: ed59d04ef09ef3aedb7bee438feae1719a7f82e1
    Former-commit-id: 27fa88c304315435caa3a0ea6048ed5999194cc4
    Former-commit-id: cf1a722ecc502b86c92574562e55fca74fcf5882
    Former-commit-id: 596b7095f7bba129fa0e48b69740a7b3ce78dc0a

--- src/pages/core/index/index.vue
+++ src/pages/core/index/index.vue
@@ -3,7 +3,7 @@
     <d2-demo-page-cover
       title="I AM D2ADMIN"
       sub-title="追求简约美感 & 上手即用的后台管理系统模板">
-      <img src="@/assets/image/logo/w500.png">
+      <img :src="`${$assetsPublicPath}static/image/icon/500/d2admin.png`">
     </d2-demo-page-cover>
   </d2-container>
 </template>
