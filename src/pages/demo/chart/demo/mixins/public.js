a1894638217c379381a20f3a0758ba277a7cf13c
liyang <1711467488@qq.com>
2018-06-26 16:41:37 +0800

    no message
    Former-commit-id: 18a37ae1731f35f8be384568aa18ce1392272a16 [formerly 18a37ae1731f35f8be384568aa18ce1392272a16 [formerly 18a37ae1731f35f8be384568aa18ce1392272a16 [formerly 18a37ae1731f35f8be384568aa18ce1392272a16 [formerly ab6e10f4394a105b8df110f0290d2422417087de [formerly 4ef727613363e9aba78f198b85bb7017b78d47ec]]]]]
    Former-commit-id: b0463b3d445ad91caab1ee86a0301d0e996a0896
    Former-commit-id: 033e76751833de033ad8890bd9a3dc040bebe332
    Former-commit-id: 67fdc351750d8b1d4575cc2e99f01671d5867ed5 [formerly 6f9d726dca1cabc6c5faaecef2df3ba404b1f499]
    Former-commit-id: 94491db4627bd320616b71470113f2e00cca8ba9
    Former-commit-id: 4406af3d97e94fcf972333a7ba30d32356197b3d
    Former-commit-id: dcd1bbd431f1ccc0b5b9adc922272f41a13a8875
    Former-commit-id: a97855335176026ade5c5fc00b80f3f654f554fe
    Former-commit-id: 902424d3e3c19ce1fb474f9868ca18ce970b3c14

--- src/pages/demo/chart/demo/mixins/public.js
+++ src/pages/demo/chart/demo/mixins/public.js
@@ -1,5 +1,6 @@
+import d2DemoChartCard from '../components/d2-demo-chart-card.vue'
 export default {
   components: {
-    'd2-demo-chart-card': () => import('../components/d2-demo-chart-card.vue')
+    'd2-demo-chart-card': d2DemoChartCard
   }
 }
