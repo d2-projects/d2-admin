985a2c9f582fad68bcdb9c813a3aa37c87638aec
liyang <1711467488@qq.com>
2018-06-27 22:49:51 +0800

    no message
    Former-commit-id: bc5838e84123c8e42dc020ab35edcc161539c466 [formerly bc5838e84123c8e42dc020ab35edcc161539c466 [formerly bc5838e84123c8e42dc020ab35edcc161539c466 [formerly bc5838e84123c8e42dc020ab35edcc161539c466 [formerly be3a13d0a391738c3a0dd1e45a791b8301f429fa [formerly b1cec2c7ac9dd367b21db896c1c972762665983f]]]]]
    Former-commit-id: bcb6097dba45a01d088ce2257c35a8cf75fc8c17
    Former-commit-id: 487d0ba8cffee11510acee9fff5bba3ec862bc90
    Former-commit-id: 225048a8c74d57a9eb3282ba33f9bb0a36d280db [formerly bfdfa66147975fb61e4a4d9f7f0c2d076f981a56]
    Former-commit-id: 7031fbee0056d08fb42e27748c315cbd8211b4c7
    Former-commit-id: da476ff852f4ef7e13e3f6b5620d95f759434398
    Former-commit-id: 6c35d17e4a1d4a2aebe10cc4319ecbfc9ae4849a
    Former-commit-id: 6ab65e3ae28cbbc44668623a3a358135bdc2474c
    Former-commit-id: 61eeeeaa3b8a3f8106b5716253e1008ef601c23b

--- src/components/core/d2-layout-main/components/mixin/menu.js
+++ src/components/core/d2-layout-main/components/mixin/menu.js
@@ -4,7 +4,6 @@ export default {
       if (/^d2-menu-empty-\d+$/.test(index)) {
         this.$message('功能正在开发')
       } else {
-        console.log(index)
         this.$router.push(index)
       }
     }
