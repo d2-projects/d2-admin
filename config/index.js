926701ea701294671909310bbb278cd42f40b028
liyang <1711467488@qq.com>
2018-06-27 13:34:01 +0800

    no message
    Former-commit-id: 410b7caa772ac92996bb8d4fb5a0ed5be2328915 [formerly 410b7caa772ac92996bb8d4fb5a0ed5be2328915 [formerly 410b7caa772ac92996bb8d4fb5a0ed5be2328915 [formerly 410b7caa772ac92996bb8d4fb5a0ed5be2328915 [formerly 1d638668ef8eaa8bf67ec463ee68e86cd8337a99 [formerly 2e8abe05c7b4ded215634b22fe7b445d337d3372]]]]]
    Former-commit-id: 33bbfe4fcad6162aef45ab23ae5895bb7f5af00f
    Former-commit-id: b4c43693a8bc6ac105bafb5b38959c1ab3a74771
    Former-commit-id: f7da96f1e40f45f4870cffd9818341466fcca9a3 [formerly 85ad34dda93bd036e235b23eb7c57ec1d29c7323]
    Former-commit-id: e3e438a4789fd8a3573dc6a2fae43178e5c0a6ca
    Former-commit-id: 07783bbdd57d3b8fc1cfccd65ce8388e9dbdb72f
    Former-commit-id: 11423c305520b6e3705b3ab81c49dae31df7042c
    Former-commit-id: 6090cf03436f74b19e40f7f07d3ec8ba50e19e9b
    Former-commit-id: a1a36d1aa9a4dc2232cecefc78408730c6625afb

--- config/index.js
+++ config/index.js
@@ -15,7 +15,7 @@ module.exports = {
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
-    autoOpenBrowser: false,
+    autoOpenBrowser: true,
     errorOverlay: true,
     notifyOnErrors: true,
     poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
