7b6f034293db98f956a35316fab1243f40ce3836
liyang <1711467488@qq.com>
2018-06-29 20:26:13 +0800

    no message
    Former-commit-id: 64e0898e84251dcedfd503bbc700229c119b0c16 [formerly 64e0898e84251dcedfd503bbc700229c119b0c16 [formerly 64e0898e84251dcedfd503bbc700229c119b0c16 [formerly 64e0898e84251dcedfd503bbc700229c119b0c16 [formerly 901369805f22fe72761ad167708194919563ddcb [formerly 1fab8d2e1985ca694c9f061762f69f528ad467b7]]]]]
    Former-commit-id: 3c15fd01f56dbdba6d84d1263e0cf8359e550f67
    Former-commit-id: 1d7fea993a8f433272a1bf162829154a8a07601e
    Former-commit-id: 38d00da33db641f5eaa777d9c725fd7c9ccaa9fc [formerly ea291c01a74aab411dc59821b77025addede5b0c]
    Former-commit-id: 7e05af70b9a4414aebf3988a1227e42df90663f2
    Former-commit-id: dff282720abe3784d7d6ba50e30f739ebb6ecb92
    Former-commit-id: b4354767b43efb5ac7a275395078399b07decc70
    Former-commit-id: a97639f01dbb5736667c9a21d8b5450fa8dd2fe8
    Former-commit-id: 98d7a25ac1a11ada7dd3b362019328c58b7d3d81

--- src/pages/demo/playground/fullscreen/index.vue
+++ src/pages/demo/playground/fullscreen/index.vue
@@ -1,26 +1,27 @@
 <template>
-  <d2-container type="full" class="page-page-name">
-    page-name
+  <d2-container class="page-demo-playground-fullscreen">
+    <template slot="header">全屏</template>
+    <el-switch
+      v-model="isFullScreen"
+      active-text="全屏"
+      inactive-text="正常"
+      @change="toggleFullScreen">
+    </el-switch>
   </d2-container>
 </template>
 
 <script>
+import { mapState, mapMutations } from 'vuex'
 export default {
-  data () {
-    return {}
-  },
   computed: {
+    ...mapState({
+      isFullScreen: state => state.d2admin.isFullScreen
+    })
   },
   methods: {
+    ...mapMutations([
+      'toggleFullScreen'
+    ])
   }
 }
 </script>
-
-<style lang="scss" scoped>
-@import '~@/assets/style/public.scss';
-.page-page-name {
-  // Your page style here
-  // Do not use empty rulesets
-  // If empty, delete it
-}
-</style>
