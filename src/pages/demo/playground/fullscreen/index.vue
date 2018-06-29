f635d20662d934adcd300837d2974995fca24736
liyang <1711467488@qq.com>
2018-06-29 20:17:40 +0800

    no message
    Former-commit-id: 06daced7c248cec6c0cf3a29c8f73dd0363bf9be [formerly 06daced7c248cec6c0cf3a29c8f73dd0363bf9be [formerly 06daced7c248cec6c0cf3a29c8f73dd0363bf9be [formerly 06daced7c248cec6c0cf3a29c8f73dd0363bf9be [formerly 6148e48523bf8bbc9132eb6c38c2e923f0fa2477 [formerly 6908d49e421000cef9eea1546105dea66a5648ec]]]]]
    Former-commit-id: 3ef341cd419e3a9890ed1fd54b604b1906eea911
    Former-commit-id: 22e7832a7fbeddac9be66d944fe653ed5fe906c9
    Former-commit-id: 90fa42dc8ab58f964cd71c3e942b0fe0b580e490 [formerly 798dcb43b04373398a09630d10c173a60a35e546]
    Former-commit-id: d6119bf8372bddfc8f23ce74c18d6338fdbd0704
    Former-commit-id: 3eb6ecc8e30b00ecbef850b99de07ac90bc80173
    Former-commit-id: d328c68c0166704eb8fead74f139b2ef2802c24e
    Former-commit-id: f9e245445b3172a4da3d43a2675590e07bc321d0
    Former-commit-id: ba1f44510caded10d0f5984f80a131b4be8e9463

---
+++ src/pages/demo/playground/fullscreen/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <d2-container type="full" class="page-page-name">
+    page-name
+  </d2-container>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  },
+  computed: {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~@/assets/style/public.scss';
+.page-page-name {
+  // Your page style here
+  // Do not use empty rulesets
+  // If empty, delete it
+}
+</style>
