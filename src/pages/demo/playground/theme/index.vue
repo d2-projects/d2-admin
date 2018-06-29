ea5b91886dc5d8bbf462b2993902eebd172f3fa2
liyang <1711467488@qq.com>
2018-06-29 17:05:32 +0800

    no message

--- src/pages/demo/playground/theme/index.vue
+++ src/pages/demo/playground/theme/index.vue
@@ -1,5 +1,15 @@
 <template>
-  <d2-container type="full">
-    theme
+  <d2-container>
+    new page
   </d2-container>
 </template>
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
