7e590d4112a378eea0223a0937e70d461cee3021
liyang <1711467488@qq.com>
2018-06-29 09:54:28 +0800

    no message

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import d2admin from './modules/d2admin'
+
 import menu from './modules/menu'
 import fullScreen from './modules/fullScreen'
 import theme from './modules/theme'
@@ -9,6 +11,7 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
+    d2admin,
     menu,
     fullScreen,
     theme
