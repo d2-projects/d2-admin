27ab18faa002e0ac2b6dba36e165ba628480c316
liyang <1711467488@qq.com>
2018-06-30 19:29:01 +0800

    no message
    Former-commit-id: 5a98ab9fad1cd3487d5975a13be18e7e974ac943 [formerly 5a98ab9fad1cd3487d5975a13be18e7e974ac943 [formerly 5a98ab9fad1cd3487d5975a13be18e7e974ac943 [formerly 5a98ab9fad1cd3487d5975a13be18e7e974ac943 [formerly 435c4d936c2f8ab5eea278e4432a0df286286f01 [formerly f87cff38d27cea83e7a7fd974aff47bac7801bc7]]]]]
    Former-commit-id: 848af72c6b5e06521da331a7d35c2ef802d17a22
    Former-commit-id: ae82c772d169467c14849964521f203c819bc4cf
    Former-commit-id: 75e0c51e1a5a1720a40d48b2319f69fa0dcd9dfd [formerly 8732955133a2a8df5b4c4ed780161a27b8468dc8]
    Former-commit-id: e0404cd785b26d49dd4309fa182d921679401c17
    Former-commit-id: 8073756eb1b1431acc4f31798822e453304d6545
    Former-commit-id: 4c642306a6aaa3daf3f6cafaaae40129e879e6d4
    Former-commit-id: e18a58a22d64f6ec527909074a193716f163489b
    Former-commit-id: 9b9eeb009de56ee3405ac4be0eed2551f1ed06d8

---
+++ src/pages/demo/playground/page-cache/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <d2-container type="full">
+    page-name
+  </d2-container>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  },
+  computed: {
+  },
+  methods: {
+  }
+}
+</script>
