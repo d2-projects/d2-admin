b4c4b38fd8920beb70e49a2687e6c7ee7d493b94
liyang <1711467488@qq.com>
2018-07-02 14:02:08 +0800

    no message
    Former-commit-id: 42a881264ab44c352b5ef70b37f53bb63480bd69 [formerly e534589555cce5e01e68872b050e1f48c6b278c1] [formerly 42a881264ab44c352b5ef70b37f53bb63480bd69 [formerly e534589555cce5e01e68872b050e1f48c6b278c1] [formerly 42a881264ab44c352b5ef70b37f53bb63480bd69 [formerly e534589555cce5e01e68872b050e1f48c6b278c1] [formerly e534589555cce5e01e68872b050e1f48c6b278c1 [formerly 8fced8ea8f9f382e9b6ead0907cf723b8ae877d6 [formerly 766b963544bfa65ddc0d80570b60aed126d62cde]]]]]
    Former-commit-id: c05343ad2424249b6fde2b73c7bc245700fd8776
    Former-commit-id: d6b99760299556b9db31432862c317e380a0135e
    Former-commit-id: f3b86222cc37779ce61836ef21b267f63b832aee [formerly d682119da6ed042d37966b2f394e5b0f06af79ca]
    Former-commit-id: bafde65dfb7eba76eeb404da5d65f85178991ee0
    Former-commit-id: 893a3aed293356471ab422ff47495711769dd95c
    Former-commit-id: e8009974cd7a8a1f7382f4a3838f42eb6498fa77
    Former-commit-id: ab96ef07a9996511bd63b32e2da037830bd24526
    Former-commit-id: 644d90e0b96c81f8209e965b6fa847103594fa6e

--- src/pages/demo/components/container/ghost-header.vue
+++ src/pages/demo/components/container/ghost-header.vue
@@ -1,6 +1,6 @@
 <template>
   <d2-container type="ghost">
-    <template slot="header">header</template>
+    <template slot="header">隐形页面容器头部</template>
     <el-card shadow="never" style="width: 50%;">
       <d2-markdown :source="doc"/>
     </el-card>
@@ -8,7 +8,7 @@
 </template>
 
 <script>
-import doc from './md/doc-ghost.md'
+import doc from './md/doc-ghost-header.md'
 export default {
   data () {
     return {
