a40d811478ce61eadae20d2709a50fb314c2fa93
liyang <1711467488@qq.com>
2018-07-02 22:07:53 +0800

    no message
    Former-commit-id: 8a2320703fe374a1a6f168594029b5d059d2b658 [formerly 8a2320703fe374a1a6f168594029b5d059d2b658 [formerly 8a2320703fe374a1a6f168594029b5d059d2b658 [formerly 8a2320703fe374a1a6f168594029b5d059d2b658 [formerly 4968bd4e96b04a12d5f11e8c3e86b26d1e1c1d58 [formerly 9151b703c583b6571c0e42cadae57a83d81f8a28]]]]]
    Former-commit-id: 003174b17e25317300868dac16aa62200aa5ed0d
    Former-commit-id: 08c80a57cd50b0f5eed9540e3d34ed14bf3eb11c
    Former-commit-id: 78c612e01f2a491aee73e5b091a450f53583a68e [formerly 1ca65f2d6d05a6c68ab578a9e3d2dcdd700fa6e1]
    Former-commit-id: ea3fae48083d426e9ea188df31dacc9e7fa4aef9
    Former-commit-id: 3cdb05dc12819e42837432032bf3997781aa1ef0
    Former-commit-id: 257146cea365af132b7b58ca9cc7bb0b685570b9
    Former-commit-id: 475aaba1906e6071afc1dd8fa3d0e5242677917a
    Former-commit-id: 1668921032ba1a7fc9662bd2dc7f4f4923b3b95d

---
+++ src/pages/demo/playground/page-argu/get.vue
@@ -0,0 +1,14 @@
+<template>
+  <d2-container type="full">
+    <h1>$route.params.username: {{$route.params.username}}</h1>
+    <h1>$route.query: {{$route.query}}</h1>
+  </d2-container>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
---
+++ src/pages/demo/playground/page-argu/send.vue
@@ -0,0 +1,49 @@
+<template>
+  <d2-container type="full">
+    <el-form :model="sendForm" :rules="rules" label-position="top" ref="sendForm">
+      <el-form-item label="username 通过动态路由匹配发送" prop="username">
+        <el-input v-model="sendForm.username"></el-input>
+      </el-form-item>
+      <el-form-item label="userid 通过参数发送" prop="userid">
+        <el-input v-model="sendForm.userid"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="handleSubmit('sendForm')">跳转</el-button>
+      </el-form-item>
+    </el-form>
+  </d2-container>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      sendForm: {
+        username: 'FairyEver',
+        userid: '001'
+      },
+      rules: {
+        username: [
+          { required: true, message: '请输入活动名称', trigger: 'blur' },
+          { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+        ],
+        userid: [
+          { required: true, message: '请输入活动名称', trigger: 'blur' },
+          { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+        ]
+      }
+    };
+  },
+  methods: {
+    handleSubmit (formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          alert('submit!')
+        } else {
+          return false
+        }
+      })
+    }
+  }
+}
+</script>
