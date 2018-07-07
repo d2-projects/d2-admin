f78b7cf8d21992be57dd72dcfb90e75ad47281e5
liyang <1711467488@qq.com>
2018-07-07 08:35:30 +0800

    bug fixed
    Former-commit-id: cee2e84bf93a15902e054f7dcf9d423dd7a8cb7d [formerly a3e4670d8a128ddd2709d03a6ce386c06ccdb75e] [formerly cee2e84bf93a15902e054f7dcf9d423dd7a8cb7d [formerly a3e4670d8a128ddd2709d03a6ce386c06ccdb75e] [formerly cee2e84bf93a15902e054f7dcf9d423dd7a8cb7d [formerly a3e4670d8a128ddd2709d03a6ce386c06ccdb75e] [formerly a3e4670d8a128ddd2709d03a6ce386c06ccdb75e [formerly 010f1cfa84ee2bfe58b2127efd66ef7bfb3f1c52 [formerly 1f0832d66825cb9ec4426a43f4b62a7c9c6266ed]]]]]
    Former-commit-id: 6c447d20ab042c5dab8bfc2641be5bc282d6e99e
    Former-commit-id: aee978972a1c863b00fe22dfb1db645ebf04ae57
    Former-commit-id: 276be3dffedd92185ec302f0c36ce3cf9ac7ae36 [formerly 1b2771c8fb15db40dafb075d25ede92ee8ab50eb]
    Former-commit-id: 8afa5f03f430475749aae3d66014091a765e2333
    Former-commit-id: 3d3d7c9d15922416bd12d8c61be12dd114e3453a
    Former-commit-id: 9ba07ef04f76fb8e351803e91e45165145e3ba64
    Former-commit-id: 452c3282ee5fa9a704a2bcbff2eafd8b28abc582
    Former-commit-id: 09e7ebc77a8e1af4f5a1784971432c1953c8b3cb

--- src/libs/util.js
+++ src/libs/util.js
@@ -26,37 +26,6 @@ util.title = function (title) {
 }
 
 /**
- * @description 打开全屏
- */
-util.openFullScreen = function () {
-  const body = document.body
-  if (body.requestFullscreen) {
-    body.requestFullscreen()
-  } else if (body.mozRequestFullScreen) {
-    body.mozRequestFullScreen()
-  } else if (body.webkitRequestFullScreen) {
-    body.webkitRequestFullScreen()
-  } else if (body.msRequestFullscreen) {
-    body.msRequestFullscreen()
-  }
-}
-
-/**
- * @description 关闭全屏
- */
-util.exitFullScreen = function () {
-  if (document.exitFullscreen) {
-    document.exitFullscreen()
-  } else if (document.mozCancelFullScreen) {
-    document.mozCancelFullScreen()
-  } else if (document.webkitCancelFullScreen) {
-    document.webkitCancelFullScreen()
-  } else if (document.msExitFullscreen) {
-    document.msExitFullscreen()
-  }
-}
-
-/**
  * @description 在每次打开新页面的时候调用 打开一个新的 tab
  * @param {object} vm vue
  * @param {string} name route name
