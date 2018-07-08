c53919bdc656522ca81dc4499df46525aad3cefd
liyang <1711467488@qq.com>
2018-07-08 08:01:42 +0800

    no message

---
+++ deploy/preview.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+npm run build
+
+# 进入生成的文件夹
+cd dist
+
+# 如果是发布到自定义域名
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果发布到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+git push -f git@gitee.com:fairyever/d2-admin-start-kit-preview.git master
+
+cd -
+
+echo "publish to https://fairyever.gitee.io/d2-admin-start-kit-preview/#/index"
