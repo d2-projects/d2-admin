6c7651dbc28744fd7225317b29bcdba22ffc8f05
liyang <1711467488@qq.com>
2018-07-17 22:03:45 +0800

    完善
    Former-commit-id: 98a459c4e885b792c11f1e73d63df90292c68c3d [formerly 98a459c4e885b792c11f1e73d63df90292c68c3d [formerly 98a459c4e885b792c11f1e73d63df90292c68c3d [formerly 98a459c4e885b792c11f1e73d63df90292c68c3d [formerly 51eaa80fa9bb33bd4dc1b6a5851b6351f6d4a9a9 [formerly 9a7efd3db60366cd8ead383ce061a887f029509d]]]]]
    Former-commit-id: 73488faeba91a46d73471befd2074a3acf69ebc6
    Former-commit-id: e62ae6f90d974ba2337aeaa342fbfc1a146aa818
    Former-commit-id: 1b1b788db4d0b5a05328a443164cb99290f0ae20 [formerly 40395c0d06f0c418a905b16a563ce0b15bfa2a22]
    Former-commit-id: b51cb0e9c85def26253424d00fcbcd225a5e3d37
    Former-commit-id: 467bd66bc6685dff6b057ed5d8191b17243efbb3
    Former-commit-id: 514185199078e3e623f4e135408c6935ec45e494
    Former-commit-id: 7f5767bfe8efb54511fd22d2be2e787e6a248e54
    Former-commit-id: 59011c70629efa7ca0163b76af17e9f217783221

---
+++ src/menu/index.js
@@ -0,0 +1,55 @@
+// 插件
+import demoPlugins from './modules/demo-plugins'
+// 组件
+import demoComponents from './modules/demo-components'
+// 组件
+import demoCharts from './modules/demo-charts'
+// 组件库
+import demoElement from './modules/demo-element'
+// 试验台
+import demoPlayground from './modules/demo-playground'
+
+// 菜单 侧边栏
+export const side = [
+  demoPlugins,
+  demoComponents,
+  demoCharts,
+  demoElement,
+  demoPlayground
+]
+
+// 菜单 顶栏
+export default [
+  {
+    path: '/index',
+    title: '首页',
+    icon: 'home'
+  },
+  {
+    path: '/demo',
+    title: '功能',
+    icon: 'puzzle-piece',
+    children: [
+      demoElement,
+      demoComponents,
+      demoCharts,
+      demoPlugins,
+      demoPlayground,
+      {
+        title: '空菜单演示',
+        icon: 'folder-o',
+        children: [
+          {
+            title: '正在开发 1',
+            children: [
+              { title: '正在开发 1-1' },
+              { title: '正在开发 1-2' }
+            ]
+          },
+          { title: '正在开发 2' },
+          { title: '正在开发 3' }
+        ]
+      }
+    ]
+  }
+]
