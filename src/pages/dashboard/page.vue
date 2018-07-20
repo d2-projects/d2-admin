0b92c2f6a7ed0b89904e89971ee15773d4cc0cfd
liyang <1711467488@qq.com>
2018-07-20 17:09:36 +0800

    完善
    Former-commit-id: 2f9851f1d6ea4b55228795bd4ccfd36f1c78cf2c [formerly 2f9851f1d6ea4b55228795bd4ccfd36f1c78cf2c [formerly 2f9851f1d6ea4b55228795bd4ccfd36f1c78cf2c [formerly 2f9851f1d6ea4b55228795bd4ccfd36f1c78cf2c [formerly 1e2be03af8c1e899a0010438a0db5cb3c011983f [formerly 02bdfa97a8aebb601237d75109697ade07948483]]]]]
    Former-commit-id: b2e7221aa2dcdb20d9cc8a7222b98f5a3427601f
    Former-commit-id: 51442ed33afe2ca3aa35374e37652cba14f5198b
    Former-commit-id: 487976119d2dd2db7d5984d5b93be2a0dd42cf5e [formerly fbf72b4760b4416a69630af7a44a3fa43fd2e8a2]
    Former-commit-id: 03d720bf4827cff0ed406b64ae487025078d4fbd
    Former-commit-id: bda6b07fe65d3c499216da343c1cdf0db065f294
    Former-commit-id: 17be2bd42175165470c943923214bb75b3beef6c
    Former-commit-id: 0d37132ff578ffa060c2f755664ac9faa24dfd07
    Former-commit-id: 883a7c9bc897f6a5047a1d90423257a3d2894f38

--- src/pages/dashboard/page.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <d2-container>
-    <d2-page-cover
-      title="I AM D2ADMIN"
-      sub-title="追求简约美感 & 上手即用的后台管理系统模板">
-      <img src="./image/icon.png">
-    </d2-page-cover>
-  </d2-container>
-</template>
