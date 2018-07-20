3b1eb51172e4bd4b3332adb1b76a1bc6bc885107
liyang <1711467488@qq.com>
2018-07-20 10:53:42 +0800

    优化
    Former-commit-id: 9de6bd465b86f97a1012907c3aedf872e097d16b [formerly 9de6bd465b86f97a1012907c3aedf872e097d16b [formerly 9de6bd465b86f97a1012907c3aedf872e097d16b [formerly 9de6bd465b86f97a1012907c3aedf872e097d16b [formerly 7989a948bc4c6cf498aa5b267a1d8c75517d7fc6 [formerly e18b272b8ab918d5c29a1935615e2c355e088d04]]]]]
    Former-commit-id: 76de543b05a7ff3f8840b65073a6b722661a0ec9
    Former-commit-id: 9128c9ec8b639f5c6abe21aae7fd5460e1fdc58c
    Former-commit-id: 0e9f03944745c713bf6d211c01a69e5e3e6cbaa5 [formerly b878b40243eeb8b74ca4e01c34fc592a6acbcdec]
    Former-commit-id: 5a8a3c981ed5344460d366f1857d80c729bc69ce
    Former-commit-id: e8bbf1cb816e7c3ff74a7c977634a6d565023ea5
    Former-commit-id: 4ece75860d4f8e6a2336e4eedcbddffe374b1b5d
    Former-commit-id: a42b05b73d29208e631d5005c1ebe0ba98bb2724
    Former-commit-id: 7c7588ba504e51a6de4c34beb20d600282dad7d7

--- src/pages/demo/playground/db/index.vue
+++ src/pages/demo/playground/db/index.vue
@@ -1,23 +1,16 @@
 <template>
   <d2-container type="full" class="page">
     <template slot="header">本地数据</template>
-    <tree-view :data="dbData" :options="options"></tree-view>
+    <d2-highlight :code="dbData"/>
   </d2-container>
 </template>
 
 <script>
-// 插件
 import db from '@/libs/db.js'
-
 export default {
   data () {
     return {
-      dbData: {},
-      options: {
-        maxDepth: 10,
-        rootObjectKey: 'db.value()',
-        modifiable: false
-      }
+      dbData: {}
     }
   },
   mounted () {
@@ -25,7 +18,7 @@ export default {
   },
   methods: {
     load () {
-      this.dbData = db.value()
+      this.dbData = JSON.stringify(db.value(), null, 2)
     }
   }
 }
