99f9c21b752cf1c48b62e8cc08be4ed0a0374f26
liyang <1711467488@qq.com>
2018-07-22 18:35:37 +0800

    表格组件示例

--- src/pages/demo/element/data-table/index.vue
+++ src/pages/demo/element/data-table/index.vue
@@ -2,56 +2,35 @@
   <d2-container type="ghost">
     <template slot="header">表格组件</template>
     <div class="d2-mt d2-mr">
-
-      <el-card shadow="never" class="d2-card">
-        <template slot="header">基础用法</template>
-        <el-table
-          :data="tableData"
-          style="width: 100%">
-          <el-table-column
-            prop="date"
-            label="日期"
-            width="180">
-          </el-table-column>
-          <el-table-column
-            prop="name"
-            label="姓名"
-            width="180">
-          </el-table-column>
-          <el-table-column
-            prop="address"
-            label="地址">
-          </el-table-column>
-        </el-table>
+      <el-card
+        v-for="(table, index) in tableList"
+        :key="index"
+        shadow="never"
+        class="d2-card d2-mb">
+        <template slot="header">{{table.title}}</template>
+        <component :is="table.component"/>
       </el-card>
-
     </div>
   </d2-container>
 </template>
 
 <script>
+const req = context => context.keys().map(context)
+const tables = req(require.context('./components', true, /\.vue$/))
+const components = {}
+const tableList = []
+tables.forEach((table, index) => {
+  components[`d2-demo-el-table-${index + 1}`] = table.default
+  tableList.push({
+    title: table.default.title,
+    component: `d2-demo-el-table-${index + 1}`
+  })
+})
 export default {
+  components,
   data () {
     return {
-      tableData: [
-        {
-          date: '2016-05-02',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        }, {
-          date: '2016-05-04',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1517 弄'
-        }, {
-          date: '2016-05-01',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1519 弄'
-        }, {
-          date: '2016-05-03',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1516 弄'
-        }
-      ]
+      tableList
     }
   }
 }
