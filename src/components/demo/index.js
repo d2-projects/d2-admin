069a571195bad2e1ab9df49960df29b80efc2893
liyang <1711467488@qq.com>
2018-07-24 15:54:10 +0800

    减少层级
    Former-commit-id: 889b557de9234ebb4364ebb87f5bf8b0935294eb [formerly 889b557de9234ebb4364ebb87f5bf8b0935294eb [formerly 889b557de9234ebb4364ebb87f5bf8b0935294eb [formerly 889b557de9234ebb4364ebb87f5bf8b0935294eb [formerly 41b59da80ab4c5c0b9eceeec8fd2f527d22c4ab8 [formerly 3dc1bcda38f391962e102a990c2b68b7a33d4683]]]]]
    Former-commit-id: 3cd64e384a81021adeab4674894f3e4ba934924a
    Former-commit-id: 6c1d5c80613ac406276127f3b01566ace11becab
    Former-commit-id: fb1dae74ec5eea07c96632f6e8841b1bc42a614a [formerly 159c5ea89b386d484b2ef90733075633b156d87e]
    Former-commit-id: 5be175c876d576777072d4430a8520fc7b255112
    Former-commit-id: ec65f0871fca703dbbc7a428ca9e4b582043c361
    Former-commit-id: 80b73945bd350373ce7e3d15b8f0432c1427d8e3
    Former-commit-id: cc2112baadf022e42428dd0019a764e266570b21
    Former-commit-id: b7c059087a58e03c0d954af34b30f94b3e03b60c

--- src/components/demo/index.js
+++
@@ -1,6 +0,0 @@
-import Vue from 'vue'
-
-import d2DemoLinkBtn from '@/components/demo/d2-link-btn'
-
-Vue.component('d2-link-btn', d2DemoLinkBtn)
-Vue.component('d2-page-cover', () => import('@/components/demo/d2-page-cover'))
