b5b6607cb319bda7173c94240f0b250b7459d0d1
liyang <1711467488@qq.com>
2018-07-24 15:47:13 +0800

    访问环境变量示例
    Former-commit-id: 6e597ba63c326e2ad33e3308cc7ee9a084d87924 [formerly 6e597ba63c326e2ad33e3308cc7ee9a084d87924 [formerly 6e597ba63c326e2ad33e3308cc7ee9a084d87924 [formerly 6e597ba63c326e2ad33e3308cc7ee9a084d87924 [formerly a75fc16f0f7033d4c1b1ae73b147b6d7675431ca [formerly 3c3f17d09105dd2ec5789e22999bc29853e79566]]]]]
    Former-commit-id: 821b0ca3d9b733666118a5bb2f159ea2f9997d79
    Former-commit-id: 66edddc9ecea35af298779ae0ee4297e57081108
    Former-commit-id: c32be8ef92b46f420ca307be4afc2d1cf0e38ee8 [formerly 93599337d7201edc7b7a3ecde3027a8b66130104]
    Former-commit-id: 547618df81312f79dc6f0e3d7b657c1c81bd07fa
    Former-commit-id: a52a84ae1563e65c83d3664ca6922801a080e2e8
    Former-commit-id: 34b3fe0b25e12e3789c3ceafd8838017bb0121d9
    Former-commit-id: 40bf9418ef68c40079abc68b50ef8793e8215f79
    Former-commit-id: 39d0dcf1c080345edd4cdc2d8ff59e78e466bef4

--- src/pages/demo/playground/env/index.vue
+++ src/pages/demo/playground/env/index.vue
@@ -3,7 +3,26 @@
     <template slot="header">环境信息</template>
     <el-alert
       :title="`this.$env = ${$env}`"
-      type="info">
+      type="info"
+      class="d2-mb">
     </el-alert>
+    <el-card shadow="never">
+      <p class="d2-mt-0">process.env.NODE_ENV = {{env}}</p>
+      <p>process.env.BASE_URL = {{baseUrl}}</p>
+      <p class="d2-mb-0">process.env.VUE_APP_TITLE = {{title}}</p>
+    </el-card>
   </d2-container>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      env: process.env.NODE_ENV,
+      baseUrl: process.env.BASE_URL,
+      title: process.env.VUE_APP_TITLE
+    }
+  }
+}
+</script>
+
