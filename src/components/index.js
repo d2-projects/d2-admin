22f000821ded13120c4d64bf3e7731cebb5a6210
liyang <1711467488@qq.com>
2018-07-25 08:59:36 +0800

    小优化

--- src/components/index.js
+++ src/components/index.js
@@ -6,6 +6,8 @@ import SplitPane from 'vue-splitpane'
 import d2Container from './d2-container'
 import d2LinkBtn from './d2-link-btn'
 
+// 注意 有些组件使用异步加载会有影响
+
 Vue.component('d2-grid-layout', GridLayout)
 Vue.component('d2-grid-item', GridItem)
 Vue.component('SplitPane', SplitPane)
