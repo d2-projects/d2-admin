55a94016109473965edf285d5dfa4294c26b3bec
liyang <1711467488@qq.com>
2018-07-26 20:40:55 +0800

    修改提示样式

--- src/layout/header-aside/components/mixin/menu.js
+++ src/layout/header-aside/components/mixin/menu.js
@@ -2,7 +2,7 @@ export default {
   methods: {
     handleMenuSelect (index, indexPath) {
       if (/^d2-menu-empty-\d+$/.test(index)) {
-        this.$message('功能正在开发')
+        this.$message.warning('临时菜单')
       } else {
         this.$router.push({
           path: index
