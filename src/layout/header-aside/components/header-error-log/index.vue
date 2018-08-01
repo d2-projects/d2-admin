25329e68b7c4feac565562f22cf3732c7dc5630e
liyang <1711467488@qq.com>
2018-08-01 22:55:40 +0800

    新建相关文件
    Former-commit-id: a8bf514c7999f8be7e16a1ce0a11c80ddb7669be [formerly a8bf514c7999f8be7e16a1ce0a11c80ddb7669be [formerly a8bf514c7999f8be7e16a1ce0a11c80ddb7669be [formerly a8bf514c7999f8be7e16a1ce0a11c80ddb7669be [formerly 4b80b101b9b441edfc3a52a7ef7be4d7a1eb673a [formerly 083338976fea668a05c241b503a1759bc98123ba]]]]]
    Former-commit-id: f7cb601687755202fd6e9ef2270b1b68016997e0
    Former-commit-id: 9ff05052fe6b6e6146c26da66f7ed484e2d84bca
    Former-commit-id: c3faaf4377a616ff4473449de17ebfcbc1edb7b8 [formerly 50c632cab919970ffa8bcb4675cc163fc1404c18]
    Former-commit-id: 865165843bb9c82a11312a5a9aa8a6514f486bb6
    Former-commit-id: 1cafbbc8c25bad67bde221860be54af8b98b30b8
    Former-commit-id: 31b05c2304e9a876c9ad4938ac7adfd885580e36
    Former-commit-id: 9b18591f9ab4b78128c3112048ea30cd0334699d
    Former-commit-id: 899b9c72d1c12a8ee1601cad0694689a4c7ed5bd

---
+++ src/layout/header-aside/components/header-error-log/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <el-tooltip effect="dark" content="异常" placement="bottom">
+      <el-button class="d2-ml-0 d2-mr btn-text can-hover" type="text" @click="dialogVisible = true">
+        <d2-icon name="bug" style="font-size: 20px"/>
+      </el-button>
+    </el-tooltip>
+    <el-dialog
+      title="异常日志"
+      :fullscreen="true"
+      :visible.sync="dialogVisible"
+      :append-to-body="true">
+      <d2-error-log-list/>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      dialogVisible: false
+    }
+  }
+}
+</script>
