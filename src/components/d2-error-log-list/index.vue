75303448d059a7776a5a4a0f8466d93cd2e7c58a
liyang <1711467488@qq.com>
2018-08-02 08:24:51 +0800

    删除示例代码中无用的部分
    Former-commit-id: 0f667e4fcf3883eba654460dc59279ac1bab259e [formerly 0f667e4fcf3883eba654460dc59279ac1bab259e [formerly 0f667e4fcf3883eba654460dc59279ac1bab259e [formerly 0f667e4fcf3883eba654460dc59279ac1bab259e [formerly 378b783ed03db1feee43d5aabede916c0c9c8c7f [formerly d0889ae86988276d50f5f3ab44c3fe6d2e8e160d]]]]]
    Former-commit-id: 35d15ca17006b5c7f36297d778c19913bacc52bb
    Former-commit-id: 4f162cbc3428eb8154027f13147a67e5dd823292
    Former-commit-id: 25ceb52c453041c5a9e2fd5114409b17d3c165e4 [formerly 1795bd84de74f3c0f7562a5843c0cdc83d953fb1]
    Former-commit-id: 8f394c678c86109e5c588f71f83476fac2977487
    Former-commit-id: b86d072c5f9a42d4958e668c742e1e076996929b
    Former-commit-id: 12ffbc2ac3b8e697f0a1d34dabcaa9203617fbb9
    Former-commit-id: 87e7d72c44c6598a355c63a6edd9addfb9c902f4
    Former-commit-id: d84a5d6c5047fc71d8a66a6f2a93e511ffa08a43

--- src/components/d2-error-log-list/index.vue
+++ src/components/d2-error-log-list/index.vue
@@ -1,11 +1,66 @@
 <template>
-  <div>
-    d2-error-log-list
-  </div>
+  <el-table
+    :data="tableData5"
+    style="width: 100%">
+    <el-table-column type="expand">
+      <template slot-scope="props">
+        {{props.row}}
+      </template>
+    </el-table-column>
+    <el-table-column
+      label="商品 ID"
+      prop="id">
+    </el-table-column>
+    <el-table-column
+      label="商品名称"
+      prop="name">
+    </el-table-column>
+    <el-table-column
+      label="描述"
+      prop="desc">
+    </el-table-column>
+  </el-table>
 </template>
 
 <script>
 export default {
-  name: 'd2-error-log-list'
+  name: 'd2-error-log-list',
+  data () {
+    return {
+      tableData5: [{
+        id: '12987122',
+        name: '好滋好味鸡蛋仔',
+        category: '江浙小吃、小吃零食',
+        desc: '荷兰优质淡奶，奶香浓而不腻',
+        address: '上海市普陀区真北路',
+        shop: '王小虎夫妻店',
+        shopId: '10333'
+      }, {
+        id: '12987123',
+        name: '好滋好味鸡蛋仔',
+        category: '江浙小吃、小吃零食',
+        desc: '荷兰优质淡奶，奶香浓而不腻',
+        address: '上海市普陀区真北路',
+        shop: '王小虎夫妻店',
+        shopId: '10333'
+      }, {
+        id: '12987125',
+        name: '好滋好味鸡蛋仔',
+        category: '江浙小吃、小吃零食',
+        desc: '荷兰优质淡奶，奶香浓而不腻',
+        address: '上海市普陀区真北路',
+        shop: '王小虎夫妻店',
+        shopId: '10333'
+      }, {
+        id: '12987126',
+        name: '好滋好味鸡蛋仔',
+        category: '江浙小吃、小吃零食',
+        desc: '荷兰优质淡奶，奶香浓而不腻',
+        address: '上海市普陀区真北路',
+        shop: '王小虎夫妻店',
+        shopId: '10333'
+      }]
+    }
+  }
 }
 </script>
