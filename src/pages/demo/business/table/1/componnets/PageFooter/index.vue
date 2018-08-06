a891f78232898b5947f251593193b17a4d8e9f4a
liyang <1711467488@qq.com>
2018-08-06 23:20:19 +0800

    分页控件和表格显示调整

--- src/pages/demo/business/table/1/componnets/PageFooter/index.vue
+++ src/pages/demo/business/table/1/componnets/PageFooter/index.vue
@@ -1,5 +1,34 @@
 <template>
-  <div>
-    footer
-  </div>
+  <el-pagination
+    :current-page="page.current"
+    :page-size="page.size"
+    :total="page.total"
+    :page-sizes="[100, 200, 300, 400]"
+    layout="total, sizes, prev, pager, next, jumper"
+    style="margin: -10px;"
+    @size-change="handleSizeChange"
+    @current-change="handleCurrentChange">
+  </el-pagination>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      page: {
+        current: 1,
+        size: 100,
+        total: 400
+      }
+    }
+  },
+  methods: {
+    handleSizeChange(val) {
+      console.log(`每页 ${val} 条`)
+    },
+    handleCurrentChange(val) {
+      console.log(`当前页: ${val}`)
+    }
+  }
+}
+</script>
