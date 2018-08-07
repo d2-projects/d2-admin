5933728338e29275c7f590e9ea5ce19643e31613
liyang <1711467488@qq.com>
2018-08-07 19:08:42 +0800

    简化mock注册方式 优化mock ajax测试页面显示效果

--- src/pages/demo/plugins/mock/ajax.vue
+++ src/pages/demo/plugins/mock/ajax.vue
@@ -1,9 +1,17 @@
 <template>
   <d2-container>
     <div slot="header">
-      <el-button @click="ajax">发送请求</el-button>
+      <el-button
+        size="mini"
+        type="primary"
+        @click="ajax">
+        <d2-icon name="paper-plane"/>
+        发送请求
+      </el-button>
     </div>
-    <el-table v-bind="table" style="width: 100%">
+    <el-table
+      v-bind="table"
+      style="width: 100%">
       <el-table-column
         v-for="(item, index) in table.columns"
         :key="index"
