83cdf8ca4bf4a421642dcabb5d75577a06379df3
liyang <1711467488@qq.com>
2018-08-09 14:12:34 +0800

    account

--- src/layout/header-aside/components/header-user/index.vue
+++ src/layout/header-aside/components/header-user/index.vue
@@ -19,7 +19,7 @@ export default {
     ])
   },
   methods: {
-    ...mapActions('d2admin', [
+    ...mapActions('d2admin/account', [
       'logout'
     ]),
     /**
--- src/store/modules/d2admin/index.js
+++ src/store/modules/d2admin/index.js
@@ -12,6 +12,7 @@ import user from './modules/user'
 import menu from './modules/menu'
 import theme from './modules/theme'
 import log from './modules/log'
+import account from './modules/account'
 
 const pageOpenedDefult = {
   name: 'index',
@@ -29,7 +30,8 @@ export default {
     user,
     menu,
     theme,
-    log
+    log,
+    account
   },
   state: {
     // 全屏
@@ -61,86 +63,6 @@ export default {
         }
         return true
       }).map(e => e.name)
-    }
-  },
-  actions: {
-    /**
-     * 登陆
-     * @param {Object} param context
-     * @param {Object} param { vue, username, password }
-     */
-    login ({ commit }, { vm, username, password }) {
-      // 开始请求登录接口
-      vm.$axios({
-        method: 'post',
-        url: '/login',
-        data: {
-          username,
-          password
-        }
-      })
-        .then(res => {
-          // 设置 cookie 一定要存 uuid 和 token 两个 cookie
-          // 整个系统依赖这两个数据进行校验和存储
-          // uuid 是用户身份唯一标识 用户注册的时候确定 并且不可改变 不可重复
-          // token 代表用户当前登录状态 建议在网络请求中携带 token
-          // 如有必要 token 需要定时更新，默认保存一天
-          utilLib.cookies.set('uuid', res.data.uuid)
-          utilLib.cookies.set('token', res.data.token)
-          // 设置 vuex 用户信息
-          commit('user/set', {
-            name: res.data.name
-          })
-          // 用户登陆后从数据库加载一系列的设置
-          commit('loginSuccessLoad')
-          // 跳转路由
-          vm.$router.push({
-            name: 'index'
-          })
-        })
-        .catch(err => {
-          console.group('登陆结果')
-          console.log('err: ', err)
-          console.groupEnd()
-        })
-    },
-    /**
-     * 注销用户并返回登陆页面
-     * @param {Object} param context
-     * @param {Object} confirm need confirm ?
-     */
-    logout ({ commit }, { vm, confirm }) {
-      /**
-       * @description 注销
-       */
-      function logout () {
-        // 删除cookie
-        utilLib.cookies.remove('token')
-        utilLib.cookies.remove('uuid')
-        // 跳转路由
-        vm.$router.push({
-          name: 'login'
-        })
-      }
-      // 判断是否需要确认
-      if (confirm) {
-        commit('grayModeSet', true)
-        vm.$confirm('注销当前账户吗?  打开的标签页和用户设置将会被保存。', '确认操作', {
-          confirmButtonText: '确定注销',
-          cancelButtonText: '放弃',
-          type: 'warning'
-        })
-          .then(() => {
-            commit('grayModeSet', false)
-            logout()
-          })
-          .catch(() => {
-            commit('grayModeSet', false)
-            vm.$message('放弃注销用户')
-          })
-      } else {
-        logout()
-      }
     }
   },
   mutations: {
@@ -266,21 +188,6 @@ export default {
      */
     uaGet (state) {
       state.ua = utilLib.ua()
-    },
-    /**
-     * @class ...
-     * @description 用户登陆后从数据库加载一系列的设置
-     * @param {Object} state vuex state
-     */
-    loginSuccessLoad (state) {
-      // DB -> store 加载用户名
-      this.commit('d2admin/user/load')
-      // DB -> store 加载主题
-      this.commit('d2admin/theme/load')
-      // DB -> store 数据库加载上次退出时的多页列表
-      this.commit('d2admin/pageOpenedListLoad')
-      // DB -> store 数据库加载这个用户之前设置的侧边栏折叠状态
-      this.commit('d2admin/menu/asideCollapseLoad')
     },
     /**
      * @class pagePool
