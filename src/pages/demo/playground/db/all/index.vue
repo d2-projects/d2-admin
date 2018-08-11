e5d8d022f01912183125415a51ecb7f2387a8f0a
liyang <1711467488@qq.com>
2018-08-11 22:16:16 +0800

    db update
    Former-commit-id: 3efab3f3c7cf7a34d78ebc1aebcf895fe1832970 [formerly 3efab3f3c7cf7a34d78ebc1aebcf895fe1832970 [formerly 3efab3f3c7cf7a34d78ebc1aebcf895fe1832970 [formerly 3efab3f3c7cf7a34d78ebc1aebcf895fe1832970 [formerly 3608951ec0d14f5fdda5f5c3d74ca1658b4c220a [formerly b3ea1a90fb68b439126a0e163a017e242a0a8dbd]]]]]
    Former-commit-id: 452ee4fcd7e53cad75c82f54925db50caf4da311
    Former-commit-id: a6b77ca047f33d44ec3cc9c7311ad85fda1cbdaf
    Former-commit-id: 625313ff14d3e4a6c4496e0dbbc5d1e2e4624543 [formerly 7b93944ff7f9803377076bd9312f11c82ad60ddd]
    Former-commit-id: dab47fcc9792a84172a95410cf8359e31c7c08de
    Former-commit-id: e341761c086bb8eb5d842abf411189ec35699361
    Former-commit-id: 2bada492361b11dc9e7c5e2eff0baaad7a1aa822
    Former-commit-id: 66175c2c3ac72e9b31a66021d4d208c76348e70a
    Former-commit-id: b6844ade3e3cdfabb5e58010df6ad0ffa2b2da97

---
+++ src/pages/demo/playground/db/all/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <d2-container class="page">
+    <template slot="header">
+      <el-button type="primary" @click="load">
+        重新获取本地数据
+      </el-button>
+    </template>
+    <d2-highlight :code="dbData"/>
+  </d2-container>
+</template>
+
+<script>
+import db from '@/libs/db.js'
+export default {
+  data () {
+    return {
+      dbData: ''
+    }
+  },
+  mounted () {
+    this.load()
+  },
+  methods: {
+    load () {
+      this.dbData = JSON.stringify(db.value(), null, 2)
+    }
+  }
+}
+</script>
