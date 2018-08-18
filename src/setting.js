bf3ea8deb6bbfc9be53da303ec96b07e447488f5
liyang <1711467488@qq.com>
2018-08-18 10:27:10 +0800

    顺序整理
    Former-commit-id: 95dab17076892c5d76f26ce43a75566edc50580c [formerly 95dab17076892c5d76f26ce43a75566edc50580c [formerly 95dab17076892c5d76f26ce43a75566edc50580c [formerly 95dab17076892c5d76f26ce43a75566edc50580c [formerly 742ca799c0d528ef15f3df85f1da47d81ce16355 [formerly caab06ee31c4b00ab37dc8ea6aa8408e52122d85]]]]]
    Former-commit-id: 9d5d438ef73a60ac2d0b9148e4e844365cee0f55
    Former-commit-id: 7e2e85fbd1a419e28ea9ea3448c94d387ed7d2a0
    Former-commit-id: e9acc40cbef8b59e8c155bb9740727cd155d99b1 [formerly 7850a06ed145f64b04d98cad51337f907f0de448]
    Former-commit-id: a02688e010f723dc49136c7a530be2a552c99bf9
    Former-commit-id: 8235b91b496dbe86ecdd95864fe99d2d72ac455d
    Former-commit-id: 3e07baf19db5a2c9922c702b98ddae787c6fdf72
    Former-commit-id: b919cf34172fa0d857245ec0a5543518b8c6d399
    Former-commit-id: dad76d00d192a27570c2fd35f187ef90986a9314

--- src/setting.js
+++ src/setting.js
@@ -1,11 +1,9 @@
 import { version } from '../package'
 
 const setting = {
-  // 版本
-  version: version,
-  // 是否默认开启页面切换动画
-  transition: {
-    active: true
+  // 侧边栏默认折叠状态
+  menu: {
+    asideCollapse: false
   },
   // 在读取持久化数据失败时默认页面
   page: {
@@ -19,9 +17,9 @@ const setting = {
       }
     ]
   },
-  // 侧边栏默认折叠状态
-  menu: {
-    asideCollapse: false
+  // 版本
+  releases: {
+    version: version
   },
   // 注册的主题
   theme: {
@@ -54,6 +52,10 @@ const setting = {
         preview: 'image/theme/tomorrow-night-blue/preview@2x.png'
       }
     ]
+  },
+  // 是否默认开启页面切换动画
+  transition: {
+    active: true
   },
   // 在读取持久化数据失败时默认用户信息
   user: {
