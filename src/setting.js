1baac207245652183b03747c8f01dd01649a16e8
liyang <1711467488@qq.com>
2018-08-18 09:12:21 +0800

    setting.page.opened
    Former-commit-id: 6c9d1551a1272b4ec72ddd8c2b234032fa6c0f61 [formerly 6c9d1551a1272b4ec72ddd8c2b234032fa6c0f61 [formerly 6c9d1551a1272b4ec72ddd8c2b234032fa6c0f61 [formerly 6c9d1551a1272b4ec72ddd8c2b234032fa6c0f61 [formerly ba4d62f46328960667d1a494f3e7ce3ed70a245d [formerly 8a9e597d75d85be0733820959990f24ccf7512c7]]]]]
    Former-commit-id: c0c553913c0860aa6d57168dea8e121e7c4acb6f
    Former-commit-id: bcea6ec987b15dcf777cde8e01a1d809ca7ef6b6
    Former-commit-id: 33243469bbcb5ed69f831df6c32cdc50ea342f51 [formerly b43657fc42fe609ddc7d9e1f4c258aca6d8288ce]
    Former-commit-id: f8dad1ef93466ca9e0330fbb49c228af5549d197
    Former-commit-id: 48e155804b06454f70de094e5d4a54e2af4f5d64
    Former-commit-id: 029dd50c1fa0588c9c849b9c13f81ce855efa32a
    Former-commit-id: 580777b7e7e1f40ebf51de38493e98e051b15343
    Former-commit-id: 93f9de93a6697ea991b3ec3b0f694fb52562e0e6

--- src/setting.js
+++ src/setting.js
@@ -6,6 +6,18 @@ const setting = {
   // 是否默认开启页面切换动画
   transition: {
     active: true
+  },
+  // 在读取页面持久化数据失败时默认页面
+  page: {
+    opened: [
+      {
+        name: 'index',
+        meta: {
+          title: '首页',
+          requiresAuth: false
+        }
+      }
+    ]
   }
 }
 
