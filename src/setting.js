653f835ab8064cab4c0744d8b61ea2b9992b4455
liyang <1711467488@qq.com>
2018-08-18 09:05:39 +0800

    setting.transition.active

--- src/setting.js
+++ src/setting.js
@@ -1,7 +1,12 @@
 import { version } from '../package'
 
 const setting = {
-  version
+  // 版本
+  version: version,
+  // 是否默认开启页面切换动画
+  transition: {
+    active: true
+  }
 }
 
 export default setting
