4d16ed0a2219e9ff2550e18dac09ef9bd71dcb4f
liyang <1711467488@qq.com>
2018-08-18 14:43:48 +0800

    增加信息
    Former-commit-id: e8fcdb27602f7b9085b96f35e072ff779708f72f [formerly e8fcdb27602f7b9085b96f35e072ff779708f72f [formerly e8fcdb27602f7b9085b96f35e072ff779708f72f [formerly e8fcdb27602f7b9085b96f35e072ff779708f72f [formerly aa4b2e67844a18b4c3dbccbab6c3c8a168de40e2 [formerly fc9c0567361db3f7ac9fadfd7200f3223e44aeb9]]]]]
    Former-commit-id: 001e3b3d9f701d72c05db75d060e74863d8362ef
    Former-commit-id: 609ff7546c6545e45a8ea9320f0d10ed6f252b84
    Former-commit-id: 7e0697499331e3bac39d4de84dca1dd2eab6c1dd [formerly 96f76273c67a09e615a880b3e4d0368e5b83d6a2]
    Former-commit-id: 857519f5bfb45d0fcb74cd43c600504645234c74
    Former-commit-id: f54353b8458588655d9c3c1c9ecab93ccfc324a7
    Former-commit-id: 528558425ee616260a7411367d678420e3241bd5
    Former-commit-id: 80ebe3104177dd648f978be9642ae4337bc1744d
    Former-commit-id: 83a789056a7e5b8f5016bdf98cabc32116282969

--- src/pages/demo/playground/store/transition/index.vue
+++ src/pages/demo/playground/store/transition/index.vue
@@ -1,7 +1,10 @@
 <template>
   <d2-container type="card">
+    <template slot="header">
+      当前状态 {{active ? '开启过渡动画' : '关闭过渡动画'}}
+    </template>
     <el-button type="primary" @click="set(!active)">
-      {{active ? '关闭页面切换动画' : '打开页面切换动画'}}
+      {{active ? '关闭页面过渡动画' : '打开页面过渡动画'}}
     </el-button>
   </d2-container>
 </template>
