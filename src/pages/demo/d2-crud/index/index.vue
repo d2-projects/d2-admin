b2706d8478d91989ff11cf2e2b15e42746726e2f
liyang <1711467488@qq.com>
2018-08-22 19:41:00 +0800

    基本文件创建

---
+++ src/pages/demo/d2-crud/index/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <d2-container>
+    <d2-page-cover
+      title="D2 CRUD"
+      sub-title="表格封装">
+      <img src="./image/icon.png">
+    </d2-page-cover>
+  </d2-container>
+</template>
