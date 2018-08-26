c86d621469dcf4452ca30c3c979146cdf9f1c61c
liyang <1711467488@qq.com>
2018-08-26 22:07:26 +0800

    注释改为了中文

--- src/plugin/axios/index.js
+++ src/plugin/axios/index.js
@@ -2,17 +2,17 @@ import axios from 'axios'
 import { Message } from 'element-ui'
 import util from '@/libs/util'
 
-// create an axios instance
+// 创建一个 axios 实例
 const service = axios.create({
   baseURL: process.env.VUE_APP_API,
   timeout: 5000 // 请求超时时间
 })
 
-// request interceptor
+// 请求拦截器
 service.interceptors.request.use(
   config => {
+    // 在请求发送之前做一些处理
     if (!(/^https:\/\/|http:\/\//.test(config.url))) {
-      // Do something before request is sent
       const token = util.cookies.get('token')
       if (token && token !== 'undefined') {
         // 让每个请求携带token-- ['X-Token']为自定义key 请根据实际情况自行修改
@@ -22,19 +22,20 @@ service.interceptors.request.use(
     return config
   },
   error => {
-    // Do something with request error
-    console.log(error) // for debug
+    // 发送失败
+    console.log(error)
     Promise.reject(error)
   }
 )
 
-// respone interceptor
+// 响应拦截器
 service.interceptors.response.use(
   response => {
     return response.data
   },
   error => {
-    console.log('err' + error) // for debug
+    util.log.danger('>>>>>> Error [ service.interceptors.response ] >>>>>>')
+    console.log(error)
     Message({
       message: error.message,
       type: 'error',
