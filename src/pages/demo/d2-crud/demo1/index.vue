569b5f690dc35b7c1fc212ad6893513916c2ce15
孙昊翔 <673686754@qq.com>
2018-08-26 21:22:04 +0800

    d2-crud doc 基本表格

--- src/pages/demo/d2-crud/demo1/index.vue
+++ src/pages/demo/d2-crud/demo1/index.vue
@@ -1,9 +1,58 @@
 <template>
   <d2-container>
-    <template slot="header">header</template>
-    main
+    <template slot="header">基本表格</template>
+    <d2-crud
+      :columns="columns"
+      :data="data"
+    >
+    </d2-crud>
     <template slot="footer">
-      <d2-link-btn title="D2 CRUD" link="https://github.com/d2-projects"/>
+      <d2-link-btn title="D2 CRUD" link="https://github.com/d2-projects/d2-crud"/>
     </template>
   </d2-container>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      columns: [
+        {
+          title: '日期',
+          key: 'date'
+        },
+        {
+          title: '姓名',
+          key: 'name'
+        },
+        {
+          title: '地址',
+          key: 'address'
+        }
+      ],
+      data: [
+        {
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        },
+        {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄'
+        },
+        {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄'
+        },
+        {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }
+      ]
+    }
+  }
+}
+</script>
