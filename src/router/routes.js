d395754352d228b851b5a4a0dd9dcc9a8276fb04
孙昊翔 <673686754@qq.com>
2018-08-27 15:29:00 +0800

    流体高度

--- src/router/routes.js
+++ src/router/routes.js
@@ -65,7 +65,8 @@ const frameIn = [
       { path: 'demo3', name: `${pre}demo3`, component: () => import('@/pages/demo/d2-crud/demo3'), meta: { ...meta, title: '带边框表格' } },
       { path: 'demo4', name: `${pre}demo4`, component: () => import('@/pages/demo/d2-crud/demo4'), meta: { ...meta, title: '带状态表格' } },
       { path: 'demo5', name: `${pre}demo5`, component: () => import('@/pages/demo/d2-crud/demo5'), meta: { ...meta, title: '固定表头' } },
-      { path: 'demo6', name: `${pre}demo6`, component: () => import('@/pages/demo/d2-crud/demo6'), meta: { ...meta, title: '固定列' } }
+      { path: 'demo6', name: `${pre}demo6`, component: () => import('@/pages/demo/d2-crud/demo6'), meta: { ...meta, title: '固定列' } },
+      { path: 'demo7', name: `${pre}demo7`, component: () => import('@/pages/demo/d2-crud/demo7'), meta: { ...meta, title: '流体高度' } }
     ])('demo-d2-crud-')
   },
   {
