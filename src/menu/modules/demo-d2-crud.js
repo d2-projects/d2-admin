ae1591df313e461449c2957407911e0c9ec393d2
孙昊翔 <673686754@qq.com>
2018-08-27 13:25:32 +0800

    d2-crud 带斑马纹表格
    Former-commit-id: bcae52d5809c0b37ded9c403afaa3368014c30a6 [formerly bcae52d5809c0b37ded9c403afaa3368014c30a6 [formerly bcae52d5809c0b37ded9c403afaa3368014c30a6 [formerly bcae52d5809c0b37ded9c403afaa3368014c30a6 [formerly 0a6b9ebf02f0898a03e2e2eb5bf8707a9027f7f4 [formerly 3be928fb9c57859903f6cc1179001ab4afd0c903]]]]]
    Former-commit-id: cb1a3799d7152a98af9f69d7b07e759d560d45b4
    Former-commit-id: 6e3b6f0807ad073a26ece5b1168456ec06d26954
    Former-commit-id: bae445be64d1eac45dc62d9f9622f9fd7251b341 [formerly 7a2a1f2b113a7911702f16dd78f2f16e72525c82]
    Former-commit-id: d91cf753c577384a5dec1ccfdc4d92b8eaf48e56
    Former-commit-id: 6834b79945fb644e7809929f8a66145208cf2931
    Former-commit-id: 1041c5f3fece097df722d3de337b70bf55cf3402
    Former-commit-id: 70899c992f215e302a08ea72823ca32c41e8dd76
    Former-commit-id: 14763df34cba0c66390d77b24045195212ad0d68

--- src/menu/modules/demo-d2-crud.js
+++ src/menu/modules/demo-d2-crud.js
@@ -4,6 +4,7 @@ export default {
   icon: 'table',
   children: (pre => [
     { path: `${pre}index`, title: 'D2 CRUD 首页', icon: 'home' },
-    { path: `${pre}demo1`, title: '基础表格' }
+    { path: `${pre}demo1`, title: '基础表格' },
+    { path: `${pre}demo2`, title: '带斑马纹表格' }
   ])('/demo/d2-crud/')
 }
