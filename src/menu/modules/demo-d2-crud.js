02b1c4a4b1c010d1f16884999ebcbdf77ab89530
孙昊翔 <673686754@qq.com>
2018-08-28 11:01:20 +0800

    d2-crud 示例菜单分级

--- src/menu/modules/demo-d2-crud.js
+++ src/menu/modules/demo-d2-crud.js
@@ -4,20 +4,25 @@ export default {
   iconSvg: 'd2-crud',
   children: (pre => [
     { path: `${pre}index`, title: 'D2 CRUD 首页', icon: 'home' },
-    { path: `${pre}demo1`, title: '基础表格' },
-    { path: `${pre}demo2`, title: '带斑马纹表格' },
-    { path: `${pre}demo3`, title: '带边框表格' },
-    { path: `${pre}demo4`, title: '带状态表格' },
-    { path: `${pre}demo5`, title: '固定表头' },
-    { path: `${pre}demo6`, title: '固定列' },
-    { path: `${pre}demo7`, title: '流体高度' },
-    { path: `${pre}demo8`, title: '多级表头' },
-    { path: `${pre}demo9`, title: '单选' },
-    { path: `${pre}demo10`, title: '多选' },
-    { path: `${pre}demo11`, title: '排序' },
-    { path: `${pre}demo12`, title: '筛选' },
-    { path: `${pre}demo13`, title: '表尾合计行' },
-    { path: `${pre}demo14`, title: '合并行' },
-    { path: `${pre}demo15`, title: '合并列' }
+    {
+      title: '基础功能',
+      children: [
+        { path: `${pre}demo1`, title: '基础表格' },
+        { path: `${pre}demo2`, title: '带斑马纹表格' },
+        { path: `${pre}demo3`, title: '带边框表格' },
+        { path: `${pre}demo4`, title: '带状态表格' },
+        { path: `${pre}demo5`, title: '固定表头' },
+        { path: `${pre}demo6`, title: '固定列' },
+        { path: `${pre}demo7`, title: '流体高度' },
+        { path: `${pre}demo8`, title: '多级表头' },
+        { path: `${pre}demo9`, title: '单选' },
+        { path: `${pre}demo10`, title: '多选' },
+        { path: `${pre}demo11`, title: '排序' },
+        { path: `${pre}demo12`, title: '筛选' },
+        { path: `${pre}demo13`, title: '表尾合计行' },
+        { path: `${pre}demo14`, title: '合并行' },
+        { path: `${pre}demo15`, title: '合并列' }
+      ]
+    }
   ])('/demo/d2-crud/')
 }
