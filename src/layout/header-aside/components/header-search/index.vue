615dde5e050cf7bb076decf1207db8b8b2913235
liyang <1711467488@qq.com>
2018-08-28 11:16:00 +0800

    顶栏搜索按钮加大
    Former-commit-id: 40cd281fdff846c727c580d6709724d4c5a3f146 [formerly 40cd281fdff846c727c580d6709724d4c5a3f146 [formerly 40cd281fdff846c727c580d6709724d4c5a3f146 [formerly 40cd281fdff846c727c580d6709724d4c5a3f146 [formerly ca5621d4201f3d678f21fb2c964128bea232ee21 [formerly 34449f9f0a499ec2de4c57f2d3f1d37baa267a73]]]]]
    Former-commit-id: 1f680ef63f7837d95b1edcd67283d6457018afe8
    Former-commit-id: 1154896c0852c2d8d83475eaf08b0bb3723a73ab
    Former-commit-id: 593cbf3ea1dbf22d73ddd778ba1eb19f99ba4f25 [formerly cc39b4f0b7e1ac85e3fe4a7da679b4389c4c90fb]
    Former-commit-id: e94065719b4d6de919ffa43e258701bff3dd17e4
    Former-commit-id: abdadc2f531e5e4d7b3578906529a986bf1aa578
    Former-commit-id: 749a2a8542f8565d4a55306a2fa3a613b2633b9c
    Former-commit-id: 240a1ddf7b71bf8446f504b5a741331bb186a01c
    Former-commit-id: 5880d87decd9e8e2af4283ee305bd63d21b1e4ab

--- src/layout/header-aside/components/header-search/index.vue
+++ src/layout/header-aside/components/header-search/index.vue
@@ -1,6 +1,6 @@
 <template>
   <el-button class="d2-mr btn-text can-hover" type="text" @click="handleClick">
-    <d2-icon name="search"/>
+    <d2-icon name="search" style="font-size: 18px;"/>
   </el-button>
 </template>
 
