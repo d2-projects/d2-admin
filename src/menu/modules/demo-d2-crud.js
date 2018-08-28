983eab079d40951e39d295ac1c527b73b1f68c80
孙昊翔 <673686754@qq.com>
2018-08-28 10:40:17 +0800

    合并行
    Former-commit-id: 80bdfce859a175114e69639bdec119b7f28383b3 [formerly 7e8742b94e50636c473709ef67fc92ee362a521c] [formerly 80bdfce859a175114e69639bdec119b7f28383b3 [formerly 7e8742b94e50636c473709ef67fc92ee362a521c] [formerly 80bdfce859a175114e69639bdec119b7f28383b3 [formerly 7e8742b94e50636c473709ef67fc92ee362a521c] [formerly 7e8742b94e50636c473709ef67fc92ee362a521c [formerly 2041b9949949dbca0bd964c5557e2dcddb95303a [formerly 9dcc76f94eb45430d2f99444d61a6ed28a67df04]]]]]
    Former-commit-id: 7a046a638cbe4371a047be6eb8a3c5e04b5cd714
    Former-commit-id: 1e0c24ab7dabecf892a170d5ac00893807f23345
    Former-commit-id: ad0b0c4fbcce781d0ba0d6f28bc3fa297e1cbf5d [formerly 9e0f19ad4965cfed84acdf462f4f392448f1095d]
    Former-commit-id: 1d051ac234e5ee97e1f78269551e1da7eb86354c
    Former-commit-id: 0ef69b2a85496a4b78e97c89c7363a5e30fcabd5
    Former-commit-id: dc44ea4032a76a3983dce62fcacbc60737410c41
    Former-commit-id: 559e29f5b0a2016cb0f4714e37ac50bfd30e1720
    Former-commit-id: 087134a1991cec33b65e28d12565fd6783fadc80

--- src/menu/modules/demo-d2-crud.js
+++ src/menu/modules/demo-d2-crud.js
@@ -16,6 +16,7 @@ export default {
     { path: `${pre}demo10`, title: '多选' },
     { path: `${pre}demo11`, title: '排序' },
     { path: `${pre}demo12`, title: '筛选' },
-    { path: `${pre}demo13`, title: '表尾合计行' }
+    { path: `${pre}demo13`, title: '表尾合计行' },
+    { path: `${pre}demo14`, title: '合并行' }
   ])('/demo/d2-crud/')
 }
