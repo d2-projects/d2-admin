2d264eb09ca5cdb580b459a876369d24b4d7a089
孙昊翔 <673686754@qq.com>
2018-08-28 16:01:48 +0800

    d2-crud 表单组件渲染 表单布局
    Former-commit-id: dffabbae052e82d011c49a482e2c6579b510131c [formerly dffabbae052e82d011c49a482e2c6579b510131c [formerly dffabbae052e82d011c49a482e2c6579b510131c [formerly dffabbae052e82d011c49a482e2c6579b510131c [formerly 393c14f4ef07c4f7920adb79d9784961826f909d [formerly fa929764dd324849a7d1032aa98cab34600f4d85]]]]]
    Former-commit-id: 8f9e71375a306a4f71e9688e302327acfd1ccd6f
    Former-commit-id: 5c4bc13c8c115282d98b5de8f56fa46cc523c2d6
    Former-commit-id: 5c59b4084b8f86ce00488020787a1d8361c099c1 [formerly 818a62a1c53edf8827226e96a16ef7b38414a5a4]
    Former-commit-id: eeb546c951042a284a8553cd4c4fe4f18804ff8b
    Former-commit-id: 17fa37ebac025b07a355da563dc490aa39507877
    Former-commit-id: d0f5a02d0af43ab33e8b59b22335182ef2527cf0
    Former-commit-id: 8115efda42fdb71b1df18e56a7336df8117bd5c7
    Former-commit-id: 9be41ad5cd1dbf6f772e64c0381e26b0a8eceb91

--- src/menu/modules/demo-d2-crud.js
+++ src/menu/modules/demo-d2-crud.js
@@ -30,7 +30,9 @@ export default {
         { path: `${pre}demo16`, title: '新增数据' },
         { path: `${pre}demo17`, title: '修改数据' },
         { path: `${pre}demo18`, title: '删除数据' },
-        { path: `${pre}demo19`, title: '自定义操作列' }
+        { path: `${pre}demo19`, title: '自定义操作列' },
+        { path: `${pre}demo20`, title: '表单组件渲染' },
+        { path: `${pre}demo21`, title: '表单布局' }
       ]
     }
   ])('/demo/d2-crud/')
