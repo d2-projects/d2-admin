d1ee5ff7e7beb597e0d533771364eac46c771d1a
liyang <1711467488@qq.com>
2018-08-29 20:14:14 +0800

    更改菜单标题
    Former-commit-id: 10dbef81192848923018d0c46e1ab5dbc2cd8b46 [formerly ce8c46678bae6579f6ac764db3f86ed289248c29] [formerly 10dbef81192848923018d0c46e1ab5dbc2cd8b46 [formerly ce8c46678bae6579f6ac764db3f86ed289248c29] [formerly 10dbef81192848923018d0c46e1ab5dbc2cd8b46 [formerly ce8c46678bae6579f6ac764db3f86ed289248c29] [formerly ce8c46678bae6579f6ac764db3f86ed289248c29 [formerly a10b32da5f74d94da06e7cd71aa33556fda9df08 [formerly 65fd96febf0ec82e2d0a3c6f368b3882101873b6]]]]]
    Former-commit-id: 1f3376cc20eef14a3ff26ef02a705bfaa1ba6925
    Former-commit-id: d1b32ce89532ee2d9cb255530f0d8b742850db5a
    Former-commit-id: 73ac21e8558f745a45c0bb7e0ff0f77677c33204 [formerly 2788e7332be8d0a82c8ec2596cb4bef23c700a5d]
    Former-commit-id: cc5fc094f83b1855196b4b79b272cc9ce2acf34c
    Former-commit-id: ce441a2fbab9e49ccfd7e31bcf59bcd1cdb8b0f3
    Former-commit-id: 0bc054ec930323bf32136a9e38fc613f69e82039
    Former-commit-id: 1275c34599eba7bdf15b3a037f732b33e7c87c51
    Former-commit-id: 34ab4975c03606cde596b5f7da3833861a1b6dbf

--- src/menu/modules/demo-frame.js
+++ src/menu/modules/demo-frame.js
@@ -1,6 +1,6 @@
 export default {
   path: '/demo/frame',
-  title: '第三方网页',
+  title: '内嵌网页',
   icon: 'globe',
   children: (pre => [
     { path: `${pre}d2-doc`, title: 'D2Admin 中文文档', iconSvg: 'd2-admin' }
