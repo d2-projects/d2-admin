f535d7163f74d6bea299b68ced4773e9b7703149
liyang <1711467488@qq.com>
2018-09-12 10:31:16 +0800

    no message
    Former-commit-id: 7293d05bbbce121ca11e06de91ac2f1b775c14d9 [formerly 7293d05bbbce121ca11e06de91ac2f1b775c14d9 [formerly 7293d05bbbce121ca11e06de91ac2f1b775c14d9 [formerly 7293d05bbbce121ca11e06de91ac2f1b775c14d9 [formerly 78de87c4262aff3a4c314eba6308e80bc4b94568 [formerly af05d745c14160b7c5d74645c04b52653af3d6a6]]]]]
    Former-commit-id: 8d552fccc75f9e545862d3a0de474c1460b2b35b
    Former-commit-id: 86c5771f8d752674e7a84468e0e0f79f8e311602
    Former-commit-id: e876ce46ba02cb64874edd55fd4c2bbed529c30a [formerly b6f22036fb293f78cd12866d09d951f44bf0fae0]
    Former-commit-id: aa944f9181fa0bbfd45041c63d52ce4cee4b6fcc
    Former-commit-id: c9e2b70e8b207c6fce4764921ebea4922dec2988
    Former-commit-id: a27f7a6b68419d2767d2c5bdcbdd82951831f87e
    Former-commit-id: 78af6b9ccd824244e328beaae291056c77af0cab
    Former-commit-id: 126348234a6e5fe0915a7c12be737e861b0ffbab

--- src/menu/modules/demo-playground.js
+++ src/menu/modules/demo-playground.js
@@ -61,6 +61,7 @@ export default {
       icon: 'bolt',
       children: [
         { path: `${pre}store/menu`, title: '菜单控制', icon: 'bars' },
+        { path: `${pre}store/size`, title: '全局尺寸', icon: 'font' },
         { path: `${pre}store/ua`, title: '浏览器信息', icon: 'info-circle' },
         { path: `${pre}store/gray`, title: '灰度模式', icon: 'eye' },
         { path: `${pre}store/fullscreen`, title: '全屏', icon: 'arrows-alt' },
