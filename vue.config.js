a6ac80928887898b792c07bf460a2b817c6adeaa
liyang <1711467488@qq.com>
2018-09-13 18:01:39 +0800

    no message

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,9 @@ function resolve (dir) {
 }
 
 // 基础路径 注意发布之前要先修改这里
-const baseUrl = '/d2-admin/'
+let baseUrl = '/'
+// 演示项目自动构建使用
+if (process.env.NODE_ENV === 'travis') baseUrl = '/d2-admin/'
 
 module.exports = {
   baseUrl: baseUrl, // 根据你的实际情况更改这里
