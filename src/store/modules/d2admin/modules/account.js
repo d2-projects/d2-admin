af3a9db1228e26aacfac6411fc27e4eb09a3ceff
liyang <1711467488@qq.com>
2018-09-14 09:02:49 +0800

    transition

--- src/store/modules/d2admin/modules/account.js
+++ src/store/modules/d2admin/modules/account.js
@@ -100,7 +100,7 @@ export default {
         // DB -> store 加载主题
         await dispatch('d2admin/theme/load', null, { root: true })
         // DB -> store 加载页面过渡效果设置
-        commit('d2admin/transition/load', null, { root: true })
+        await dispatch('d2admin/transition/load', null, { root: true })
         // DB -> store 持久化数据加载上次退出时的多页列表
         commit('d2admin/page/openedLoad', null, { root: true })
         // DB -> store 持久化数据加载侧边栏折叠状态
