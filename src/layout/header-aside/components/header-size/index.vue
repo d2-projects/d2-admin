a7af1acbf4d4dc6a82ca57e8edf58235f7854d07
liyang <1711467488@qq.com>
2018-09-14 15:09:58 +0800

    no message
    Former-commit-id: a98f0dc7f8c8202102557ec44a29081de09ef657 [formerly a98f0dc7f8c8202102557ec44a29081de09ef657 [formerly a98f0dc7f8c8202102557ec44a29081de09ef657 [formerly a98f0dc7f8c8202102557ec44a29081de09ef657 [formerly f244e1fdd164c1b79a79bb5a6b747ee86b9d8589 [formerly 9a88fe535b9d40bff1dcca1e9c312ab81c0961b5]]]]]
    Former-commit-id: 80f45f42407a12231dcb32861eae95da8b8f27d5
    Former-commit-id: 294d0beb7890b73dd462900f6634b9ed43dda50b
    Former-commit-id: f639bbc1de8e27b18262e84545ff22b8556c402a [formerly 0f01b98c3d1b41eaa511bb56813d860105da930f]
    Former-commit-id: 9a33c6b83d309a76711c4fad397c149dac55a8b5
    Former-commit-id: f556363312d2e505acc24ec531e787fc3863f9fc
    Former-commit-id: 4433f382bc7c25d437eabfab96e4a7b0dbca28b5
    Former-commit-id: eb558eb22372f558e8d93ae8dac07c40016e2644
    Former-commit-id: 963b8effbe52965ae14a77172a37c50dcfd6cf56

--- src/layout/header-aside/components/header-size/index.vue
+++ src/layout/header-aside/components/header-size/index.vue
@@ -21,7 +21,7 @@
 </template>
 
 <script>
-import { mapState, mapMutations, mapActions } from 'vuex'
+import { mapState, mapActions } from 'vuex'
 export default {
   name: 'd2-header-size',
   computed: {
@@ -38,7 +38,7 @@ export default {
           // 设置 element 全局尺寸
           this.$ELEMENT.size = val
           // 清空缓存设置
-          this.keepAliveClean()
+          this.pageKeepAliveClean()
           // 刷新此页面
           const { path, query } = this.$route
           this.$router.replace({
@@ -50,14 +50,12 @@ export default {
     }
   },
   methods: {
-    ...mapMutations('d2admin/page', [
-      'keepAliveClean'
-    ]),
-    ...mapActions('d2admin/size', [
-      'set'
-    ]),
+    ...mapActions({
+      pageKeepAliveClean: 'd2admin/page/keepAliveClean',
+      sizeSet: 'd2admin/size/set'
+    }),
     handleChange (value) {
-      this.set(value)
+      this.sizeSet(value)
     },
     iconName (name) {
       return name === this.value ? 'dot-circle-o' : 'circle-o'
