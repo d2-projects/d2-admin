3137f365564c051a2b16f5fd858d3b6817816e2b
liyang <1711467488@qq.com>
2018-10-23 19:05:33 +0800

    删除请求新版本相关的代码

--- src/pages/index/page.vue
+++ src/pages/index/page.vue
@@ -6,11 +6,12 @@
       <d2-icon-svg style="width: 120px;" name="d2-admin"/>
       <template slot="footer">
         <div class="page__btn-group">
-          <span @click="$open('https://github.com/d2-projects')">项目组</span> |
-          <span @click="$open('https://d2-projects.github.io/d2-admin-doc/zh/')">使用文档</span> |
-          <span @click="$open('https://github.com/d2-projects/d2-admin-start-kit')">简化版脚手架</span> |
-          <span @click="$open('https://alibaba.github.io/ice/scaffold?type=vue')">飞冰物料</span> |
-          <span @click="$open('https://juejin.im/user/57a48b632e958a006691b946/posts')">掘金专栏</span> |
+          <span @click="$open('https://github.com/d2-projects')">开源组织</span> |
+          <span @click="$open('https://d2-projects.github.io/d2-admin-doc/zh/')">文档</span> |
+          <span @click="$open('https://github.com/d2-projects/d2-admin-start-kit')">简化版</span> |
+          <span @click="$open('https://alibaba.github.io/ice/scaffold?type=vue')">飞冰</span> |
+          <span @click="$open('https://juejin.im/user/57a48b632e958a006691b946/posts')">掘金</span> |
+          <span @click="$open('https://awesome.fairyever.com/daily/')">程序员日报</span> |
           <el-popover
             :width="172"
             trigger="hover">
