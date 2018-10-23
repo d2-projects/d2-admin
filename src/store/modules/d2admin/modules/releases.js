7b9d46655d7fedd28104a07bd49c4adaa05c6c3a
liyang <1711467488@qq.com>
2018-10-23 19:05:33 +0800

    删除请求新版本相关的代码
    Former-commit-id: c4aafa90eef4fa78782d750fc33e484d071e93af [formerly c4aafa90eef4fa78782d750fc33e484d071e93af [formerly c4aafa90eef4fa78782d750fc33e484d071e93af [formerly c4aafa90eef4fa78782d750fc33e484d071e93af [formerly 2ef73ea49b3405be63a1552eaa45649af7d559df [formerly e8792780e531de7b81bec704953a57fb05c3255e]]]]]
    Former-commit-id: 80c8177590bbadb58158ea371d1aa67f05de60f5
    Former-commit-id: ef3cdcd6a5eed24569fa9a0c38ead6903d8f84b0
    Former-commit-id: 9874c1f8f43c8beda5404b222d53697774f808bb [formerly 810d6baa2283c6310dd8ae35fa99b810331cd7f3]
    Former-commit-id: cd5531c1a53a55fa62d897a8ffa27b30b0d5be50
    Former-commit-id: 79be8e7248a71c1b608c009d4b200c6cf04572ca
    Former-commit-id: 0d797ed715b7091e9b9a3748dd85152a7c86549e
    Former-commit-id: c175d6cb3db552a3b8749fa51c0cb6375fcfc0a7
    Former-commit-id: 6c2d3338e77a6f3ca12fee92621d23ea795f7a3d

--- src/store/modules/d2admin/modules/releases.js
+++ src/store/modules/d2admin/modules/releases.js
@@ -1,5 +1,3 @@
-import { httpGet } from '@/api/sys/http'
-import semver from 'semver'
 import util from '@/libs/util.js'
 import setting from '@/setting.js'
 
@@ -7,33 +5,7 @@ export default {
   namespaced: true,
   state: {
     // D2Admin 版本
-    version: setting.releases.version,
-    // 最新版本的信息
-    latest: {},
-    // 有新版本
-    update: false
-  },
-  actions: {
-    /**
-     * @description 检查版本更新
-     * @param {Object} param context
-     */
-    checkUpdate ({ state, commit }) {
-      httpGet(setting.releases.api)
-        .then(res => {
-          let versionGet = res.tag_name
-          const update = semver.lt(state.version, versionGet)
-          if (update) {
-            util.log.capsule('D2Admin', `New version ${res.name}`)
-            console.log(`版本号: ${res.tag_name} | 详情 ${res.html_url}`)
-            commit('updateSet', true)
-          }
-          commit('latestSet', res)
-        })
-        .catch(err => {
-          console.log('checkUpdate error', err)
-        })
-    }
+    version: setting.releases.version
   },
   mutations: {
     /**
@@ -44,24 +16,7 @@ export default {
       util.log.capsule('D2Admin', `v${state.version}`)
       console.log('Github https://github.com/d2-projects/d2-admin')
       console.log('Doc    https://d2-projects.github.io/d2-admin-doc/zh/')
-    },
-    /**
-     * @description 设置是否有新的 D2Admin 版本
-     * @param {Object} state vuex state
-     * @param {Boolean} update can update
-     */
-    updateSet (state, update) {
-      // store 赋值
-      state.update = update
-    },
-    /**
-     * @description 设置最新版本的信息
-     * @param {Object} state vuex state
-     * @param {Object}} latest releases value
-     */
-    latestSet (state, latest) {
-      // store 赋值
-      state.latest = latest
+      console.log('请不要吝啬您的 star，谢谢 ~')
     }
   }
 }
