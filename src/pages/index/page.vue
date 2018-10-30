2985a155ecbf51ba4309bcebde6e41931756ddd1
liyang <1711467488@qq.com>
2018-10-30 16:40:12 +0800

    no message

--- src/pages/index/page.vue
+++ src/pages/index/page.vue
@@ -1,9 +1,7 @@
 <template>
   <d2-container class="page">
-    <d2-page-cover
-      title="I AM D2ADMIN"
-      sub-title="追求简约美感的后台管理系统集成方案">
-      <d2-icon-svg style="width: 120px;" name="d2-admin"/>
+    <d2-page-cover title="D2 Admin" sub-title="追求简约美感的后台管理系统集成方案">
+      <d2-icon-svg class="page__logo" name="d2-admin"/>
       <template slot="footer">
         <div class="page__btn-group">
           <span @click="$open('https://github.com/d2-projects')">开源组织</span> |
@@ -11,19 +9,12 @@
           <span @click="$open('https://github.com/d2-projects/d2-admin-start-kit')">简化版</span> |
           <span @click="$open('https://alibaba.github.io/ice/scaffold?type=vue')">飞冰</span> |
           <span @click="$open('https://juejin.im/user/57a48b632e958a006691b946/posts')">掘金</span> |
-          <span @click="$open('https://awesome.fairyever.com/daily/')">程序员日报</span> |
-          <el-popover
-            :width="172"
-            trigger="hover">
+          <span @click="$open('https://awesome.fairyever.com/daily/')">开发者日报</span> |
+          <el-popover :width="172" trigger="hover">
             <p class="d2-mt-0 d2-mb-10">D2Projects</p>
             <img src="./image/qr@2x.png" style="width: 172px;">
             <span slot="reference">微信公众号</span>
-            <p
-              style="
-                font-size: 12px;
-                margin-top: 0px;
-                margin-bottom: 0px;
-              ">
+            <p style="font-size: 12px; margin-top: 0px; margin-bottom: 0px;">
               官方公众号，主要推送前端技术类文章、框架资源、学习教程，以及 D2 系列项目更新信息
             </p>
           </el-popover>
@@ -49,6 +40,9 @@ export default {
 <style lang="scss" scoped>
 @import '~@/assets/style/public.scss';
 .page {
+  .page__logo {
+    width: 120px;
+  }
   .page__btn-group {
     color: $color-text-placehoder;
     font-size: 12px;
