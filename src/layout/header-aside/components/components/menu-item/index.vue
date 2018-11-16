af5dc868062e1076f41740319d4280e790fc1760
liyang <1711467488@qq.com>
2018-11-16 16:08:29 +0800

    feat: 设置 icon: '' 可以取消默认的菜单图标
    Former-commit-id: d120f41c803d47690de8893477c756e6ffb83db2 [formerly d120f41c803d47690de8893477c756e6ffb83db2 [formerly d120f41c803d47690de8893477c756e6ffb83db2 [formerly d120f41c803d47690de8893477c756e6ffb83db2 [formerly 2f749745ee9ff29e12160add58eec22290864fb8 [formerly 4e13189cb5c790327bfb4e2077f5c56f15435e5b]]]]]
    Former-commit-id: 44d5e2001b4fb2600683ae59cb344d11d2ce14ae
    Former-commit-id: b2cce82b9b00f5e03464736de4d49d0244afdecb
    Former-commit-id: ff18a6bc3834a98e2e40d1c7b5d36ef2bc445cac [formerly 3b1706d464f22a040836545bd1c3b9d3615791b6]
    Former-commit-id: e4ed2abcdedf4064d8a610e529e2deb9304f869c
    Former-commit-id: 2268aacbbc45cd16be1db1eeecc59c0b86206afd
    Former-commit-id: 92d4ed88a305128f919d14c1af2f3a164657bcc4
    Former-commit-id: ac16e300526c7dcf20d84bc95347be9f8e11216d
    Former-commit-id: 3a9646f02accd23e664068d42efa5b7d057889bc

--- src/layout/header-aside/components/components/menu-item/index.vue
+++ src/layout/header-aside/components/components/menu-item/index.vue
@@ -1,8 +1,8 @@
 <template>
   <el-menu-item :index="menu.path || uniqueId">
     <i v-if="menu.icon" :class="`fa fa-${menu.icon}`"></i>
-    <d2-icon-svg v-else-if="menu.iconSvg" :name="menu.iconSvg"/>
-    <i v-else class="fa fa-file-o"></i>
+    <i v-if="menu.icon === undefined" class="fa fa-file-o"></i>
+    <d2-icon-svg v-if="menu.iconSvg" :name="menu.iconSvg"/>
     <span slot="title">{{menu.title || '未命名菜单'}}</span>
   </el-menu-item>
 </template>
