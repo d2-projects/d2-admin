c6feba642d0cadcef1c8dd2c6509efe60e44dc3c
liyang <1711467488@qq.com>
2018-11-17 15:43:45 +0800

    bind attrs

--- src/pages/demo/frame/d2-doc/index.vue
+++ src/pages/demo/frame/d2-doc/index.vue
@@ -1,3 +1,13 @@
 <template>
-  <d2-container-frame src="https://doc.d2admin.fairyever.com/zh/"/>
+  <d2-container-frame :filename="filename" src="https://doc.d2admin.fairyever.com/zh/"/>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      filename: __filename
+    }
+  }
+}
+</script>
