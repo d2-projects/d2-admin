757d58b2446734c192830b6193f3e14649570251
liyang <1711467488@qq.com>
2018-11-27 22:33:37 +0800

    add html demo

--- src/router/modules/frame.js
+++ src/router/modules/frame.js
@@ -9,6 +9,7 @@ export default {
   redirect: { name: 'demo-frame-index' },
   component: layoutHeaderAside,
   children: (pre => [
+    { path: 'html', name: `${pre}html`, component: () => import('@/pages/demo/frame/html'), meta: { ...meta, title: '静态 HTML' } },
     { path: 'd2-doc', name: `${pre}d2-doc`, component: () => import('@/pages/demo/frame/d2-doc'), meta: { ...meta, title: 'D2Admin 中文文档' } }
   ])('demo-frame-')
 }
