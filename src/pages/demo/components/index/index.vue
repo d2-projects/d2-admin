e1b1bbf2649b03aa0b2dfffc02523940f150df31
liyang <1711467488@qq.com>
2018-12-03 11:35:37 +0800

    在每个模块的首页显示链接列表

--- src/pages/demo/components/index/index.vue
+++ src/pages/demo/components/index/index.vue
@@ -1,14 +1,17 @@
 <template>
   <d2-container :filename="filename" type="ghost">
-    <d2-module-banner slot="header" v-bind="banner"/>
+    <d2-module-index-banner slot="header" v-bind="banner"/>
+    <d2-module-index-menu :menu="menu"/>
   </d2-container>
 </template>
 
 <script>
+import menu from '@/menu/modules/demo-components'
 export default {
   data () {
     return {
       filename: __filename,
+      menu,
       banner: {
         title: 'COMPONENTS',
         subTitle: 'D2Admin 为你提供了一些上手即用的组件'
--- src/pages/demo/playground/index/index.vue
+++ src/pages/demo/playground/index/index.vue
@@ -1,14 +1,17 @@
 <template>
   <d2-container :filename="filename" type="ghost">
-    <d2-module-banner slot="header" v-bind="banner"/>
+    <d2-module-index-banner slot="header" v-bind="banner"/>
+    <d2-module-index-menu :menu="menu"/>
   </d2-container>
 </template>
 
 <script>
+import menu from '@/menu/modules/demo-playground'
 export default {
   data () {
     return {
       filename: __filename,
+      menu,
       banner: {
         title: 'PLAYGROUND',
         subTitle: '在这里可以测试一些 D2Admin 的系统功能'
