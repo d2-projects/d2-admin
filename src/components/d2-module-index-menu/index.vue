4e59b306669944fb46f37fd032c1eaf18789b17f
liyang <1711467488@qq.com>
2018-12-03 11:35:37 +0800

    在每个模块的首页显示链接列表
    Former-commit-id: 6cab63bfcc9fbbb16740f86c293fec3996061006 [formerly 6cab63bfcc9fbbb16740f86c293fec3996061006 [formerly 6cab63bfcc9fbbb16740f86c293fec3996061006 [formerly 6cab63bfcc9fbbb16740f86c293fec3996061006 [formerly beab075483c86b86dcf8088652d47d10d17e791b [formerly aa0a4c06e6e6c365614a38137e570322db6356f5]]]]]
    Former-commit-id: e27ebca4c2bb81ef3930d2a2744b569e3ba0c47e
    Former-commit-id: 05ab8b935658496dbb83ebd1726fd7abdb85b4a5
    Former-commit-id: 9fe99c61fd2c812e7ac86a65ef7a96fb77e2555a [formerly 8448054abcfec1aeb5d29b6b3ab9bb7994a0e5d3]
    Former-commit-id: c40b050f64112493e7ea738e8cac1b3ec32839d6
    Former-commit-id: cdb02c46bc4ec0dd38a15c7682acbe4da3f4ffe2
    Former-commit-id: 6e620c8a7bf1f4feae914af55a3d4e48f2527adc
    Former-commit-id: 46e4a65d456e1159c9bb5d4c34d4c171ea38c145
    Former-commit-id: 90f1ebb055ffc14de13d90d00dc7bb14545c34c8

---
+++ src/components/d2-module-index-menu/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <transition name="fade-transverse">
+    <div v-if="show" class="d2-module-index-menu">
+      <d2-module-index-menu-group
+        v-for="(item, index) in menu.children"
+        :key="index"
+        :menu="item"/>
+    </div>
+  </transition>
+</template>
+
+<script>
+import d2ModuleIndexMenuGroup from './components/group'
+export default {
+  components: {
+    d2ModuleIndexMenuGroup
+  },
+  props: {
+    menu: {
+      default: () => ({
+        children: []
+      })
+    }
+  },
+  data () {
+    return {
+      show: false
+    }
+  },
+  mounted () {
+    setTimeout(() => {
+      this.show = true
+    }, 300)
+  }
+}
+</script>
+
+<style lang="scss">
+.d2-module-index-menu {
+  @extend %unable-select;
+  margin-top: 20px;
+}
+</style>
