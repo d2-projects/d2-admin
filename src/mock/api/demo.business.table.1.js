b1afe31a051d235e8b7e45353fce58db29254252
liyang <1711467488@qq.com>
2018-12-12 22:58:18 +0800

    更新mock设置
    Former-commit-id: 3561c0b2ab4034d5d5a89b6a2f35de618c147c2c [formerly 3561c0b2ab4034d5d5a89b6a2f35de618c147c2c [formerly 3561c0b2ab4034d5d5a89b6a2f35de618c147c2c [formerly 3561c0b2ab4034d5d5a89b6a2f35de618c147c2c [formerly ac1af8ddc9819b789fd70b8da10ee223faed6e69 [formerly 22a45253b3ddb18715a39500f9bf5b56e1b37c6e]]]]]
    Former-commit-id: acba007c5344dce6a0122676027ba0266c5636b0
    Former-commit-id: 33b90caaf8810a029cf1f051b4b0f30f7c328309
    Former-commit-id: 6d602d737c16c5eda5f8e2448acb7c83c8df8c4c [formerly 415683706be473d2888806175eec075c57798e0e]
    Former-commit-id: c5285636ff550374708ba4346fc6e98eb08d32e3
    Former-commit-id: 0ed99aac2efee06256bc94d52eac820ca24dda35
    Former-commit-id: d8a75b3f1da2a75645e0965f2b278bde10ef16cb
    Former-commit-id: 8b424ac8ef5df06cb89d76e4ba3a3fcdeb34f113
    Former-commit-id: 93f866cbfcf129ed0f0393c4bf567a45c6a17568

--- src/mock/api/demo.business.table.1.js
+++ src/mock/api/demo.business.table.1.js
@@ -1,29 +1,25 @@
 export default {
   path: RegExp('/api/demo/business/table/1'),
   method: 'post',
-  handle ({ method, url, params, body, Mock, Random, Generator, Repeat }) {
+  handle ({ body, Repeat }) {
     const { page } = body
     page.total = 1000
-    return Mock.mock(
-      {
-        code: 0,
-        msg: '获取数据成功',
-        data: {
-          page,
-          'list|20': [
-            {
-              'key': '@guid',
-              'value|1': [10, 100, 200, 500],
-              'type': '@boolean',
-              'admin': '@cname',
-              'adminNote': '@cparagraph(0.5)',
-              'dateTimeCreat': '@datetime',
-              'used': '@boolean',
-              'dateTimeUse': '@datetime'
-            }
-          ]
-        }
+    return {
+      code: 0,
+      msg: '获取数据成功',
+      data: {
+        page,
+        list: Repeat(page.size, {
+          'key': '@guid',
+          'value|1': [10, 100, 200, 500],
+          'type': '@boolean',
+          'admin': '@cname',
+          'adminNote': '@cparagraph(0.5)',
+          'dateTimeCreat': '@datetime',
+          'used': '@boolean',
+          'dateTimeUse': '@datetime'
+        })
       }
-    )
+    }
   }
 }
