17d8a708979de828369a00bc27f0f26e3566fba1
liyang <1711467488@qq.com>
2018-12-16 13:36:32 +0800

    前缀改为 date
    Former-commit-id: a7de50a0c5cb4b52d312f52754c963b04915048f [formerly a7de50a0c5cb4b52d312f52754c963b04915048f [formerly a7de50a0c5cb4b52d312f52754c963b04915048f [formerly a7de50a0c5cb4b52d312f52754c963b04915048f [formerly 881400792d8358d396ef093d9c057fe67ddf639b [formerly dcab6ba2e48a1f0b6a850ff6d833d338181751e7]]]]]
    Former-commit-id: 37b3f5705798919ab3a6d78b0045fb865af805e5
    Former-commit-id: 1ef37f5db55fd10650b066ec78cadff72019ae1b
    Former-commit-id: b3f214e0620c26307ea5f62cf3bdd4a1bebc3114 [formerly 0b60de30f3e0c9be8abca715ab3f2db0ac2ff83a]
    Former-commit-id: 584d8b27e2ab6afceb687aef3c6c92525e1f7df5
    Former-commit-id: 4da9fbb42d117a1e4fda2d1e36b3db3d9c11936c
    Former-commit-id: b4c62a79e03448524126a8c49c8cdb2c148fb841
    Former-commit-id: 922607fe8153c8a65ac15dd437acb31ce72af650
    Former-commit-id: 3820a773b0d91146f7b1eaaf3d5106ef4a6d0f76

--- src/filters/module.date.js
+++ src/filters/module.date.js
@@ -34,59 +34,59 @@ const filters = {
   // 时间字符串 | Date 对象 | Unix 时间戳 (毫秒)
   day: value => dayjs(value),
   // Unix 时间戳 (秒)
-  day_unix: value => dayjs.unix(value),
+  date_unix: value => dayjs.unix(value),
   // ---------- [ 获取 ] ----------
-  day_year: Day => P(Day).year(),
-  day_month: Day => P(Day).month(),
-  day_date: Day => P(Day).date(),
-  day_day: Day => P(Day).day(),
-  day_hour: Day => P(Day).hour(),
-  day_minute: Day => P(Day).minute(),
-  day_second: Day => P(Day).second(),
-  day_millisecond: Day => P(Day).millisecond(),
+  date_year: Day => P(Day).year(),
+  date_month: Day => P(Day).month(),
+  date_date: Day => P(Day).date(),
+  date_day: Day => P(Day).day(),
+  date_hour: Day => P(Day).hour(),
+  date_minute: Day => P(Day).minute(),
+  date_second: Day => P(Day).second(),
+  date_millisecond: Day => P(Day).millisecond(),
   // ---------- [ 设置 ] ----------
   // date | day | month | year | hour | minute | second | millisecond
   // 对大小写不敏感
-  day_set: (Day, unit, value) => P(Day).set(unit, value),
+  date_set: (Day, unit, value) => P(Day).set(unit, value),
   // ---------- [ 操作 ] ----------
   // 增加
-  day_add: (Day, value, unit) => P(Day).add(value, unit),
+  date_add: (Day, value, unit) => P(Day).add(value, unit),
   // 减少
-  day_subtract: (Day, value, unit) => P(Day).subtract(value, unit),
+  date_subtract: (Day, value, unit) => P(Day).subtract(value, unit),
   // 开头时间
-  day_startof: (Day, unit) => P(Day).startOf(unit),
+  date_startof: (Day, unit) => P(Day).startOf(unit),
   // 末尾时间
-  day_endof: (Day, unit) => P(Day).endOf(unit),
+  date_endof: (Day, unit) => P(Day).endOf(unit),
   // ---------- [ 显示 ] ----------
   // 格式化
-  day_format: (Day, setting = 'YYYY-MM-DD HH:mm:ss') => P(Day).format(setting),
+  date_format: (Day, setting = 'YYYY-MM-DD HH:mm:ss') => P(Day).format(setting),
   // 时间差
-  day_diff: (Day, Day2 = '', unit = 'millisecond', accurate = false) => P(Day).diff(dayjs(Day2), unit, accurate),
+  date_diff: (Day, Day2 = '', unit = 'millisecond', accurate = false) => P(Day).diff(dayjs(Day2), unit, accurate),
   // Unix 时间戳 (毫秒)
-  day_value_millisecond: Day => P(Day).valueOf(),
+  date_value_millisecond: Day => P(Day).valueOf(),
   // Unix 时间戳 (秒)
-  day_value_second: Day => P(Day).unix(),
+  date_value_second: Day => P(Day).unix(),
   // 月份的天数
-  day_days_in_month: Day => P(Day).daysInMonth(),
+  date_days_in_month: Day => P(Day).daysInMonth(),
   // Date 对象
-  day_to_date: Day => P(Day).toDate(),
+  date_to_date: Day => P(Day).toDate(),
   // 数组
-  day_to_array: Day => P(Day).toArray(),
+  date_to_array: Day => P(Day).toArray(),
   // JSON
-  day_to_json: Day => P(Day).toJSON(),
+  date_to_json: Day => P(Day).toJSON(),
   // ISO8601 格式
-  day_to_iso: Day => P(Day).toISOString(),
+  date_to_iso: Day => P(Day).toISOString(),
   // 对象
-  day_to_object: Day => P(Day).toObject(),
+  date_to_object: Day => P(Day).toObject(),
   // 字符
-  day_to_string: Day => P(Day).toString(),
+  date_to_string: Day => P(Day).toString(),
   // ---------- [ 查询 ] ----------
   // 是否之前
-  day_is_before: (Day, Day2, unit = 'millisecond') => P(Day).isBefore(dayjs(Day2), unit),
+  date_is_before: (Day, Day2, unit = 'millisecond') => P(Day).isBefore(dayjs(Day2), unit),
   // 是否之后
-  day_is_after: (Day, Day2, unit = 'millisecond') => P(Day).isAfter(dayjs(Day2), unit),
+  date_is_after: (Day, Day2, unit = 'millisecond') => P(Day).isAfter(dayjs(Day2), unit),
   // 是否相同
-  day_is_same: (Day, Day2, unit = 'millisecond') => P(Day).isSame(dayjs(Day2), unit)
+  date_is_same: (Day, Day2, unit = 'millisecond') => P(Day).isSame(dayjs(Day2), unit)
 }
 
 export default {
