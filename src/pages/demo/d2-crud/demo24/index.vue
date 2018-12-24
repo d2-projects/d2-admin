cf62a4014d500a6094842c4d2e1362f3dbf99853
孙昊翔 <673686754@qq.com>
2018-12-24 11:18:55 +0800

    demo29

--- src/pages/demo/d2-crud/demo24/index.vue
+++ src/pages/demo/d2-crud/demo24/index.vue
@@ -22,54 +22,54 @@
 </template>
 
 <script>
-import doc from "./doc.md";
-import code from "./code.js";
+import doc from './doc.md'
+import code from './code.js'
 
 export default {
-  data() {
+  data () {
     return {
       filename: __filename,
       doc,
       code,
       columns: [
         {
-          title: "日期",
-          key: "date",
-          width: "180"
+          title: '日期',
+          key: 'date',
+          width: '180'
         },
         {
-          title: "姓名",
-          key: "name",
-          width: "180"
+          title: '姓名',
+          key: 'name',
+          width: '180'
         },
         {
-          title: "地址",
-          key: "address"
+          title: '地址',
+          key: 'address'
         }
       ],
       data: [
         {
-          date: "2016-05-02",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄"
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
         },
         {
-          date: "2016-05-04",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1517 弄"
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄'
         },
         {
-          date: "2016-05-01",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1519 弄"
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄'
         },
         {
-          date: "2016-05-03",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1516 弄"
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
         }
       ]
-    };
+    }
   }
-};
+}
 </script>
