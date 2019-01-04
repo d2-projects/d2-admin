3d42cebf87d34dba8c940096944e220bf221b732
孙昊翔 <673686754@qq.com>
2019-01-04 10:05:07 +0800

    unit test: d2-container-card

--- tests/unit/d2-container-card.spec.js
+++ tests/unit/d2-container-card.spec.js
@@ -2,7 +2,16 @@ import { mount } from '@vue/test-utils'
 import D2ContainerCard from '@/components/d2-container/components/d2-container-card.vue'
 
 describe('d2-container-card.vue', () => {
-  it('渲染slot', () => {
+  // 存在且是Vue组件实例
+  it('is a vue instance', () => {
+    const wrapper = mount(D2ContainerCard)
+
+    expect(wrapper.exists()).toBeTruthy()
+    expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+
+  // 包含特定类名
+  it('contains specific classnames', () => {
     const wrapper = mount(D2ContainerCard, {
       slots: {
         default: '<div>body</div>',
@@ -11,8 +20,25 @@ describe('d2-container-card.vue', () => {
       }
     })
 
-    expect(wrapper.html()).toContain('<div class="d2-container-card__body"><div class="d2-container-card__body-card"><div>body</div></div>')
-    expect(wrapper.html()).toContain('<div class="d2-container-card__header"><div>header</div></div>')
-    expect(wrapper.html()).toContain('<div class="d2-container-card__footer"><div>footer</div></div>')
+    expect(wrapper.is('.d2-container-card')).toBeTruthy()
+    expect(wrapper.contains('.d2-container-card__header')).toBeTruthy()
+    expect(wrapper.contains('.d2-container-card__body')).toBeTruthy()
+    expect(wrapper.contains('.d2-container-card__body-card')).toBeTruthy()
+    expect(wrapper.contains('.d2-container-card__footer')).toBeTruthy()
+  });
+
+  // 渲染slot
+  it('has one or more slots', () => {
+    const wrapper = mount(D2ContainerCard, {
+      slots: {
+        default: '<div>body</div>',
+        header: '<div>header</div>',
+        footer: '<div>footer</div>'
+      }
+    })
+
+    expect(wrapper.text()).toMatch('header')
+    expect(wrapper.text()).toMatch('body')
+    expect(wrapper.text()).toMatch('footer')
   })
 })
