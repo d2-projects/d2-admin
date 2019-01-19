48c4c06fee4470bc94634562c8502044fe2295ad
liyang <1711467488@qq.com>
2019-01-19 16:22:16 +0800

    add D2 Crud info

--- src/store/modules/d2admin/modules/releases.js
+++ src/store/modules/d2admin/modules/releases.js
@@ -9,8 +9,9 @@ export default {
      */
     versionShow () {
       util.log.capsule('D2Admin', `v${process.env.VUE_APP_VERSION}`)
-      console.log('Github https://github.com/d2-projects/d2-admin')
-      console.log('Doc    https://doc.d2admin.fairyever.com/zh/')
+      console.log('D2 Admin  https://github.com/d2-projects/d2-admin')
+      console.log('D2 Crud   https://github.com/d2-projects/d2-crud')
+      console.log('Document  https://doc.d2admin.fairyever.com/zh/')
       console.log('请不要吝啬您的 star，谢谢 ~')
     }
   }
