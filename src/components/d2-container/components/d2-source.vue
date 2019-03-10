dff880c8988c4cb05b3af3136bee3f79375b429c
liyang <1711467488@qq.com>
2019-03-10 19:09:30 +0800

    中文提示 更容易理解

--- src/components/d2-container/components/d2-source.vue
+++ src/components/d2-container/components/d2-source.vue
@@ -4,7 +4,7 @@
     class="d2-source"
     :class="{ 'd2-source--active': isActive }"
     @click="handleClick">
-    <d2-icon name="code"/> SourceCode
+    <d2-icon name="code"/> 本页源码
   </div>
 </template>
 
