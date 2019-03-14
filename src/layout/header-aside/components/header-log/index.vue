3180feded25798ff13dece7f528bd9d8bb0db344
liyang <1711467488@qq.com>
2019-03-14 16:58:09 +0800

    删除多余的 div

--- src/layout/header-aside/components/header-log/index.vue
+++ src/layout/header-aside/components/header-log/index.vue
@@ -1,29 +1,27 @@
 <template>
-  <div>
-    <el-tooltip
-      effect="dark"
-      :content="tooltipContent"
-      placement="bottom">
-      <el-button
-        class="d2-ml-0 d2-mr btn-text can-hover"
-        type="text"
-        @click="handleClick">
-        <el-badge
-          v-if="logLength > 0"
-          :max="99"
-          :value="logLengthError"
-          :is-dot="logLengthError === 0">
-          <d2-icon
-            :name="logLengthError === 0 ? 'dot-circle-o' : 'bug'"
-            style="font-size: 20px"/>
-        </el-badge>
+  <el-tooltip
+    effect="dark"
+    :content="tooltipContent"
+    placement="bottom">
+    <el-button
+      class="d2-ml-0 d2-mr btn-text can-hover"
+      type="text"
+      @click="handleClick">
+      <el-badge
+        v-if="logLength > 0"
+        :max="99"
+        :value="logLengthError"
+        :is-dot="logLengthError === 0">
         <d2-icon
-          v-else
-          name="dot-circle-o"
+          :name="logLengthError === 0 ? 'dot-circle-o' : 'bug'"
           style="font-size: 20px"/>
-      </el-button>
-    </el-tooltip>
-  </div>
+      </el-badge>
+      <d2-icon
+        v-else
+        name="dot-circle-o"
+        style="font-size: 20px"/>
+    </el-button>
+  </el-tooltip>
 </template>
 
 <script>
