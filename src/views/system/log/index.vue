b07250cc409540acd707cd284497895e9b62dfc3
liyang <1711467488@qq.com>
2019-03-19 19:50:23 +0800

    系统日志页面搭建
    Former-commit-id: 5acd4b9057871a6523fff27c28647a4087b977dc [formerly 5acd4b9057871a6523fff27c28647a4087b977dc [formerly 5acd4b9057871a6523fff27c28647a4087b977dc [formerly 5acd4b9057871a6523fff27c28647a4087b977dc [formerly a7dbb26902e95b3a64e57793e7b072adce4a2ec4 [formerly 4cc2829e67d3f66865415ced76cd7c8a1cce504c]]]]]
    Former-commit-id: 637aad670296f24c5ced7f13246a2e54987f7904
    Former-commit-id: a51647921d27766c644d04e5d5e2ea83525231c6
    Former-commit-id: da28fbc3875fa0bb4aa0b4ebea035e391d1ded0c [formerly ac56d726dd3e4760951c39f5744444fb20075105]
    Former-commit-id: ace138db807039e2915fac0a6ec9311dc1ac843b
    Former-commit-id: 6087d466bd3ce2fc6bd1365b1de44e30941d1bca
    Former-commit-id: 78c0406ed6f149c165da73fc477ece0407e409b3
    Former-commit-id: 48e719e1cd05a0ba06cd46f85efa44f125381bb4
    Former-commit-id: 785e4a4ecae7175700d20d322309aaff445e1512

---
+++ src/views/system/log/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <d2-container>
+    <template slot="header">
+      <el-button
+        type="primary"
+        size="mini"
+        :loading="uploading"
+        @click="handleUpload">
+        Upload
+      </el-button>
+    </template>
+    Log Page
+  </d2-container>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      uploading: false
+    }
+  },
+  methods: {
+    handleUpload () {
+      this.uploading = true
+      this.$notify({
+        type: 'info',
+        title: '日志上传',
+        message: '开始上传日志'
+      })
+      setTimeout(() => {
+        this.uploading = false
+        this.$notify({
+          type: 'success',
+          title: '日志上传',
+          message: '上传成功'
+        })
+      }, 3000)
+    }
+  }
+}
+</script>
+
