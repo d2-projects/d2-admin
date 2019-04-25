493718bf6c4b47f0a6d69ee25348f58aee075e18
liyang <1711467488@qq.com>
2019-04-25 11:07:12 +0800

    最新依赖 + dartsass

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,5 @@
 module.exports = {
   presets: [
     '@vue/app'
-  ],
-  plugins: [
-    // https://babeljs.io/docs/en/babel-plugin-transform-runtime/
-    '@babel/plugin-transform-runtime'
   ]
 }
