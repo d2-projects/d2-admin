953a203e80d52171273a1930785b527ae19fd22b
liyang <1711467488@qq.com>
2019-05-10 10:23:54 +0800

    :construction: netlify test
    Former-commit-id: 694fa8abd65ae947c9f7a8869ec6603491ed1fda [formerly 34bc2923d35422bbcba984e32486c0205ec11f10] [formerly 694fa8abd65ae947c9f7a8869ec6603491ed1fda [formerly 34bc2923d35422bbcba984e32486c0205ec11f10] [formerly 694fa8abd65ae947c9f7a8869ec6603491ed1fda [formerly 34bc2923d35422bbcba984e32486c0205ec11f10] [formerly 34bc2923d35422bbcba984e32486c0205ec11f10 [formerly b7650434d305b2c409e552286f9ecc4e0136d944 [formerly d354fe3ebe71a63d5216ede92a40e6c416fbf0b1]]]]]
    Former-commit-id: e72d2064d00890c9fd6843506d5598e61bc1c083
    Former-commit-id: 763de57fa64dacdc978f9b4707088e40f021492a
    Former-commit-id: f99c4b74eaf7ab391b8ab19cf46a5751fa804f94 [formerly 40e43416861cb5c2f4397e79ed2b980ddfe0d20f]
    Former-commit-id: 034144fb00d002147875d1505895a7a6e7f31a65
    Former-commit-id: 6668fa18dd54e1b034c7bb81cdb84a705af364fb
    Former-commit-id: eeb92acefc3389b1c0acf85752840ecdefc69d56
    Former-commit-id: 478ac903d508cd617b2d5050968fb224b5ea5b93
    Former-commit-id: 2fcff008fbc02b6351ebb956778a1cf2359bcf76

--- vue.config.js
+++ vue.config.js
@@ -12,8 +12,6 @@ process.env.VUE_APP_BUILD_TIME = require('dayjs')().format('YYYY-M-D HH:mm:ss')
 // 基础路径 注意发布之前要先修改这里
 let publicPath = '/'
 
-if (process.env.VUE_APP_BUILD_MODE === 'NETLIFY') publicPath = '/dist/'
-
 module.exports = {
   publicPath, // 根据你的实际情况更改这里
   lintOnSave: true,
