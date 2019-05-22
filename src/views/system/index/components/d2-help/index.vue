5027a1f11ba9f1fae9a7545cabc9abe29ca14067
liyang <1711467488@qq.com>
2019-05-22 23:43:58 +0800

    多国语优化

--- src/views/system/index/components/d2-help/index.vue
+++ src/views/system/index/components/d2-help/index.vue
@@ -1,10 +1,6 @@
 <template>
   <div>
-    <el-button
-      type="primary"
-      @click="dialogVisible = true"
-      plain
-      round>
+    <el-button type="primary" @click="dialogVisible = true" plain round>
       <d2-icon name="question-circle-o" class="d2-mr-5"/>
       {{ $t('views.system.index.help.button') }}
     </el-button>
@@ -31,15 +27,23 @@
             {{ $t('views.system.index.help.dialog.link.text.new-issue') }}
           </el-button>
         </el-button-group>
-        <h2>{{ $t('views.system.index.help.dialog.sub-title.join-chat-group') }}</h2>
+        <h2>{{ $t('views.system.index.help.dialog.sub-title.join') }}</h2>
         <el-row :gutter="20">
           <el-col :span="12">
-            <img src="./image/qq.jpg" class="d2-mb" style="width: 100%;">
-            <p>QQ群 [ 1500+ / 2000 人 ]</p>
+            <el-alert
+              :closable="false"
+              type="info"
+              :title="$t('views.system.index.help.dialog.join.qq')"
+              class="d2-mb"/>
+            <img src="./image/qq.jpg" style="width: 100%;">
           </el-col>
           <el-col :span="12">
-            <img src="./image/we.jpg" class="d2-mb" style="width: 100%;">
-            <p>与作者成为好友后邀请进微信群"</p>
+            <el-alert
+              :closable="false"
+              type="info"
+              :title="$t('views.system.index.help.dialog.join.we')"
+              class="d2-mb"/>
+            <img src="./image/we.jpg" style="width: 100%;">
           </el-col>
         </el-row>
       </div>
