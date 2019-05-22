f90566f2fbd3e28e22cdee67a5a9ad8c6dc00650
liyang <1711467488@qq.com>
2019-05-22 08:53:33 +0800

    多国语优化
    Former-commit-id: eaf2280f24b18f3f68407e2102b1f5e1303a990e [formerly eaf2280f24b18f3f68407e2102b1f5e1303a990e [formerly eaf2280f24b18f3f68407e2102b1f5e1303a990e [formerly eaf2280f24b18f3f68407e2102b1f5e1303a990e [formerly 9dc91cf38de012013013912fcc40b1c5d9cf0871 [formerly 5a523dac50ee3832c2ed3f99683eaf29c7151268]]]]]
    Former-commit-id: 60a70a22a467a1b6af40a60a8aa537b36ff40266
    Former-commit-id: ca85e1476b6e0f3138bbe840fc70ef6d38aa0881
    Former-commit-id: 14a1544228265bbe58206188ef182c167c183c15 [formerly 219d178e2271b2a584f949694645683161519373]
    Former-commit-id: c9719d3a73a148e6958bcca466058b7df8433b1b
    Former-commit-id: a2896c5587348dca3345c6674602df08578dafec
    Former-commit-id: 2fdef3c0edd9e86ad00357495ab805fa67e1c987
    Former-commit-id: f4e7b80606149e91b9c8e243c0163fb9df3a9201
    Former-commit-id: d0f52d1de087791127e7d9cc80ced8fa69dc7925

--- vue.config.js
+++ vue.config.js
@@ -110,7 +110,7 @@ module.exports = {
   // i18n
   pluginOptions: {
     i18n: {
-      locale: 'zh',
+      locale: 'zh-chs',
       fallbackLocale: 'en',
       localeDir: 'locales',
       enableInSFC: true
