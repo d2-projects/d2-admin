f9c93010aa03b6b169612d163bdce5c9154361be
liyang <1711467488@qq.com>
2019-05-23 21:41:04 +0800

    默认中文

--- vue.config.js
+++ vue.config.js
@@ -110,8 +110,8 @@ module.exports = {
   // i18n
   pluginOptions: {
     i18n: {
-      locale: 'en',
-      fallbackLocale: 'zh-chs',
+      locale: 'zh-chs',
+      fallbackLocale: 'en',
       localeDir: 'locales',
       enableInSFC: true
     }
