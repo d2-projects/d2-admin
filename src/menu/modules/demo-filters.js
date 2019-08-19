820df1f0da18052d5ed34aeabff20f78cb5ee721
FairyEver <1711467488@qq.com>
2019-08-19 23:25:25 +0800

    remove filters

--- src/menu/modules/demo-filters.js
+++
@@ -1,9 +0,0 @@
-export default {
-  path: '/demo/filters',
-  title: '内置过滤器',
-  icon: 'flask',
-  children: (pre => [
-    { path: `${pre}index`, title: '内置过滤器', icon: 'home' },
-    { path: `${pre}day`, title: '日期和时间', icon: 'calendar' }
-  ])('/demo/filters/')
-}
