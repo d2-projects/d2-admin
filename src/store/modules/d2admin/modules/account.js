6f426425c1016967509632d428576514c6135eba
FairyEver <1711467488@qq.com>
2019-09-25 15:14:36 +0800

    提取 layout 和登录页面的公用切换语言逻辑
    Former-commit-id: 43ed6a96bd897366861aed0915b87f2b65df4710 [formerly 43ed6a96bd897366861aed0915b87f2b65df4710 [formerly 43ed6a96bd897366861aed0915b87f2b65df4710 [formerly 43ed6a96bd897366861aed0915b87f2b65df4710 [formerly 777111bc2dbc71b3ff2549572e38e78d3035d9ca [formerly e6ef292ed2b174fc179c662698b564887654b68c]]]]]
    Former-commit-id: 509743dec34795b62814f46da3fcdc8b87bc95af
    Former-commit-id: 930b9e6713d8330b182e7f9cac621b2179c6515c
    Former-commit-id: 95707d671efc130e783bb46a87fbd0ec10f143b6 [formerly 3fa108f2d027a562ac3e81635a815197b42171f1]
    Former-commit-id: 9937d0028823f1c4b89d463a1f1f25bbee245d67
    Former-commit-id: 525670c8db99a28555c847a506613fa852c7bb50
    Former-commit-id: bbb54be5298d9cde045fae85c20b6f91bee642e7
    Former-commit-id: c8cbad1e3bad52b4405ab92f490c86475f2d4d76
    Former-commit-id: c86e7baad3cd7b82f2a9a121c6fdc84ba8fe2e5f

--- src/store/modules/d2admin/modules/account.js
+++ src/store/modules/d2admin/modules/account.js
@@ -70,8 +70,6 @@ export default {
       if (confirm) {
         commit('d2admin/gray/set', true, { root: true })
         MessageBox.confirm('确定要注销当前用户吗', '注销用户', {
-          confirmButtonText: '确定',
-          cancelButtonText: '取消',
           type: 'warning'
         })
           .then(() => {
