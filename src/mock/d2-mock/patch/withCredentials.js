ecc43fd67deadea28b827d3cc4f26ae98509c9ea
FairyEver <1711467488@qq.com>
2019-11-23 12:51:40 +0800

    fix 244
    Former-commit-id: 090f70e5ee1074ad4fa0232212da596818a170fd
    Former-commit-id: e9ca4e5a7b10c3fbd79d9ef966fbd9d377574f99
    Former-commit-id: 3b3ae486c3789190214502b0f22e45ec62dd86f9
    Former-commit-id: 8b4e7322cf56c3ee1b86785e455a52af38ed6442 [formerly fca7c2263e116ae2c91c483c986f3ba6904bd48f]
    Former-commit-id: 423908cf32625f2c5bd374ce5c298038c914560b
    Former-commit-id: aad33feba903ccdad8bd59d5c68e036578c2eddb
    Former-commit-id: ecec60dbf286d5f11828eafdbb3886df15af368f
    Former-commit-id: 08197b214676b116159c19bc5ae994d494ea4771
    Former-commit-id: 7dee5f3deccc9b1c635c7d1af37af4ec96f1c90a

--- src/mock/d2-mock/patch/withCredentials.js
+++ src/mock/d2-mock/patch/withCredentials.js
@@ -2,7 +2,10 @@ export default function (Mock) {
   // http://cnine.me/note/FrontEnd/mock-lose-cookies-dbg.html
   Mock.XHR.prototype.__send = Mock.XHR.prototype.send
   Mock.XHR.prototype.send = function () {
-    if (this.custom.xhr) this.custom.xhr.withCredentials = this.withCredentials || false
+    if (this.custom.xhr) {
+      this.custom.xhr.withCredentials = this.withCredentials || false
+      this.custom.xhr.responseType = this.responseType
+    }
     this.__send.apply(this, arguments)
   }
 }
