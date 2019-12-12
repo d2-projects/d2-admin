251df63305e17c78eabbfc8ad04be6ef73d41dea
FairyEver <1711467488@qq.com>
2019-12-12 21:04:00 +0800

    重置
    Former-commit-id: c067e78a8b3439a7d5452d855255b263268e7c6a
    Former-commit-id: 36f5fc1c344e4366ef5df0c5249060ef272e923a
    Former-commit-id: d7b80329e1e32d765dfcf7137a0258d04ea828e9
    Former-commit-id: b4953f1722e9f1b5fd6e2e8e810d4939b88c777d [formerly 0e84aa3da905a2296a1173481ad471fcc9694c87]
    Former-commit-id: f8cbd3411f3beade8e09897214e90c4524d990c3
    Former-commit-id: 8c39014477b6b5cb6a1a631f655f8e186f329d08
    Former-commit-id: 99ac48e01d5d5b0ebde0d4382eb975c336cdcb6c
    Former-commit-id: 3e684fafe6d2c3c956bbc2c6df84d106678958da
    Former-commit-id: c83934c358b246f51b1fd0f29ab02720d8d6b162

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,8 +8,8 @@ module.exports = {
     '@vue/standard'
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-console': 'off',
+    'no-debugger': 'off'
   },
   parserOptions: {
     parser: 'babel-eslint'
