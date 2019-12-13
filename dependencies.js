13496d187ac42aa24e2a4335d03b2b712958ef69
FairyEver <1711467488@qq.com>
2019-12-13 17:17:00 +0800

    cdn
    Former-commit-id: cbe02f9079eb9f862302422c69d41bcc031b0218
    Former-commit-id: b8f6a6fe1d9d3702524bbcdf6a790e247396ea61
    Former-commit-id: a06ce9805f9692b875f8e2058102029ed542c037
    Former-commit-id: 55d0e944d7f7e4d2d09ecd6cce12da0e20b02cd5 [formerly 8060aa8c582057f51361cb45618a79cb1b463686]
    Former-commit-id: c95d99c9bd189dc2c30fd5c68201d3fe6cab073d
    Former-commit-id: 538d3f81cedefa6e3c76a496c4873bdb43fb4776
    Former-commit-id: 828f6d000c025c8c1e4744dadc8ef4bd597e2691
    Former-commit-id: 1b769306155dc3f65854a441e149959c4a3aa066
    Former-commit-id: 15576c0820844ca0db378dadb8ce26d7ec1578c7

--- dependencies.js
+++ dependencies.js
@@ -6,6 +6,12 @@ module.exports = [
     js: 'https://cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.js'
   },
   {
+    name: 'vue-i18n',
+    library: 'VueI18n',
+    css: '',
+    js: 'https://unpkg.com/vue-i18n@8.15.1/dist/vue-i18n.js'
+  },
+  {
     name: 'vue-router',
     library: 'VueRouter',
     css: '',
@@ -83,5 +89,59 @@ module.exports = [
     library: 'hljs',
     css: 'https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@9.17.1/build/styles/github-gist.min.css',
     js: 'https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@9.17.1/build/highlight.min.js'
+  },
+  {
+    name: 'mockjs',
+    library: 'Mock',
+    css: '',
+    js: 'https://unpkg.com/mockjs@1.1.0/dist/mock.js'
+  },
+  {
+    name: 'lodash',
+    library: '_',
+    css: '',
+    js: 'https://cdn.jsdelivr.net/npm/lodash@4.17.15/lodash.min.js'
+  },
+  {
+    name: 'vue-json-tree-view',
+    library: 'TreeView',
+    css: '',
+    js: 'https://unpkg.com/vue-json-tree-view@2.1.4/dist/vue-json-tree-view.min.js'
+  },
+  {
+    name: 'ua-parser-js',
+    library: 'UAParser',
+    css: '',
+    js: 'https://cdn.jsdelivr.net/npm/ua-parser-js@0.7.20/dist/ua-parser.min.js'
+  },
+  {
+    name: 'js-cookie',
+    library: 'Cookies',
+    css: '',
+    js: 'https://cdn.jsdelivr.net/npm/js-cookie@2.2.1/src/js.cookie.min.js'
+  },
+  {
+    name: 'nprogress',
+    library: 'NProgress',
+    css: 'https://unpkg.com/nprogress@0.2.0/nprogress.css',
+    js: 'https://unpkg.com/nprogress@0.2.0/nprogress.js'
+  },
+  {
+    name: 'dayjs',
+    library: 'dayjs',
+    css: '',
+    js: 'https://cdn.jsdelivr.net/npm/dayjs@1.8.17/dayjs.min.js'
+  },
+  {
+    name: 'fuse.js',
+    library: 'Fuse',
+    css: '',
+    js: 'https://cdnjs.cloudflare.com/ajax/libs/fuse.js/3.4.5/fuse.min.js'
+  },
+  {
+    name: 'hotkeys-js',
+    library: 'hotkeys',
+    css: '',
+    js: 'https://unpkg.com/hotkeys-js@3.7.3/dist/hotkeys.min.js'
   }
 ]
