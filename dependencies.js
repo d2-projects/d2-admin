5a1f1089cf7a03221763f173c0f429099d710c44
FairyEver <1711467488@qq.com>
2019-12-13 11:22:12 +0800

    v-contextmenu
    Former-commit-id: 243483b33ab4e7683d7bf319f1c75835acddf86d
    Former-commit-id: c68464c56e54615ba9cee1b80e1b3b8be27d26c3
    Former-commit-id: f36b35f337c03871d5bdf54a8607744580b68a54
    Former-commit-id: e0db29f54a13ba993da6282b2bbbcf976b8a58e1 [formerly 355b520170638d34b6286088df36dae6e6d707c9]
    Former-commit-id: e51bce007923bc9968591a032947b0b72a28783e
    Former-commit-id: d372872320398ff8563456fc11145b5ebbcd0cb5
    Former-commit-id: 367bf91c7475c2cfa7ed3b5ce5e6622866db4a6f
    Former-commit-id: 3e769d030925cda27b8859d71e6bb0b141f66482
    Former-commit-id: 6a7d187e38cb8a1b8a82ef6141e6f72f2117af4e

--- dependencies.js
+++ dependencies.js
@@ -1,35 +1,5 @@
 module.exports = [
   {
-    name: 'vue',
-    library: 'Vue',
-    css: '',
-    js: 'https://cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.js'
-  },
-  {
-    name: 'vue-router',
-    library: 'VueRouter',
-    css: '',
-    js: 'https://unpkg.com/vue-router@3.0.6/dist/vue-router.js'
-  },
-  {
-    name: 'vuex',
-    library: 'Vuex',
-    css: '',
-    js: 'https://unpkg.com/vuex@3.1.1/dist/vuex.js'
-  },
-  {
-    name: 'axios',
-    library: 'axios',
-    css: '',
-    js: 'https://unpkg.com/axios@0.19.0/dist/axios.min.js'
-  },
-  {
-    name: 'better-scroll',
-    library: 'BScroll',
-    css: '',
-    js: 'https://unpkg.com/better-scroll@1.15.2/dist/bscroll.min.js'
-  },
-  {
     name: '@d2-projects/d2-crud',
     library: 'D2Crud',
     css: '',
@@ -46,5 +16,41 @@ module.exports = [
     library: 'VueTableImport',
     css: '',
     js: 'https://cdn.d2.pub/packages/@d2-projects/vue-table-import@1.0.0/vue-table-import.js'
+  },
+  {
+    name: 'axios',
+    library: 'axios',
+    css: '',
+    js: 'https://unpkg.com/axios@0.19.0/dist/axios.min.js'
+  },
+  {
+    name: 'better-scroll',
+    library: 'BScroll',
+    css: '',
+    js: 'https://unpkg.com/better-scroll@1.15.2/dist/bscroll.min.js'
+  },
+  {
+    name: 'v-contextmenu',
+    library: 'VContextmenu',
+    css: 'https://unpkg.com/v-contextmenu@2.9.0/dist/index.css',
+    js: 'https://unpkg.com/v-contextmenu@2.9.0/dist/index.common.js'
+  },
+  {
+    name: 'vue',
+    library: 'Vue',
+    css: '',
+    js: 'https://cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.js'
+  },
+  {
+    name: 'vue-router',
+    library: 'VueRouter',
+    css: '',
+    js: 'https://unpkg.com/vue-router@3.0.6/dist/vue-router.js'
+  },
+  {
+    name: 'vuex',
+    library: 'Vuex',
+    css: '',
+    js: 'https://unpkg.com/vuex@3.1.1/dist/vuex.js'
   }
 ]
