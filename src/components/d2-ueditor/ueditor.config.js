e8a231fb60198aa23af01f91320751ea82dfdd69
FairyEver <1711467488@qq.com>
2019-12-19 08:57:34 +0800

    更新 ueditor 配置

--- src/components/d2-ueditor/ueditor.config.js
+++ src/components/d2-ueditor/ueditor.config.js
@@ -1,5 +1,5 @@
 export default {
-  UEDITOR_HOME_URL: `${process.env.BASE_URL}/lib/UEditor/`,
+  UEDITOR_HOME_URL: `${process.env.BASE_URL}lib/UEditor/`,
   // 编辑器不自动被内容撑高
   autoHeightEnabled: false,
   // 初始容器高度
