06f1acf522c83b966ec244bd2c9bbd9b3972f0a5
FairyEver <1711467488@qq.com>
2020-04-19 11:50:32 +0800

    fix: 修复菜单自动折叠的 bug
    fix #229

--- src/menu/index.js
+++ src/menu/index.js
@@ -1,3 +1,5 @@
+import { uniqueId } from 'lodash'
+
 // 插件
 import demoPlugins from './modules/demo-plugins'
 // 组件
@@ -15,8 +17,23 @@ import demoD2Crud from './modules/demo-d2-crud'
 // 第三方网页
 import demoFrame from './modules/demo-frame'
 
+/**
+ * @description 给菜单数据补充上 path 字段
+ * @description https://github.com/d2-projects/d2-admin/issues/209
+ * @param {Array} menu 原始的菜单数据
+ */
+function supplementPath (menu) {
+  return menu.map(e => ({
+    ...e,
+    path: e.path || uniqueId('d2-menu-empty-'),
+    ...e.children ? {
+      children: supplementPath(e.children)
+    } : {}
+  }))
+}
+
 // 菜单 侧边栏
-export const menuAside = [
+export const menuAside = supplementPath([
   demoComponents,
   demoPlugins,
   demoCharts,
@@ -25,10 +42,10 @@ export const menuAside = [
   demoBusiness,
   demoD2Crud,
   demoFrame
-]
+])
 
 // 菜单 顶栏
-export const menuHeader = [
+export const menuHeader = supplementPath([
   {
     path: '/index',
     title: '首页',
@@ -60,4 +77,4 @@ export const menuHeader = [
   },
   demoPlayground,
   demoBusiness
-]
+])
