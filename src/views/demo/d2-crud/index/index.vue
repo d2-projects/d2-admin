a494560c8a6bab6a17bce423286e5593de0f3b8f
FairyEver <1711467488@qq.com>
2020-04-21 16:47:57 +0800

    feat: 移除无用的 d2-crud v1 提示

--- src/views/demo/d2-crud/index/index.vue
+++ src/views/demo/d2-crud/index/index.vue
@@ -1,14 +1,6 @@
 <template>
   <d2-container type="ghost">
     <d2-module-index-banner slot="header" v-bind="banner"/>
-    <el-alert
-      title="目前 d2-crud 已经升级至 2.x，并且和 1.x 存在一些 API 不兼容，如果您使用的是 1.x，请点击此消息跳转至旧版示例"
-      type="warning"
-      :closable="false"
-      class="d2-mt"
-      show-icon
-      @click.native="$open('https://v1.d2-crud.fairyever.com/#/d2-crud/index')">
-    </el-alert>
     <d2-module-index-menu :menu="menu"/>
   </d2-container>
 </template>
