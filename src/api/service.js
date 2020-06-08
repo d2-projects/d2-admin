079fd1c2809ea7f47d3f835a65d94a39ed9a8cf6
FairyEver <1711467488@qq.com>
2020-06-08 15:48:21 +0800

    feat: :sparkles: 移除对 error 的无用判断
    Variable 'error' is null checked here, but afterwards at line 68 , it is passed as the first parameter of 'errorLog()' without null check. Note that the parameter's property is accessed in the function body at line 72 of src/api/tools.js.

--- src/api/service.js
+++ src/api/service.js
@@ -48,22 +48,20 @@ function createService () {
       }
     },
     error => {
-      if (error) {
-        const status = get(error, 'response.status')
-        switch (status) {
-          case 400: error.message = '请求错误'; break
-          case 401: error.message = '未授权，请登录'; break
-          case 403: error.message = '拒绝访问'; break
-          case 404: error.message = `请求地址出错: ${error.response.config.url}`; break
-          case 408: error.message = '请求超时'; break
-          case 500: error.message = '服务器内部错误'; break
-          case 501: error.message = '服务未实现'; break
-          case 502: error.message = '网关错误'; break
-          case 503: error.message = '服务不可用'; break
-          case 504: error.message = '网关超时'; break
-          case 505: error.message = 'HTTP版本不受支持'; break
-          default: break
-        }
+      const status = get(error, 'response.status')
+      switch (status) {
+        case 400: error.message = '请求错误'; break
+        case 401: error.message = '未授权，请登录'; break
+        case 403: error.message = '拒绝访问'; break
+        case 404: error.message = `请求地址出错: ${error.response.config.url}`; break
+        case 408: error.message = '请求超时'; break
+        case 500: error.message = '服务器内部错误'; break
+        case 501: error.message = '服务未实现'; break
+        case 502: error.message = '网关错误'; break
+        case 503: error.message = '服务不可用'; break
+        case 504: error.message = '网关超时'; break
+        case 505: error.message = 'HTTP版本不受支持'; break
+        default: break
       }
       errorLog(error)
       return Promise.reject(error)
