bb8ece7d25efc79d2f435cd44216763a3db96c3f
FairyEver <1711467488@qq.com>
2020-08-28 09:46:20 +0800

    feat: :sparkles: 在扫描 src/api/modules 下的接口配置时 支持多级文件夹下的文件

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,7 @@ import faker from 'faker/locale/zh_CN'
 import { service, request, serviceForMock, requestForMock, mock } from './service'
 import * as tools from './tools'
 
-const files = require.context('./modules', false, /\.js$/)
+const files = require.context('./modules', true, /\.js$/)
 const generators = files.keys().map(key => files(key).default)
 
 export default assign({}, ...map(generators, generator => generator({
