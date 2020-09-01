d505583b35901afe5f4943895d6fadef808a3dce
FairyEver <1711467488@qq.com>
2020-09-01 16:40:36 +0800

    feat: :sparkles: d2-crud-plus 演示首页优化

--- src/views/demo/d2-crud-plus/index/index.vue
+++ src/views/demo/d2-crud-plus/index/index.vue
@@ -1,11 +1,11 @@
 <template>
   <d2-container type="ghost">
     <d2-module-index-banner slot="header" v-bind="banner"/>
-    <el-tabs>
+    <el-tabs v-model="tab">
       <el-tab-pane label="效果" name="result">
         <img src="./image/gif.webp" style="width: 600px; border: 1px solid #E4E7ED; border-radius: 4px;">
       </el-tab-pane>
-      <el-tab-pane label="code" name="code">
+      <el-tab-pane label="代码" name="code">
         <d2-highlight :code="code" lang="javascript"/>
       </el-tab-pane>
     </el-tabs>
@@ -19,6 +19,7 @@ export default {
   data () {
     return {
       code,
+      tab: 'result',
       banner: {
         title: 'D2 CRUD PLUS',
         subTitle: 'D2 Crud Plus 是一个面向配置的Crud开发框架，辅助d2-crud和d2-crud-x，让开发crud更简单',
