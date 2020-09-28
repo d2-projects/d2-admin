68a5bcbe2eab32732374ee15857360113067728e
FairyEver <1711467488@qq.com>
2020-09-28 09:34:08 +0800

    style: 移除一处 d2-icon 多余代码

--- src/layout/header-aside/components/menu-side/index.js
+++ src/layout/header-aside/components/menu-side/index.js
@@ -22,7 +22,7 @@ export default {
       {
         this.aside.length === 0 && !this.asideCollapse
           ? <div class="d2-layout-header-aside-menu-empty" flex="dir:top main:center cross:center">
-            <d2-icon name="inbox"></d2-icon>
+            <d2-icon name="inbox"/>
             <span>没有侧栏菜单</span>
           </div>
           : null
