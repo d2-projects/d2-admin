f46d1c0001b9bffb24620f76a18bc0a337e8a090
liy <1711467488@qq.com>
2023-05-14 17:20:21 +0800

    chore: adjustment

--- packages/assets/vite.config.js
+++ packages/assets/vite.config.js
@@ -2,7 +2,6 @@ import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue()
