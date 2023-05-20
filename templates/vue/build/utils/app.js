e3b12d7eb591b62d7138eb46c4084bf14e4faac8
liy <1711467488@qq.com>
2023-05-20 20:39:15 +0800

    chore: adjustment

--- templates/vue/build/utils/app.js
+++ templates/vue/build/utils/app.js
@@ -1,6 +1,7 @@
 import { resolve } from 'path'
 import glob from 'fast-glob'
 import { options } from './options.js'
+import { objectToAscii, tableToAscii } from '@d2-framework/utils'
 
 const metaDefault = {
   title: '',
@@ -13,6 +14,7 @@ export async function scanProjects ({} = {}) {
   const projects = await Promise.all(entries.map(async entry => {
     const name = entry.match(/projects\/(.+)\/index.html/)[1];
     const { default: meta } = await import(resolve(entry, '../project.js'));
+    console.log('meta')
     console.log(meta);
     const build = options.project.length === 0 || options.project.includes(name);
     return {
@@ -26,5 +28,23 @@ export async function scanProjects ({} = {}) {
   if (options.index && options.project.length === 1) {
     projects[projects.findIndex(project => project.name === options.project[0])].output = 'index';
   }
+  console.log(objectToAscii({
+    title: 'arguments',
+    data: options
+  }))
+  console.log(tableToAscii({
+    title: 'projects',
+    columns: [
+      { label: 'project', key: 'name'   },
+      { label: 'entry'  , key: 'entry'  },
+      { label: 'build'  , key: 'build'  },
+      { label: 'output' , key: 'output' },
+    ],
+    formatters: {
+      build: value => value ? 'yes' : 'no',
+      output: value => value || '-'
+    },
+    data: projects,
+  }))
   return projects;
 }
--- templates/vue/vite.config.js
+++ templates/vue/vite.config.js
@@ -7,31 +7,10 @@ import Jsx from '@vitejs/plugin-vue-jsx'
 import { d2LogoSvg } from '@d2-framework/assets'
 import VirtualHtml from 'vite-plugin-virtual-html'
 import { visualizer } from 'rollup-plugin-visualizer'
-import { objectToAscii, tableToAscii } from '@d2-framework/utils'
-import { options } from './build/utils/options.js'
 import { scanProjects } from './build/utils/app.js'
 
 export default defineConfig(async () => {
   const projects = await scanProjects()
-  console.log(projects)
-  console.log(objectToAscii({
-    title: 'arguments',
-    data: options
-  }))
-  console.log(tableToAscii({
-    title: 'projects',
-    columns: [
-      { label: 'project', key: 'name'   },
-      { label: 'entry'  , key: 'entry'  },
-      { label: 'build'  , key: 'build'  },
-      { label: 'output' , key: 'output' },
-    ],
-    formatters: {
-      build: value => value ? 'yes' : 'no',
-      output: value => value || '-'
-    },
-    data: projects,
-  }))
   return {
     plugins: [
       VirtualHtml({
